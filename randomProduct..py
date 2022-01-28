cd529ee6e6130d6d45b252b504fc835686aab681
Opeyemi Adejumo <1163018@us.ray.com>
2022-01-28 09:34:04 -0600

    Added Code Quality check with GitHub Actions

---
+++ randomProduct..py
@@ -0,0 +1,10 @@
+import random, sys
+import os
+def myfunc():
+    rand = random.random()
+    return random.randint(0, 100)
+def multiply (a,b):
+    return a *  b
+
+
+print(multiply(myfunc(), myfunc()))
