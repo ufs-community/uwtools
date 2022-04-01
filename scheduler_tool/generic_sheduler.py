063336600444ced2df9737bb4fc4a326488b904d
Opeyemi Adejumo <1163018@us.ray.com>
2022-04-01 10:57:28 -0500

    added input error handling

--- scheduler_tool/generic_sheduler.py
+++ scheduler_tool/generic_sheduler.py
@@ -28,10 +28,28 @@ def get_non_negative_int(prompt):
             break
     return value
 
+def validate_clock(in_):
+    while True:
+        try:
+            a, b, c = in_.split(":")
+            for v in [a, b, c]:
+                assert len(v) == 2
+            _, _, _ = int(a), int(b), int(c)
+        except ValueError:
+            print("Sorry, Enter in format HH:MM:SS.")
+            continue
+
+        if _ < 0:
+            print("Sorry, your response must not be negative.")
+            continue
+        else:
+            break
+    return _, _, _
+
 jobname = input("Enter Name of Job: \n")
 std_out = input("Enter Path to STDOUT: \n")
 std_err = input("Enter Path to STD_Error: \n")
-walltime = input("Enter Wall Time: \n")
+walltime = validate_clock(input("Enter Wall Time in HH:MM:SS: \n"))
 queue_name = input("Enter Queue Name: \n")
 acc_name = input("Enter Account Name: \n")
 node_count = get_non_negative_int("Enter Node Count: \n")
@@ -44,7 +62,7 @@ with open(job_file, 'w') as fh:
         fh.writelines("#SBATCH --job-name=%s\n" % jobname)
         fh.writelines("#SBATCH --output=.out/%s.out\n" % std_out)
         fh.writelines("#SBATCH --error=.out/%s.err\n" % std_err)
-        fh.writelines("#SBATCH --time=%s\n" % walltime)
+        fh.writelines("#SBATCH --time=%s:%s:%s\n" % walltime)
         fh.writelines("#SBATCH --partition=%s\n" % queue_name)
         fh.writelines("#SBATCH --account=%s\n" % acc_name)
         fh.writelines("#SBATCH --nodes=%s\n" % node_count)
