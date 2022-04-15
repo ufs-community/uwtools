6cebce0d17400465085570abc4c682e6454ba271
Opeyemi Adejumo <1163018@us.ray.com>
2022-04-15 09:23:40 -0500

    WIP- Schduler tool

--- src/uwtools/generic_scheduler/scheduler.py
+++ src/uwtools/generic_scheduler/scheduler.py
@@ -1,27 +1,42 @@
-import abc
-from ast import List
+from abc import ABC, abstractmethod
 from datetime import datetime
-from importlib.resources import path
-from msilib.schema import ListView
 from pathlib import Path
 
-
-class Scheduler:
+class Scheduler(ABC):
     def __init__(self, scheduler: str, job_name: str, partition: list, qos: str, output: Path, error: Path, walltime: datetime, account: str, nodes: int, ntasks_per_node: int, ntasks: int, cpus_per_task: int, reservation: str, join: bool, native_flags: list, run_command: str):
-        pass
-    @abc.abstractmethod
-    def add_native_flag(flag):
-        pass
-    def check_required_options():
-        pass
-    @abc.abstractmethod
-    def create_directive_list():
-        pass
-    @abc.abstractmethod
-    def join_output():
+        self.scheduler = scheduler
+        self.job_name = job_name
+        self.partition = partition
+        self.qos = qos
+        self.output = output
+        self.error = error
+        self.walltime = walltime
+        self.account = account
+        self.nodes = nodes
+        self.ntasks_per_node =ntasks_per_node
+        self.ntasks = ntasks
+        self.cpus_per_task = cpus_per_task
+        self.reservation = reservation
+        self.join = join
+        self.native_flags = native_flags
+        self.run_command = run_command
+
+    @abstractmethod
+    def add_native_flag(self, flag):
         pass
 
-    def write_job_card():
+    def check_required_options(self):
+        pass
+
+    @abstractmethod
+    def create_directive_list(self):
+        pass
+
+    @abstractmethod
+    def join_output(self):
+        pass
+
+    def write_job_card(self):
         pass
 
 
--- src/uwtools/generic_scheduler/slurm.py
+++ src/uwtools/generic_scheduler/slurm.py
@@ -1,14 +1,44 @@
-from io import StringIO
 from .scheduler import Scheduler
+from datetime import datetime
+from pathlib import Path
 
 __all__ = ['Slurm']
 
-
 class Slurm(Scheduler):
-    pass
-def map_flags():
-    return
-def add_native_flag(flag):
-    pass
-def create_directive_list():
-    pass
+    _DIRECTIVE = '#SBATCH'
+
+    def __init__(self, scheduler: str, job_name: str, partition: list, qos: str, output: Path, error: Path, walltime: datetime, account: str, nodes: int, ntasks_per_node: int, ntasks: int, cpus_per_task: int, reservation: str, join: bool, native_flags: list, run_command: str):
+        super().__init__(scheduler, job_name, partition, qos, output, error, walltime, account, nodes, ntasks_per_node, ntasks, cpus_per_task, reservation, join, native_flags, run_command)
+        
+    def map_flags():
+        mappings = {'account': '-A', 'partition': '-p', 'wallclock': '-t', 'job_name': '-J', 'nodes': '-N', 'ntasks_per_node': '-n', 'output': '-o', 'error': '-e'}
+        return mappings
+
+    def add_native_flag(flag):
+        pass
+
+    def create_directive_list(self,specs):
+        '''
+        Generate the account specific items of the job card.
+        '''
+        strings = []
+        if 'jobname' in specs:
+            strings.append(f"{self._DIRECTIVE} --job-name {specs.jobname}")
+        if 'account' in specs:
+            strings.append(f"{self._DIRECTIVE} --account {specs.account}")
+        if 'walltime' in specs:
+            strings.append(f"{self._DIRECTIVE} --time {specs.walltime}")
+        if 'partition' in specs and specs.partition:
+            strings.append(f"{self._DIRECTIVE} --partition={specs.partition}")
+        if 'nodes' in specs:
+            strings.append(f"{self._DIRECTIVE} --nodes={specs.nodes}")
+        if 'ntasks_per_node' in specs:
+            strings.append(f"{self._DIRECTIVE} --ntasks_per_node={specs.ntasks_per_node}")
+        if 'join' in specs:
+            strings.append(f"{self._DIRECTIVE} --error {specs.join}")
+        else:
+            if 'stdout' in specs:
+                strings.append(f"{self._DIRECTIVE} --output {specs.stdout}")
+            if 'stderr' in specs:
+                strings.append(f"{self._DIRECTIVE} --error {specs.stderr}")
+        return "\n".join(strings)
