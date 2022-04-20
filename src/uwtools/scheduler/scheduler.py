92d4be11661be39ad64961cc593c3d4b7998f492
Ryan Long <ryanlong1004@gmail.com>
2022-04-20 18:48:28 -0400

    Refactor schedulers a bit

--- src/uwtools/scheduler/scheduler.py
+++ src/uwtools/scheduler/scheduler.py
@@ -1,76 +1,32 @@
-from abc import ABC, abstractmethod
 import collections
-from datetime import datetime
-from pathlib import Path
-from typing import Any, Dict, List
+
 
 from uwtools.scheduler import slurm
 
-REQUIRED_OPTIONS: List[str] = []
 
-FLAGS: Dict[Any, Any] = {}
+class JobCard(collections.UserList):
+    def content(self, line_separator="\n"):
+        return line_separator.join(self)
 
-Directives = List
+
+MAP_SCHEDULER = {"slurm": slurm.Slurm, "pbs": slurm.PBS, "lsf": slurm.LSF, object: None}
 
 
-class Scheduler(ABC):
-    TYPE = None
-    _MAP_SCHEDULER = {"slurm": slurm.Slurm, object: None}
+class JobScheduler(collections.UserDict):
 
-    def __init__(self, **kwargs):
-        self._props = kwargs
+    _map = {}
+    prefix = ""
 
     def __getattr__(self, name):
-        if name in self.props:
-            return self.props[name]
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __repr__(self):
-        pass
-
-    def __str__():
-        pass
+        if name in self:
+            return self[name]
 
     @property
-    def content(self):
-        pass
+    def job_card(self) -> JobCard:
+        return JobCard(
+            [f"{self.prefix}{self._map[key]}{value}" for (key, value) in self.items()]
+        )
 
     @classmethod
     def get_scheduler(cls, props):
-        return cls._MAP_SCHEDULER[props.scheduler](props)
-
-    def add_native_flag(self, flag):
-        self.directives.append(self.native_flags)
-
-    def _create_directive_list(self: list):
-
-        """Uses the map_flags dict from subclass to build a directives list."""
-        # Add in dict logic from PR comment here.
-        raise NotImplementedError
-
-    @abstractmethod
-    def join_output(self):
-
-        """Different schedulers handle the joining of output by removal
-        of flags and/or the addition of other flags. PBS, for example,
-        requires the addition of "-j oe" to join output, and Slurm
-        requires that only the -o flag is provided."""
-        pass
-
-    @property
-    def directives(self):
-        ret = self._create_directive_list()
-        if self.native_flags:
-            self.add_native_flag()
-
-        if self.join:
-            self.join_output()
-        return ret
-
-    def write_job_card(self):
-
-        with open(self.job_card_path) as fn:
-            fn.write(self.directives.join("\n"))
-            fn.write(self.run_command)
+        return MAP_SCHEDULER[props.scheduler](props)
--- src/uwtools/scheduler/slurm.py
+++ src/uwtools/scheduler/slurm.py
@@ -1,80 +1,59 @@
+from typing import Any, Dict
 from src.uwtools.scheduler.scheduler import Scheduler
 from enum import Enum, auto
 
-
-# Using enum.auto() method
-class CommandKey(Enum):
-    JOB_NAME = auto()
-    OUTPUT = auto()
-    ERROR = auto()
-    TIME = auto()
-    PARTITION = auto()
-    ACCOUNT = auto()
-    NODES = auto()
-    NTASKS = auto()
-    WALL_TIME = auto()
-    QUEUE = auto()
-    TOTAL_NODES = auto()
-    CPUS = auto()
-    PLACE = auto()
-    DEBUG = auto()
-    BASH = auto()
-    CHANGE_DIR = auto()
+from string import Template
 
 
 class Slurm(Scheduler):
-    _map = {
-        CommandKey.JOB_NAME: "--job-name=",
-        CommandKey.OUTPUT: "--output=",
-        CommandKey.ERROR: "--error=",
-        CommandKey.WALL_TIME: "--time=",
-        CommandKey.PARTITION: "--partition=",
-        CommandKey.ACCOUNT: "--account=",
-        CommandKey.NODES: "--nodes=",
-        CommandKey.NTASKS: "--ntasks-per-node=",
-    }
-
     prefix = "#SBATCH"
 
-    pass
+    _map = {
+        "job_name": "--job-name=",
+        "output": "--output=",
+        "error": "--error=",
+        "wall_time": "--time=",
+        "partition": "--partition=",
+        "account": "--account=",
+        "nodes": "--nodes=",
+        "number_tasks": "--ntasks-per-node=",
+    }
 
 
 class Bash(Scheduler):
     prefix = ""
-    pass
 
 
 class PBS(Scheduler):
-    _map = {
-        CommandKey.BASH: "-S",
-        CommandKey.JOB_NAME: "-N",
-        CommandKey.OUTPUT: "-o",
-        CommandKey.JOB_NAME: "-j",
-        CommandKey.QUEUE: "-q",
-        CommandKey.ACCOUNT: "-A",
-        CommandKey.WALL_TIME: "-l walltime=",
-        CommandKey.TOTAL_NODES: "-l select=",
-        CommandKey.CPUS: "cpus=",
-        CommandKey.PLACE: "-l place=",
-        CommandKey.DEBUG: "-l debug=",
-    }
-
     prefix = "#PBS"
-    pass
+
+    _map = {
+        "bash": "-S",
+        "job_name": "-N",
+        "output": "-o",
+        "job_name": "-j",
+        "queue": "-q",
+        "account": "-A",
+        "wall_time": "-l walltime=",
+        "total_nodes": "-l select=",
+        "cpus": "cpus=",
+        "place": "-l place=",
+        "debug": "-l debug=",
+    }
 
 
 class LSF(Scheduler):
     _map = {
-        CommandKey.BASH: "-L",
-        CommandKey.JOB_NAME: "-J",
-        CommandKey.OUTPUT: "-o",
-        CommandKey.QUEUE: "-q",
-        CommandKey.ACCOUNT: "-P",
-        CommandKey.WALL_TIME: "-W",
-        CommandKey.TOTAL_NODES: "-n",
-        CommandKey.CPUS: "-R affinity[core()]", # TODO 
-        CommandKey.NTASKS: "-R span[ptile=]", # TODO
-        CommandKey.CHANGE_DIR: "-cwd /tmp",
+        "bash": "-L",
+        "job_name": "-J",
+        "output": "-o",
+        "queue": "-q",
+        "account": "-P",
+        "wall_time": "-W",
+        "total_nodes": "-n",
+        "cpus": "-R affinity[core()]",  # TODO
+        "number_tasks": "-R span[ptile=]",  # TODO
+        "change_dir": "-cwd /tmp",
     }
 
     prefix = "#BSUB"
