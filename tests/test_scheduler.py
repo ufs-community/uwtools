0f664c43847c1f3ab9650f33168f3811e63d8690
Ryan Long <ryanlong1004@gmail.com>
2022-04-21 13:41:01 -0400

    update tests

---
+++ tests/test_scheduler.py
@@ -0,0 +1,47 @@
+# pylint: disable=all
+import pytest
+
+from src.uwtools.scheduler import JobScheduler
+
+
+def test_dummy():
+    """Skip the test. Used only as a placeholder."""
+    return True
+
+
+def test_scheduler():
+
+    expected = """#SBATCH scheduler=slurm
+#SBATCH --job-name=abcd
+#SBATCH extra_stuff=12345"""
+
+    props = {"scheduler": "slurm", "job_name": "abcd", "extra_stuff": "12345"}
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_scheduler_dot_notation():
+
+    props = {"scheduler": "slurm", "job_name": "abcd", "extra_stuff": "12345"}
+
+    js = JobScheduler.get_scheduler(props)
+    expected = "abcd"
+    actual = js.job_name
+
+    assert actual == expected
+
+
+def test_scheduler_prop_not_defined_raises_key_error():
+    with pytest.raises(KeyError) as error:
+        props = {
+            "job_name": "abcd",
+            "extra_stuff": "12345",
+        }
+
+        JobScheduler.get_scheduler(props)
+    expected = "no scheduler defined in props: [job_name, extra_stuff]"
+    actual = str(error.value)
+    assert expected in actual
