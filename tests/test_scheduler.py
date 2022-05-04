19812ec704697ed4542afc6c64a5daf2e2ce9ab7
Ryan Long <69773028+ryanlong1004@users.noreply.github.com>
2022-05-04 06:42:29 -0400

    Feature remaining scheduler implementations (#38)
    * implement remaining schedulers and update tests
    
    * Update src/uwtools/scheduler.py
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>
    
    * Update tests/test_scheduler.py
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>
    
    * Initial refactor commit.
    
    * refactor for logical properties
    
    * update tests and tweak source
    
    * more tests.  update source to reflect test results.
    
    * add validator for required fields
    
    * Update tests/test_scheduler.py
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>
    
    * Update tests/test_scheduler.py
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>
    
    * Update scheduler.py
    
    * Added additional test for slurm
    
    * merge fix
    
    * merge fix
    
    * Revert "Added additional test for slurm"
    
    This reverts commit d7ac83c2b96e7e9a3ba2848fe3a54db7dd7c45e1.
    
    * Update schedulers with all tests
    
    Co-authored-by: Rahul Mahajan <aerorahul@users.noreply.github.com>
    Co-authored-by: ope-adejumo <98531279+ope-adejumo@users.noreply.github.com>

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -4,27 +4,21 @@ import pytest
 from uwtools.scheduler import JobScheduler
 
 
-def test_scheduler():
-
-    expected = """#SBATCH --job-name=abcd
-#SBATCH extra_stuff=12345
-#SBATCH scheduler=slurm"""
-
-    props = {"scheduler": "slurm", "job_name": "abcd", "extra_stuff": "12345"}
-
-    js = JobScheduler.get_scheduler(props)
-    actual = js.job_card.content()
-
-    assert actual == expected
-
-
 def test_scheduler_dot_notation():
 
-    props = {"scheduler": "slurm", "job_name": "abcd", "extra_stuff": "12345"}
+    props = {
+        "account": "user_account",
+        "scheduler": "lsf",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "tasks_per_node": 1,
+        "extra_stuff": "12345",
+        "nodes": 1,
+    }
 
     js = JobScheduler.get_scheduler(props)
-    expected = "abcd"
-    actual = js.job_name
+    expected = "user_account"
+    actual = js.account
 
     assert actual == expected
 
@@ -32,31 +26,419 @@ def test_scheduler_dot_notation():
 def test_scheduler_prop_not_defined_raises_key_error():
     with pytest.raises(KeyError) as error:
         props = {
-            "job_name": "abcd",
+            "account": "user_account",
+            "queue": "batch",
+            "walltime": "00:01:00",
+            "tasks_per_node": 1,
             "extra_stuff": "12345",
+            "nodes": 1,
         }
 
         JobScheduler.get_scheduler(props)
-    expected = "no scheduler defined in props: [job_name, extra_stuff]"
+    expected = (
+        "no scheduler defined in props: [account, queue, walltime, tasks_per_node"
+    )
     actual = str(error.value)
     assert expected in actual
 
 
-def test_scheduler_known_args():
+def test_scheduler_raises_exception_when_missing_required_attribs():
+    with pytest.raises(ValueError) as error:
+        props = {
+            "scheduler": "pbs",
+            "queue": "batch",
+            "walltime": "00:01:00",
+            "nodes": 1,
+            "tasks_per_node": 1,
+            "debug": True,
+        }
 
-    expected = """#SBATCH --extra-flags
-#SBATCH --extra_arg=12345
-#SBATCH --job-name=abcd
-#SBATCH scheduler=slurm"""
+        JobScheduler.get_scheduler(props)
+    expected = "missing required attributes: [account]"
+    actual = str(error.value)
+    assert expected in actual
+
+
+def test_pbs1():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=1:mpiprocs=1"""
 
     props = {
-        "scheduler": "slurm",
-        "job_name": "abcd",
-        "--extra_arg": "12345",
-        "--extra-flags": "",
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
     }
 
     js = JobScheduler.get_scheduler(props)
     actual = js.job_card.content()
 
+    assert actual == expected
+
+
+def test_pbs2():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=1:mpiprocs=4:mem=512M"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 4,
+        "memory": "512M",
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_pbs3():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=3:mpiprocs=4:ompthreads=2:ncpus=8"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 3,
+        "tasks_per_node": 4,
+        "threads": 2,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_pbs4():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=3:mpiprocs=4:ompthreads=2:ncpus=8:mem=512M"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 3,
+        "tasks_per_node": 4,
+        "threads": 2,
+        "memory": "512M",
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_pbs5():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=1:mpiprocs=1
+#PBS -l place=excl"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+        "exclusive": True,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_pbs6():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=1:mpiprocs=1
+#PBS -l place=vscatter"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+        "exclusive": False,
+        "placement": "vscatter",
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_pbs7():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l select=1:mpiprocs=1
+#PBS -l place=vscatter:excl"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+        "exclusive": True,
+        "placement": "vscatter",
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_pbs8():
+    expected = """#PBS -A account_name
+#PBS -q batch
+#PBS -l walltime=00:01:00
+#PBS -l debug=True
+#PBS -l select=1:mpiprocs=1"""
+
+    props = {
+        "scheduler": "pbs",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+        "debug": True,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    print(actual)
+    print("****")
+    print(expected)
+    assert actual == expected
+
+
+def test_slurm1():
+    expected = """#SBATCH --account=account_name
+#SBATCH --qos=batch
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1"""
+
+    props = {
+        "scheduler": "slurm",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_slurm2():
+    expected = """#SBATCH --account=account_name
+#SBATCH --qos=batch
+#SBATCH --partition=debug
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1"""
+
+    props = {
+        "scheduler": "slurm",
+        "account": "account_name",
+        "queue": "batch",
+        "partition": "debug",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_slurm3():
+    expected = """#SBATCH --account=account_name
+#SBATCH --qos=batch
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=4"""
+
+    props = {
+        "scheduler": "slurm",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 2,
+        "threads": 4,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_slurm4():
+    expected = """#SBATCH --account=account_name
+#SBATCH --qos=batch
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=4MB"""
+
+    props = {
+        "scheduler": "slurm",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 2,
+        "memory": "4MB",
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_slurm5():
+    expected = """#SBATCH --account=account_name
+#SBATCH --qos=batch
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive=True"""
+
+    props = {
+        "scheduler": "slurm",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+        "exclusive": True,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    
+    print(actual)
+    print("***************")
+    print(expected)
+    assert actual == expected
+
+
+def test_lsf1():
+    expected = """#BSUB -P account_name
+#BSUB -q batch
+#BSUB -W 00:01:00
+#BSUB -R affinity[core(1)]
+#BSUB -R span[ptile=1]
+#BSUB -n 1"""
+
+    props = {
+        "scheduler": "lsf",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 1,
+        "threads": 1,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    print(actual)
+    print("***************")
+    print(expected)
+    assert actual == expected
+
+
+def test_lsf2():
+    expected = """#BSUB -P account_name
+#BSUB -q batch
+#BSUB -W 00:01:00
+#BSUB -R affinity[core(1)]
+#BSUB -R span[ptile=12]
+#BSUB -n 12"""
+
+    props = {
+        "scheduler": "lsf",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 1,
+        "tasks_per_node": 12,
+        "threads": 1,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    assert actual == expected
+
+
+def test_lsf3():
+    expected = """#BSUB -P account_name
+#BSUB -q batch
+#BSUB -W 00:01:00
+#BSUB -R affinity[core(1)]
+#BSUB -R span[ptile=6]
+#BSUB -n 12"""
+
+    props = {
+        "scheduler": "lsf",
+        "account": "account_name",
+        "queue": "batch",
+        "walltime": "00:01:00",
+        "nodes": 2,
+        "tasks_per_node": 6,
+        "threads": 1,
+    }
+
+    js = JobScheduler.get_scheduler(props)
+    actual = js.job_card.content()
+
+    print(actual)
+    print("***************")
+    print(expected)
     assert actual == expected
