535d8134ec881b551244afd996283aacc58091ef
Ryan Long <ryanlong1004@gmail.com>
2022-05-26 19:05:32 +0000

    update tests

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -468,5 +468,7 @@ def test_string_output():
     }
 
     js = JobScheduler.get_scheduler(props)
-    actual = js.job_card.content()
+    jc = js.job_card
+    actual = str(jc)
+    print(jc)
     assert actual == expected
