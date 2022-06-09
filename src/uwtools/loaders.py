2a7efeb372e1c3a179cfe11c367d52f2c0329e30
Terry McGUinness <terrene.mcguinness@gmail.com>
2022-06-09 16:31:53 -0400

    moved in template,yaml_file, and nice_dict only to get needed capablities without loading solo

--- src/uwtools/loaders.py
+++ src/uwtools/loaders.py
@@ -4,23 +4,33 @@ Loads yaml configuration files as python objects
 https://pyyaml.org/wiki/PyYAMLDocumentation
 """
 
-import collections
-import pathlib
-from typing import Any, Dict
-from yaml import load
+import os
 
-try:
-    from yaml import CLoader as Loader
-except ImportError:
-    from yaml import Loader
+from uwtools.nice_dict import NiceDict
+from uwtools.template import Template, TemplateConstants
+from uwtools.yaml_file import YAMLFile
 
+class Configure(NiceDict):
 
-class Config(collections.UserDict):
-    def __getattr__(self, name):
-        return self.__dict__["data"][name]
+    """
+        A class that reads a yaml file and checks the contents against a schema using the language
+    """
 
+    def __init__(self, config_file):
+        super().__init__()
+        self.source = os.path.abspath(config_file)
+        if config_file is not None:
+            config = self.load_yaml(config_file)
+            if config is None:
+                config = {}
+            self.update(config)
 
-def load_yaml(_path: pathlib.Path):
-    with open(_path, "r") as _file:
-        props = load(_file, Loader=Loader)
-        return Config(props)
+    def load_yaml(self,config_file=None,data=None):
+        if config_file is not None:
+            config = YAMLFile(os.path.abspath(config_file))
+        else:
+            config = data
+        config = Template.substitute_structure_from_environment(config)
+        config = Template.substitute_structure( config, TemplateConstants.DOLLAR_PARENTHESES, self.get)
+        config = Template.substitute_with_dependencies(config,config,TemplateConstants.DOLLAR_PARENTHESES,shallow_precedence=False)
+        self.update(config)
---
+++ src/uwtools/yaml_file.py
@@ -0,0 +1,54 @@
+import yaml
+from .nice_dict import NiceDict
+from .basic import traverse_structure
+
+def stringify(structure):
+    """
+    Converts all basic elements of a structure into strings
+    """
+    return traverse_structure(structure, visitor)
+
+def visitor(value):
+    if isinstance(value, bool):
+        return value
+    return str(value)
+
+class YAMLFile(NiceDict):
+
+    """
+        Reads a YAML file as a NiceDict and recursively converts
+        nested dictionaries into NiceDict.
+        Provides a way of saving the dictionary issued from the yaml file, after modification or not.
+    """
+
+    def __init__(self, config_file=None, data=None):
+        super().__init__()
+        if config_file is not None:
+            with open(config_file) as f:
+                config = yaml.load(f, Loader=yaml.BaseLoader)
+        else:
+            config = data
+        if config is not None:
+            self.update(self._configure(config))
+
+    def _configure(self, config):
+        for key, value in config.items():
+            if isinstance(value, dict):
+                config[key] = NiceDict(value)
+                self._configure(value)
+            elif isinstance(value, list):
+                for i, v in enumerate(value):
+                    if isinstance(v, dict):
+                        value[i] = NiceDict(v)
+                        self._configure(v)
+        return config
+
+    def save(self, target):
+        with open(target, 'w') as f:
+            # specifies a wide file so that long strings are on one line.
+            yaml.dump(stringify(self), f, width=100000)
+        return target
+
+    def dump(self):
+        return yaml.dump(stringify(self), width=100000)
+
