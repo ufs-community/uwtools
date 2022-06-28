778baedae62bb522e0c7c61e545cbe3199bb62aa
Terry McGUinness <terrene.mcguinness@gmail.com>
2022-06-28 17:14:56 -0400

    added a couple of PEP8 required doc blocks in pytest functions for YAML loader

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -8,6 +8,7 @@ from uwtools.loaders import load_yaml
 uwtools_file_base = os.path.join(os.path.dirname(__file__))
 
 def test_yaml_loader_loads_correctly():
+    '''Simple test to load a YAML file with basic value pairs of various types and check its results'''
     actual = load_yaml(os.path.join(uwtools_file_base,pathlib.Path("fixtures/simple.yaml")))
 
     expected = {
@@ -23,6 +24,7 @@ def test_yaml_loader_loads_correctly():
     assert actual == expected
 
 def test_loader_dot_notation():
+    '''Test to check the dot shortcut for resolving a dictionary element works in the response from YAML loader'''
 
      props = load_yaml(os.path.join(uwtools_file_base,pathlib.Path("fixtures/simple.yaml")))
 
