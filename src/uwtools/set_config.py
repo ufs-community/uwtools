b0d8adf66be35c31fa5c3762522c45627de0032a
J-Derrico <jama1091@colorado.edu>
2023-02-10 12:20:15 -0700

    Adding set_config.py

---
+++ src/uwtools/set_config.py
@@ -0,0 +1,100 @@
+import os
+import argparse
+import pathlib
+from uwtools import config
+from uwtools import Logger
+
+
+def path_if_file_exists(arg):
+    '''Checks whether a file exists, and returns the path if it does.'''
+    if not os.path.exists(arg):
+        msg = f'{arg} does not exist!'
+        raise argparse.ArgumentTypeError(msg)
+    return arg
+
+def get_file_type (arg):
+    '''Gets the file type from the path'''
+    return pathlib.Path(path_if_file_exists(arg)).suffix
+
+def parse_args(argv):
+
+    '''
+    Function maintains the arguments accepted by this script. Please see
+    Python's argparse documentation for more information about settings of each
+    argument.
+    '''
+
+    parser = argparse.ArgumentParser(
+       description='Set config with user-defined settings.'
+    )
+
+    parser.add_argument(
+        '-i', '--input_base_file',
+        help='Path to a config base file.',
+        required=True,
+        type=path_if_file_exists,
+        )
+        
+    parser.add_argument(
+        '-o', '--outfile',
+        help='Full path to output file',
+        )
+
+    parser.add_argument(
+        '-c', '--config_file',
+        help='Optional path to configuration file',
+        type=path_if_file_exists,
+    )
+
+    parser.add_argument(
+        '-d', '--dry_run',
+        action='store_true',
+        help='If provided, print rendered config file to stdout only',
+    )
+    return parser.parse_args(argv)
+
+
+def create_config_obj (argv):
+    '''Main section for processing config file'''
+    user_args = parse_args(argv)
+
+    # check input type
+    file_type = get_file_type(user_args.input_base_file)
+
+    if file_type in ["yaml", "yml"]:
+        config_obj = config.YAMLConfig(user_args.input_base_file)
+
+    elif file_type in ["bash", "sh", "ini", "IN"]:
+        config_obj = config.INIConfig (user_args.input_base_file)
+
+    elif file_type == "nml":
+        config_obj = config.F90Config (user_args.input_base_file)
+
+    # If user provided -c config, load it and update config_obj
+    if user_args.config_file:
+        config_file_type = get_file_type(user_args.config_file)
+
+        if config_file_type in ["yaml", "yml"]:
+            user_config_obj = config.YAMLConfig(user_args.config_file)
+
+        elif config_file_type in ["bash", "sh", "ini", "IN"]:
+            user_config_obj = config.INIConfig (user_args.config_file)
+
+        elif config_file_type == "nml":
+            user_config_obj = config.F90Config (user_args.config_file)
+
+        config_obj.update_values(user_config_obj)
+
+    if user_args.outfile:
+        config_obj.dump_file(user_args.outfile)
+
+    # if user_args.dry_run:
+    #     if user_args.outfile:
+    #         log.info(r"warning file {outfile} ".format(outfile=user_args.outfile),
+    #              r"not written when using --dry_run")
+    #         # apply switch to allow user to view the results of rendered template
+    #         # instead of writing to disk
+    #         log.info(config_obj)
+    #     else:
+    #         config_obj.dump_file(user_args.outfile)
+
