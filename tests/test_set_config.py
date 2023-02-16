1f5e037bad2a4ac0339150b596f03f7edfce5037
j-derrico <105665462+j-derrico@users.noreply.github.com>
2023-02-16 10:18:59 -0700

    UW 203 User interface for handling config files (#153)
    The config parsing tool (probably set_config.py) needs a command line interface for user interaction. It should provide all the standard options as outlined in the Toolbox MVP and should aim at achieving the following behaviors:
    
    -i to accept an input "base" config file (required)
    -o to write a rendered output file (optional)
    -c to accept a user config file (optional)
    These files will need some way of knowing what type of file we give it. As a first pass, I think that determining type from the suffix is sufficient. We'll likely want a better solution at a future date.

---
+++ tests/test_set_config.py
@@ -0,0 +1,230 @@
+#pylint: disable=unused-variable
+
+'''
+Tests for set_config tool
+'''
+
+import argparse
+import pathlib
+import os
+import tempfile
+import io
+from contextlib import redirect_stdout
+import pytest
+from uwtools import config
+from uwtools import set_config
+
+
+uwtools_file_base = os.path.join(os.path.dirname(__file__))
+
+def compare_files(expected, actual):
+    '''Compare the content of two files.  Doing this over filecmp.cmp since 
+    we may not be able to handle end-of-file character differences with it.
+    Prints the contents of two compared files to std out if they do not match.'''
+
+    with open(expected, 'r', encoding='utf-8') as expected_file:
+        expected_content = expected_file.read().rstrip('\n')
+    with open(actual, 'r', encoding='utf-8') as actual_file:
+        actual_content = actual_file.read().rstrip('\n')
+
+    if expected_content != actual_content:
+        print('The expected file looks like:')
+        print(expected_content)
+        print('*' * 80)
+        print('The rendered file looks like:')
+        print(actual_content)
+        return False
+    return True
+
+def test_path_if_file_exists():
+    '''Make sure the function works as expected.  It is used as a type in 
+    argparse, so raises an argparse exception when the user provides a 
+    non-existant path.'''
+
+    with tempfile.NamedTemporaryFile(dir='.', mode='w') as tmp_file:
+        assert set_config.path_if_file_exists(tmp_file.name)
+
+    with pytest.raises(argparse.ArgumentTypeError):
+        not_a_filepath = './no_way_this_file_exists.nope'
+        set_config.path_if_file_exists(not_a_filepath)
+
+def test_set_config_yaml_simple():
+    ''' Test that providing a YAML base file with necessary settings 
+    will create a YAML config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple2.yaml"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dir:
+
+        out_file = f'{tmp_dir}/test_config_from_yaml.yaml'
+        args = ['-i', input_file, '-o', out_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.YAMLConfig(input_file)
+        expected_file = f'{tmp_dir}/expected_yaml.yaml'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_ini_simple():
+    ''' Test that providing a basic INI file with necessary settings 
+    will create an INI config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple.ini"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dr:
+        out_file = f'{tmp_dr}/test_config_from_ini.ini'
+        args = ['-i', input_file, '-o', out_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.INIConfig(input_file)
+        expected_file = f'{tmp_dr}/expected_ini.ini'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_f90nml_simple():
+    '''Test that providing basic f90nml file with necessary settings
+    will create f90nml config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple.nml"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dr:
+
+        out_file = f'{tmp_dr}/test_config_from_nml.nml'
+        args = ['-i', input_file, '-o', out_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.F90Config(input_file)
+        expected_file = f'{tmp_dr}/expected_nml.nml'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_bash_simple():
+    '''Test that providing bash file with necessary settings will 
+    create an INI config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple.sh"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dr:
+
+        out_file = f'{tmp_dr}/test_config_from_bash.ini'
+
+        args = ['-i', input_file, '-o', out_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.INIConfig(input_file)
+        expected_file = f'{tmp_dr}/expected_ini.ini'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_yaml_config_file():
+    '''Test that providing a yaml base input file and a config file will
+    create and update yaml config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/fruit_config.yaml"))
+    config_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/fruit_config_similar.yaml"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dir:
+
+        out_file = f'{tmp_dir}/test_config_from_yaml.yaml'
+        args = ['-i', input_file, '-o', out_file, '-c', config_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.YAMLConfig(input_file)
+        config_file_obj = config.YAMLConfig(config_file)
+        expected.update_values(config_file_obj)
+        expected_file = f'{tmp_dir}/expected_yaml.yaml'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_f90nml_config_file():
+    '''Test that providing a F90nml base input file and a config file will
+    create and update F90nml config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple.nml"))
+    config_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple2.nml"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dir:
+
+        out_file = f'{tmp_dir}/test_config_from_nml.nml'
+        args = ['-i', input_file, '-o', out_file, '-c', config_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.F90Config(input_file)
+        config_file_obj = config.F90Config(config_file)
+        expected.update_values(config_file_obj)
+        expected_file = f'{tmp_dir}/expected_nml.nml'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_ini_config_file():
+    '''Test that aproviding INI base input file and a config file will 
+    create and update INI config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple.ini"))
+    config_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple2.ini"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dir:
+
+        out_file = f'{tmp_dir}/test_config_from_ini.ini'
+        args = ['-i', input_file, '-o', out_file, '-c', config_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.INIConfig(input_file)
+        config_file_obj = config.INIConfig(config_file)
+        expected.update_values(config_file_obj)
+        expected_file = f'{tmp_dir}/expected_ini.ini'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_set_config_ini_bash_config_file():
+    '''Test that aproviding INI base input file and a config file will 
+    create and update INI config file'''
+
+    input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/simple.ini"))
+    config_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/fruit_config.sh"))
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dir:
+
+        out_file = f'{tmp_dir}/test_config_from_ini.ini'
+        args = ['-i', input_file, '-o', out_file, '-c', config_file]
+
+        set_config.create_config_obj(args)
+
+        expected = config.INIConfig(input_file)
+        config_file_obj = config.INIConfig(config_file)
+        expected.update_values(config_file_obj)
+        expected_file = f'{tmp_dir}/expected_ini.ini'
+        expected.dump_file(expected_file)
+
+        assert compare_files(expected_file, out_file)
+
+def test_incompatible_file_type():
+    '''Test that providing an incompatible file type for input base file will 
+    return print statement'''
+
+    with tempfile.TemporaryDirectory(dir='.') as tmp_dir:
+
+        input_file = os.path.join(uwtools_file_base, pathlib.Path("fixtures/model_configure.sample"))
+        args = ['-i', input_file]
+
+        outstring = io.StringIO()
+        with redirect_stdout(outstring):
+            set_config.create_config_obj(args)
+        result = outstring.getvalue()
+        outcome = "Set config failure: bad file type\n"
+
+        assert result == outcome
