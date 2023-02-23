7faf93fed474bdb0a6845b12efc8a781146f2146
Brian Weir <94982354+WeirAE@users.noreply.github.com>
2023-02-23 17:25:54 -0600

    Notable updates move the primary usage for FieldTableConfig under set_config.py to use existing args and other functionality

--- src/uwtools/set_config.py
+++ src/uwtools/set_config.py
@@ -39,12 +39,22 @@ def parse_args(argv):
         help='Path to a config base file.',
         required=True,
         type=path_if_file_exists,
-        )
+    )
+
+    parser.add_argument(
+        '--in_file_type',
+        help='Optional input file type',
+    )
 
     parser.add_argument(
         '-o', '--outfile',
         help='Full path to output file',
-        )
+    )
+
+    parser.add_argument(
+        '--out_file_type',
+        help='Optional output file type override',
+    )
 
     parser.add_argument(
         '-c', '--config_file',
@@ -53,9 +63,20 @@ def parse_args(argv):
     )
 
     parser.add_argument(
+        '--config_file_type',
+        help='Optional config file type override',
+    )
+
+    parser.add_argument(
         '-d', '--dry_run',
         action='store_true',
         help='If provided, print rendered config file to stdout only',
+    )
+
+    parser.add_argument(
+        '--values_needed',
+        action='store_true',
+        help='If provided, print a list of required configuration settings to stdout',
     )
     return parser.parse_args(argv)
 
@@ -64,25 +85,27 @@ def create_config_obj(argv):
     '''Main section for processing config file'''
 
     user_args = parse_args(argv)
-    file_type = get_file_type(user_args.input_base_file)
+    infile_type = user_args.in_file_type or get_file_type(user_args.input_base_file)
 
-    if file_type in [".yaml", ".yml"]:
+    if infile_type in [".yaml", ".yml"]:
         config_obj = config.YAMLConfig(user_args.input_base_file)
 
-    elif file_type in [".bash", ".sh", ".ini", ".IN"]:
+    elif infile_type in [".bash", ".sh", ".ini", ".IN"]:
         config_obj = config.INIConfig(user_args.input_base_file)
 
-    elif file_type == ".nml":
+    elif infile_type == ".nml":
         config_obj = config.F90Config(user_args.input_base_file)
+
+    elif infile_type == "field_table":
+        config_obj = config.FieldTableConfig()
 
     else:
         print("Set config failure: bad file type")
 
-
     if user_args.config_file:
-        config_file_type = get_file_type(user_args.config_file)
+        config_file_type = user_args.config_file_type or get_file_type(user_args.config_file)
 
-        if config_file_type in [".yaml", ".yml"]:
+        if config_file_type in [".yaml", ".yml", "YML", "field_table"]:
             user_config_obj = config.YAMLConfig(user_args.config_file)
 
         elif config_file_type in [".bash", ".sh", ".ini", ".IN"]:
@@ -94,7 +117,13 @@ def create_config_obj(argv):
         config_obj.update_values(user_config_obj)
 
     if user_args.outfile:
-        config_obj.dump_file(user_args.outfile)
+        outfile_type = user_args.out_file_type or pathlib.Path(user_args.outfile).suffix
+        if outfile_type == "field_table":
+            out_object = config.FieldTableConfig()
+            out_object.update(config_obj)
+        else:
+            out_object = config_obj
+        out_object.dump_file(user_args.outfile)
 
 if __name__ == '__main__':
     create_config_obj(sys.argv[1:])
