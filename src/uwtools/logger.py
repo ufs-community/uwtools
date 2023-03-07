cf2568e373d4265aff8bec95249dc2439f8c719c
WeirAE <weirbs@gmail.com>
2023-03-07 16:02:53 -0600

    Log decorator added to logger.py Pending test implementation and further details

--- src/uwtools/logger.py
+++ src/uwtools/logger.py
@@ -3,9 +3,12 @@ Logger
 """
 
 import sys
+import os
 from pathlib import Path
 from typing import Union, List
 import logging
+import functools
+from inspect import getframeinfo, stack
 
 
 class ColoredFormatter(logging.Formatter):
@@ -151,3 +154,50 @@ class Logger:
         handler.setFormatter(logging.Formatter(_format))
 
         return handler
+
+    def log_decorator(self, _func=None):
+        """
+        Add a logger decorator.
+        This decorator will allow setting custom logging details on a given function
+        Decorator based on work by Hima Mahajan https://github.com/hima03/log-decorator
+        """
+        def log_decorator_info(func):
+            @functools.wraps(func)
+            def log_decorator_wrapper(self, *args, **kwargs):
+                #Build logger object
+                logger_obj = Logger.get_logger(self)
+
+                # Create a list of the positional arguments passed to function.
+                # Using repr() for string representation for each argument. repr() is similar to str() with the only
+                # difference being it prints with a pair of quotes and if we calculate a value we get more
+                # precise value than str().
+                args_passed_in_function = [repr(a) for a in args]
+                # Create a list of the keyword arguments. The f-string formats each argument as key=value, where the !r
+                # specifier means that repr() is used to represent the value.
+                kwargs_passed_in_function = [f"{k}={v!r}" for k, v in kwargs.items()]
+
+                # The lists of positional and keyword arguments is joined together to form final string
+                formatted_arguments = ", ".join(args_passed_in_function + kwargs_passed_in_function)
+
+                # Generate file name and function name for calling function. __func.name__ will give the name of the
+                # caller function ie. wrapper_log_info and caller file name ie log-decorator.py
+                #- In order to get actual function and file name we will use 'extra' parameter.
+                #- To get the file name, we are using in-built module inspect.getframeinfo which returns
+                # calling file name
+                py_file_caller = getframeinfo(stack()[1][0])
+                extra_args = { 'func_name_override': func.__name__,
+                           'file_name_override': os.path.basename(py_file_caller.filename) }
+
+                # Before the function execution, log function details."""
+                logger_obj.info(f"Arguments: {formatted_arguments} - Begin function")
+                try:
+                    #log return value from the function
+                    value = func(self, *args, **kwargs)
+                    logger_obj.info(f"Returned: - End function {value!r}", extra=extra_args)
+                except:
+                    #log exception if occurs in function
+                    logger_obj.error(f"Exception: {str(sys.exc_info()[1])}", extra=extra_args)
+                    raise
+                return value
+            return log_decorator_wrapper
+        return log_decorator_info if _func is None else log_decorator_info(_func)
