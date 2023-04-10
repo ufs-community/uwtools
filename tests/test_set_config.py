94e3b404ba7d604996c3ed5b87d3be5d805b376a
Christina Holt <christina.holt@noaa.gov>
2023-04-10 09:14:09 -0600

    Typo

--- tests/test_set_config.py
+++ tests/test_set_config.py
@@ -529,7 +529,7 @@ def test_compare_nml(): #pylint: disable=unused-variable
     # A very rough estimate is that there is a word/colon set followed
     # by a -/+ set
     # This regex is meant to match the lines in the expected string
-    # above that give us the setting, key value diffs like this:
+    # above that give us the section, key value diffs like this:
     #   config:       vegetable:  - eggplant + peas
     pattern = re.compile(r'(\w):\s+(\w+):\s+-\s+(\w+)\s+\+\s+(\w+)')
     for result_line in result.split('\n'):
