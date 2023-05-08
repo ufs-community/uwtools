59400a59d9383e99cc14d55332a362818ee82512
Venita Hagerty <venita.hagerty@noaa.gov>
2023-05-08 13:22:05 -0600

    Added comments to test

--- tests/drivers/test_forecast.py
+++ tests/drivers/test_forecast.py
@@ -13,14 +13,15 @@ def test_create_directory_structure():
     """Tests create_directory_structure method given a directory."""
     forecast_obj = FV3Forecast(Driver)
     uwtools_file_base = os.path.join(os.path.dirname(__file__))
-    print("base: ", uwtools_file_base)
     run_directory = os.path.join(uwtools_file_base, "expt_dir")
     if os.path.isdir(run_directory):
         shutil.rmtree(run_directory)
 
+    # Test create_directory_structure when run directory does not exist
     forecast_obj.create_directory_structure(run_directory, "delete")
     assert os.path.isdir(os.path.join(run_directory, "RESTART"))
 
+    # Test create_directory_structure when run directory does exist
     forecast_obj.create_directory_structure(run_directory, "rename")
     copy_directory = glob.glob(run_directory + "_*")[0]
     assert os.path.isdir(os.path.join(copy_directory, "RESTART"))
