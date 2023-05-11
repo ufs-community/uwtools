a4e6010b476e950e544029b0a8d6023ddf317d17
Venita Hagerty <38571614+venitahagerty@users.noreply.github.com>
2023-05-11 08:54:27 -0600

    UW-12 Create correct directory structure (#234)
    * Added code for create_run_directory
    
    * Fixed bug in directory rename
    
    * Improved directory rename
    
    * Added creation verification
    
    * Correcting linting errors
    
    * Removed app name from forecast class and minor changes
    
    * Fixed os.path.join misuse
    
    * Two verifications, and a new method
    
    * Fixed forecast.py, added test
    
    * Fixed linting error in forecast.py
    
    * Added test for rename when existing directory
    
    * Added comments to test
    
    * Removed whitespace in blank line
    
    * Added 2 more tests for create_directory_structure
    
    * Fixed lint errors with creating test file
    
    * Removed some pylint disables, using temp dir
    
    * Fixed some linting errors
    
    * Changed how temporary directory is created
    
    * Added code to clean up after rename test
    
    * Fixed linting error

--- src/uwtools/drivers/forecast.py
+++ src/uwtools/drivers/forecast.py
@@ -6,9 +6,17 @@ This file contains the forecast drivers for a variety of apps
 and physics suites.
 '''
 
+import logging
+import os
+import sys
+
+from uwtools.utils import file_helpers
 from .driver import Driver
 
-class SRWFV3Forecast(Driver): # pragma: no cover
+logging.getLogger(__name__)
+
+
+class FV3Forecast(Driver): # pragma: no cover
     #remove pragma when completed
 
     '''
@@ -30,7 +38,8 @@ class SRWFV3Forecast(Driver): # pragma: no cover
 
     def resources(self):
 
-        ''' Determine necessary task objects and fill in resource requirements of each.
+        ''' Determine necessary task objects and fill in resource
+            requirements of each.
          Returns a Config object containing the HPC resources needed.'''
 
     def validate(self):
@@ -40,33 +49,74 @@ class SRWFV3Forecast(Driver): # pragma: no cover
 
     def create_model_config(self):
 
-        ''' Collects all the user inputs required to create a model config 
+        ''' Collects all the user inputs required to create a model config
         file, calling the existing model config tools. This will be unique
         to the app being run and will appropriately parse subsequent stages
-        of the workflow. 
-        Defaults will be filled in if not provided by the user. Equivalent 
+        of the workflow.
+        Defaults will be filled in if not provided by the user. Equivalent
         references to config_default.yaml or config.community.yaml from SRW
         will need to be made for the other apps. '''
 
     def stage_fix_files(self):
 
-        ''' Holds the knowledge for how to modify a list of fix files and 
-        stages them in the working directory. Likely gets all its info from 
-        config_obj. Calls data mover tool (could be python copy). Fix files 
+        ''' Holds the knowledge for how to modify a list of fix files and
+        stages them in the working directory. Likely gets all its info from
+        config_obj. Calls data mover tool (could be python copy). Fix files
         usually are specific to a given named grid and resolution. '''
 
     def create_namelist(self):
-        ''' Collects all the user inputs required to create a namelist 
+        ''' Collects all the user inputs required to create a namelist
         file, calling the existing namelist config tools. For SRW,
         this will take the fix files and model_config previously created to
         to generate config.yaml'''
 
+    def create_directory_structure(self, run_directory, exist_act="delete"):
+        ''' Collects the name of the desired run directory, and has an
+        optional flag for what to do if the run directory specified already
+        exists. Creates the run directory and adds subdirectories
+        INPUT and RESTART. Verifies creation of all directories.
+
+        Args:
+           run_directory: path of desired run directory
+           exist_act: - could be any of 'delete', 'rename', 'quit'
+                      - how program should act if run directory exists
+                      - default is to delete old run directory
+           Returns: None
+        '''
+
+        # Caller should only provide correct argument
+        if exist_act not in ["delete", "rename", "quit"]:
+            raise ValueError("Bad argument to create_directory_structure")
+
+        # Exit program with error if caller chooses to quit
+        if exist_act == "quit" and os.path.isdir(run_directory):
+            logging.critical("User chose quit option when creating directory")
+            sys.exit(1)
+
+        # Delete or rename directory if it exists
+        file_helpers.handle_existing(run_directory, exist_act)
+
+        # Create new run directory with two required subdirectories
+        try:
+            for subdir in ("INPUT", "RESTART"):
+                # Create and verify new directory with subdirectories
+                os.makedirs(os.path.join(run_directory, subdir))
+                if not os.path.isdir(os.path.join(run_directory, subdir)):
+                    msg = f"Directory {run_directory} with {subdir} not created"
+                    logging.critical(msg)
+                    raise RuntimeError(msg)
+            msg = f"Directory {run_directory} created with subdirectories"
+            logging.info(msg)
+        except (RuntimeError, FileExistsError) as create_error:
+            msg = f"Could not create directory {run_directory} with subdirectories"
+            raise RuntimeError(msg) from create_error
+
     def output(self):
-        ''' Create list of SRW output files and stage them in the working 
+        ''' Create list of SRW output files and stage them in the working
         directory.'''
 
     def job_card(self):
-        ''' Turns the resources config object into a batch card for the 
+        ''' Turns the resources config object into a batch card for the
         configured Task.'''
 
     def run(self):
---
+++ tests/drivers/test_forecast.py
@@ -0,0 +1,50 @@
+'''
+Tests for forecast driver
+'''
+#pylint: disable=unused-variable
+
+import os
+import shutil
+import glob
+import tempfile
+import pytest
+
+from uwtools.drivers.driver import Driver
+from uwtools.drivers.forecast import FV3Forecast
+
+
+def test_create_directory_structure():
+    """Tests create_directory_structure method given a directory."""
+    forecast_obj = FV3Forecast(Driver)
+
+    with tempfile.TemporaryDirectory() as run_directory:
+        # Test create_directory_structure when run directory does not exist
+        forecast_obj.create_directory_structure(run_directory, "delete")
+        assert os.path.isdir(os.path.join(run_directory, "RESTART"))
+
+        # Put a test file into the run directory
+        test_file = os.path.join(run_directory, "test.txt")
+        with open(test_file, "w", encoding="utf-8") as file_open:
+            file_open.write("test file")
+            file_open.close()
+
+        # Test create_directory_structure when run directory does exist
+        forecast_obj.create_directory_structure(run_directory, "delete")
+        assert os.path.isdir(os.path.join(run_directory, "RESTART"))
+        # Test file should be gone after delete
+        assert not os.path.isfile(test_file)
+
+        # Test create_directory_structure when run directory does exist
+        forecast_obj.create_directory_structure(run_directory, "rename")
+        copy_directory = glob.glob(run_directory + "_*")[0]
+        assert os.path.isdir(os.path.join(copy_directory, "RESTART"))
+
+        # Clean up copied directory fronm rename
+        if os.path.isdir(copy_directory):
+            shutil.rmtree(copy_directory)
+
+        # Test create_directory_structure when run directory does exist
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            forecast_obj.create_directory_structure(run_directory, "quit")
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 1
