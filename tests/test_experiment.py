8709f24f58107d37d276af5ea2b7426d9f8eb423
WeirAE <weirbs@gmail.com>
2023-06-02 10:18:04 -0500

    fixed tests

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,21 +1,28 @@
 '''
 Set of tests for wrapping experiments with a facade
 '''
-#pylint: disable=unused-variable, consider-using-f-string, duplicate-code
+#pylint: disable=undefined-variable, unused-variable, unused-argument, unused-import
+import os
+import pytest
 
 uwtools_file_base = os.path.join(os.path.dirname(__file__))
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_load_config():
     '''Test that YAML load, update, and dump work with a basic YAML file. '''
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_validate_config():
     '''Test that the YAML file is validated correctly. '''
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_create_experiment():
     '''Test that the experiment directory and manager files are created. '''
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_create_manager():
     '''Test that the manager files are created. '''
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_link_fix_files():
     '''Test that the fix files are linked. '''
