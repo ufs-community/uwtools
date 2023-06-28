a4e068b5f11613273d814314cbd94e40d48cf412
Paul Madden <paul.madden@noaa.gov>
2023-06-28 15:14:17 +0000

    WIP on unit tests

--- src/uwtools/test/test_validators.py
+++ src/uwtools/test/test_validators.py
@@ -4,40 +4,34 @@ Tests for schema validation tool
 
 import io
 import json
-import os
-import pathlib
 from contextlib import redirect_stdout
 
 import jsonschema
 import pytest
 import yaml
 
-uwtools_file_base = os.path.join(os.path.dirname(__file__))
+from uwtools.test.support import fixture_posix
 
 
-@pytest.mark.skip()
 def test_validate_yaml_salad():
     """
     Test that simple salad schema is accepted as valid json schema and that it
-    validates both valid and bad input files
+    validates both valid and bad input files.
     """
-    schema_fn = os.path.join(
-        os.path.dirname(uwtools_file_base), pathlib.Path("schema/salad.jsonschema")
-    )
-    input_fn = os.path.join(uwtools_file_base, pathlib.Path("fixtures/fruit_config_similar.yaml"))
-    bad_input_fn = os.path.join(uwtools_file_base, pathlib.Path("fixtures/bad_fruit_config.yaml"))
+    schema_fn = fixture_posix("schema/salad.jsonschema")
+    input_fn = fixture_posix("fruit_config_similar.yaml")
+    bad_input_fn = fixture_posix("bad_fruit_config.yaml")
 
-    with open(schema_fn, "r", encoding="utf-8") as schema_file:
-        schema = json.load(schema_file)
-        with open(input_fn, encoding="utf-8") as input_file:
-            infile = yaml.safe_load(input_file)
-        outstring = io.StringIO()
-        with redirect_stdout(outstring):
-            jsonschema.validate(infile, schema)
-        result = outstring.getvalue()
-        assert result == ""
-
-        with open(bad_input_fn, encoding="utf-8") as bad_input_file:
-            bad_infile = yaml.safe_load(bad_input_file)
-        with pytest.raises(jsonschema.exceptions.ValidationError):
-            jsonschema.validate(bad_infile, schema)
+    with open(schema_fn, "r", encoding="utf-8") as f:
+        schema = json.load(f)
+    with open(input_fn, encoding="utf-8") as f:
+        infile = yaml.safe_load(f)
+    outstring = io.StringIO()
+    with redirect_stdout(outstring):
+        jsonschema.validate(infile, schema)
+    result = outstring.getvalue()
+    assert result == ""
+    with open(bad_input_fn, encoding="utf-8") as f:
+        bad_infile = yaml.safe_load(f)
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        jsonschema.validate(bad_infile, schema)
