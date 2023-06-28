6d6bf95b5a9fec43239af76c00f78cba9bd8b88a
Paul Madden <paul.madden@noaa.gov>
2023-06-28 15:42:48 +0000

    WIP on unit tests

--- src/uwtools/test/test_validators.py
+++ src/uwtools/test/test_validators.py
@@ -1,37 +1,35 @@
+# pylint: disable=missing-function-docstring,redefined-outer-name
+
 """
 Tests for schema validation tool
 """
 
-import io
 import json
-from contextlib import redirect_stdout
 
 import jsonschema
-import pytest
 import yaml
+from pytest import fixture, raises
 
 from uwtools.test.support import fixture_posix
 
 
-def test_validate_yaml_salad():
-    """
-    Test that simple salad schema is accepted as valid json schema and that it
-    validates both valid and bad input files.
-    """
-    schema_fn = fixture_posix("schema/salad.jsonschema")
-    input_fn = fixture_posix("fruit_config_similar.yaml")
-    bad_input_fn = fixture_posix("bad_fruit_config.yaml")
+def validate(subpath, schema):
+    with open(fixture_posix(subpath), "r", encoding="utf-8") as f:
+        jsonschema.validate(yaml.safe_load(f), schema)
 
-    with open(schema_fn, "r", encoding="utf-8") as f:
-        schema = json.load(f)
-    with open(input_fn, encoding="utf-8") as f:
-        infile = yaml.safe_load(f)
-    outstring = io.StringIO()
-    with redirect_stdout(outstring):
-        jsonschema.validate(infile, schema)
-    result = outstring.getvalue()
-    assert result == ""
-    with open(bad_input_fn, encoding="utf-8") as f:
-        bad_infile = yaml.safe_load(f)
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        jsonschema.validate(bad_infile, schema)
+
+@fixture(scope="module")
+def schema():
+    with open(fixture_posix("schema/salad.jsonschema"), "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def test_validate_bad_yaml(schema):
+    with raises(jsonschema.exceptions.ValidationError):
+        validate("bad_fruit_config.yaml", schema)
+
+
+def test_validate_good_yaml(schema, capsys):
+    validate("fruit_config_similar.yaml", schema)
+    assert capsys.readouterr().err == ""
+    assert capsys.readouterr().out == ""
