20450a04af5e12f4c4bda0299e629aeca8bbb0fb
Paul Madden <paul.madden@noaa.gov>
2023-06-29 22:36:23 +0000

    Inline J2 template in test

--- src/uwtools/test/test_template.py
+++ src/uwtools/test/test_template.py
@@ -9,39 +9,35 @@ from types import SimpleNamespace as ns
 from pytest import fixture, raises
 
 from uwtools.j2template import J2Template
-from uwtools.test.support import fixture_path
 
 
 @fixture
 def testdata():
     return ns(
         config={"greeting": "Hello", "recipient": "the world"},
-        message="Hello to the world",
-        vars={"greeting", "recipient"},
+        template="{{greeting}} to {{recipient}}",
     )
 
 
-def test_render_string(testdata):
-    # Render template from input string. Check that undeclared_variables returns
-    # expected items.
-    template = J2Template(testdata.config, template_str="{{greeting}} to {{recipient}}")
+def validate(template):
     assert template.configure_obj.get("greeting") == "Hello"
     assert template.configure_obj.get("recipient") == "the world"
-    assert template.render_template() == testdata.message
-    assert template.undeclared_variables == testdata.vars
-
-
-def test_render_file(testdata):
-    # Render template from input file. Check that undeclared_variables returns
-    # expected items.
-    template = J2Template(testdata.config, template_path=fixture_path("J2Template.IN"))
-    assert template.configure_obj.get("greeting") == "Hello"
-    assert template.configure_obj.get("recipient") == "the world"
-    assert template.render_template() == testdata.message
-    assert template.undeclared_variables == testdata.vars
+    assert template.render_template() == "Hello to the world"
+    assert template.undeclared_variables == {"greeting", "recipient"}
 
 
 def test_bad_args(testdata):
     # It is an error to pass in neither a template path or a template string.
     with raises(RuntimeError):
         J2Template(testdata.config)
+
+
+def test_render_file(testdata, tmp_path):
+    path = tmp_path / "template.j2"
+    with path.open("w", encoding="utf-8") as f:
+        print(testdata.template, file=f)
+    validate(J2Template(testdata.config, template_path=str(path)))
+
+
+def test_render_string(testdata):
+    validate(J2Template(testdata.config, template_str=testdata.template))
