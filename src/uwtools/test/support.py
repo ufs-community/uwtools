c20026c7be79ef17e7b84e90cf9866f79200b024
Paul Madden <paul.madden@noaa.gov>
2023-06-29 20:16:52 +0000

    Add docstrings to test/support module

--- src/uwtools/test/support.py
+++ src/uwtools/test/support.py
@@ -7,47 +7,118 @@ from pathlib import Path
 from typing import List
 
 
-def compare_files(expected: str, actual: str) -> bool:
+def compare_files(path1: str, path2: str) -> bool:
     """
-    Compare the content of two files. Prefer to filecmp.cmp when trailing
-    newlines should be ignored. Print the two files to stdout when they do not
-    match.
+    Determines whether the two given files are identical up to any number of
+    trailing newlines, which are ignored. Print the contents of both files when
+    they do not match.
+
+    Parameters
+    ----------
+    path1
+        Path to first file
+    path2
+        Path to second file
+
+    Returns
+    -------
+    A bool indicating whether or not the files match.
     """
-
-    with open(expected, "r", encoding="utf-8") as expected_file:
-        expected_content = expected_file.read().rstrip("\n")
-    with open(actual, "r", encoding="utf-8") as actual_file:
-        actual_content = actual_file.read().rstrip("\n")
-
-    if expected_content != actual_content:
-        print("The expected file looks like:")
-        print(expected_content)
+    with open(path1, "r", encoding="utf-8") as f:
+        content1 = f.read().rstrip("\n")
+    with open(path2, "r", encoding="utf-8") as f:
+        content2 = f.read().rstrip("\n")
+    if content1 != content2:
+        print("1st file looks like:")
+        print(content1)
         print("*" * 80)
-        print("The actual file looks like:")
-        print(actual_content)
+        print("2nd file looks like:")
+        print(content2)
         return False
     return True
 
 
 def fixture_pathobj(suffix: str = "") -> Path:
+    """
+    Returns a pathlib Path object to a test-fixture resource file.
+
+    Parameters
+    ----------
+    suffix
+        A subpath relative to the location of the unit-test fixture resource
+        files. The prefix path to the resources files is known to Python and
+        varies based on installation location.
+    """
     with resources.as_file(resources.files("uwtools.test.fixtures")) as prefix:
         path = prefix / suffix
     return path
 
 
 def fixture_path(suffix: str = "") -> str:
+    """
+    Returns a POSIX path to a test-fixture resource file.
+
+    Parameters
+    ----------
+    suffix
+        A subpath relative to the location of the unit-test fixture resource
+        files. The prefix path to the resources files is known to Python and
+        varies based on installation location.
+    """
     return fixture_pathobj(suffix).as_posix()
 
 
 def fixture_uri(suffix: str = "") -> str:
+    """
+    Returns a file:// URI path to a test-fixture resource file.
+
+    Parameters
+    ----------
+    suffix
+        A subpath relative to the location of the unit-test fixture resource
+        files. The prefix path to the resources files is known to Python and
+        varies based on installation location.
+    """
     return fixture_pathobj(suffix).as_uri()
 
 
 def line_in_lines(line: str, lines: List[str]) -> bool:
+    """
+    Determines whether the given line occurs, as a line-ending suffix, in any of
+    the lines in the given list. For example, the line "bar" is a line-ending
+    suffix of the line "[2023-06-29T14:06:34] foo", but not of the line
+    "foo bar".
+
+    Paramters
+    ---------
+    line:
+        The sought-for line
+    lines:
+        The lines to be checked for the line
+
+    Returns
+    -------
+    A bool indicating whether or not the line was found.
+    """
     return any(x for x in lines if re.match(r"^.*%s$" % re.escape(line), x))
 
 
 def msg_in_caplog_records(msg: str, records: List[LogRecord]) -> bool:
+    """
+    Determines whether the given message occurs in the given list of log
+    records.
+
+    Parameters
+    ----------
+    msg
+        The sought-for message
+    records
+        The log records to be checked for the message
+
+    Returns
+    -------
+    A bool indicating whether or not the message was found.
+    """
     for record in records:
         if msg == record.msg:
             return True
