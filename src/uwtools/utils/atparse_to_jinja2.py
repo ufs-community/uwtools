f2277d12c5e4aad47b504212ab59c3df58d6353b
Paul Madden <paul.madden@noaa.gov>
2023-07-04 23:10:51 +0000

    Add uwtools.utils.atparse_to_jinja2.py

---
+++ src/uwtools/utils/atparse_to_jinja2.py
@@ -0,0 +1,36 @@
+"""
+Utilities for rendering Jinja2 templates
+"""
+
+import re
+from typing import Optional
+
+
+def convert(input_template: str, outfile: Optional[str] = None, dry_run: bool = False) -> None:
+    """
+    Renders a Jinja2 template using user-supplied configuration options via YAML
+    or environment variables.
+    """
+    with open(input_template, "rt", encoding="utf-8") as atparsetemplate:
+        if dry_run:
+            for line in atparsetemplate:
+                print(_replace(line))
+        else:
+            with open(outfile, "wt", encoding="utf-8") as jinja2template:
+                for line in atparsetemplate:
+                    jinja2template.write(_replace(line))
+
+
+def _replace(atline: str) -> str:
+    """
+    Replace @[] with {{}} in a line of text.
+    """
+    while re.search(r"\@\[.*?\]", atline):
+        # Set maxsplits to 1 so only first @[ is captured.
+        before_atparse = atline.split("@[", 1)[0]
+        within_atparse = atline.split("@[")[1].split("]")[0]
+        # Set maxsplits to 1 so only first ] is captured, which should be the
+        # bracket closing @[.
+        after_atparse = atline.split("@[", 1)[1].split("]", 1)[1]
+        atline = "".join([before_atparse, "{{", within_atparse, "}}", after_atparse])
+    return atline
