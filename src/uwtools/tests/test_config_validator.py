44cb924ea37751a518956aee6271d584fb88402c
Paul Madden <paul.madden@noaa.gov>
2023-07-05 23:10:24 +0000

    Work on 1:1 tests

--- src/uwtools/tests/test_config_validator.py
+++ src/uwtools/tests/test_config_validator.py
@@ -1,16 +1,33 @@
 # pylint: disable=missing-function-docstring,redefined-outer-name
-
 """
-Tests for schema validation tool
+Tests for uwtools.config_validator module
 """
-
 import json
 
 import jsonschema
+import pytest
 import yaml
 from pytest import fixture, raises
 
+from uwtools import config_validator
+from uwtools.logger import Logger
 from uwtools.tests.support import fixture_path
+
+
+@pytest.mark.parametrize("vals", [("good", True), ("bad", False)])
+def test_confid_is_valid_good(vals):
+    """
+    Test that a valid config file succeeds validation.
+    """
+    cfgtype, boolval = vals
+    assert (
+        config_validator.config_is_valid(
+            config_file=fixture_path(f"schema_test_{cfgtype}.yaml"),
+            validation_schema=fixture_path("schema/workflow.jsonschema"),
+            log=Logger(),
+        )
+        is boolval
+    )
 
 
 def validate(subpath, schema):
