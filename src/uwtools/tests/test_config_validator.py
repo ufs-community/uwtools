7521c8426077c2462760a21f4abf2a95d8c23c36
Paul Madden <paul.madden@noaa.gov>
2023-07-06 21:42:12 +0000

    Move workflow.jsonschema to uwtools.resources

--- src/uwtools/tests/test_config_validator.py
+++ src/uwtools/tests/test_config_validator.py
@@ -3,6 +3,7 @@
 Tests for uwtools.config_validator module
 """
 import json
+from importlib import resources
 
 import jsonschema
 import pytest
@@ -20,10 +21,12 @@ def test_confid_is_valid_good(vals):
     Test that a valid config file succeeds validation.
     """
     cfgtype, boolval = vals
+    with resources.as_file(resources.files("uwtools.resources")) as path:
+        schema = (path / "workflow.jsonschema").as_posix()
     assert (
         config_validator.config_is_valid(
             config_file=fixture_path(f"schema_test_{cfgtype}.yaml"),
-            validation_schema=fixture_path("schema/workflow.jsonschema"),
+            validation_schema=schema,
             log=Logger(),
         )
         is boolval
