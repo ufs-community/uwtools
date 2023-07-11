c3ebfc497ec89330c365b5e3a13a01778363ef18
Paul Madden <paul.madden@noaa.gov>
2023-07-11 14:59:51 +0000

    Define supported Python versions

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ set -eux
 python -m pip install -vv .
 # Copy files needed during test phase:
 dst=../test_files
+rm -fr $dst
 mkdir -pv $dst
 mv -v $PKG_NAME $dst/
 cp -v $SRC_DIR/pyproject.toml $dst/
