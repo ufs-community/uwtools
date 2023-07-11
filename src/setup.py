b6e6e31ad07bba1ca7cbad5c4edc738231b93d97
Paul Madden <paul.madden@noaa.gov>
2023-07-11 02:26:59 +0000

    Add explanatory note to setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,15 @@
 """
 Basic setuptools configuration
+
+See https://setuptools.pypa.io/en/latest/userguide/quickstart.html#basic-use for
+information on this standard setuptools file.
+
+In this codebase, it is used 1. By conda-build to install the uwtools files into
+the package tree, and 2. By condev-shell (called by "make devshell") to perform
+an "editable" install into the conda development environment, such that source
+files in the clone are live-linked into the conda environment so that they can
+be executed in a context where all build/run/test dependency packages are
+available.
 """
 
 import json
