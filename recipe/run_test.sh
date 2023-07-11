1f776037ec71653e2cfb667f41586826e9400885
Paul Madden <paul.madden@noaa.gov>
2023-07-11 02:36:17 +0000

    Add explanatory note to run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -eu
 
+# This script is called 1. By conda-build, to test code at package-build time, and 2. By "make test"
+# to test code in a live development shell. Its name is dictated by conda-build (see URL below for
+# details), but its contents drive from https://github.com/maddenp/condev/tree/main/demo. If run
+# with no arguments, it executes all test types: linting, typechecking, unit testing, and basic CLI-
+# tool verification. It can also be run with "lint", "typecheck", "unittest", or "cli" arguments to
+# run only a single test type. (The "make lint", "make typecheck", and "make unittest" targets in
+# the root-directory Makefile run the first test types individually; CLI tools can be hand-tested.)
+
+# https://docs.conda.io/projects/conda-build/en/latest/resources/define-metadata.html#test-section
+
 cli() {
   msg Testing CLI programs
   (
