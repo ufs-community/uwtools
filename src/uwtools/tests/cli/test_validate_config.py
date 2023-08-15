52863e17b162b05c598689fa2d06ee4564b7495a
Paul Madden <paul.madden@noaa.gov>
2023-08-15 04:14:11 +0000

    /dev/zero -> /dev/null

--- src/uwtools/tests/cli/test_validate_config.py
+++ src/uwtools/tests/cli/test_validate_config.py
@@ -21,7 +21,7 @@ from uwtools.tests.support import fixture_path
 def files(tmp_path):
     cfgfile = tmp_path / "cfg.yaml"
     logfile = tmp_path / "log"
-    schemafile = fixture_path("/dev/zero")
+    schemafile = fixture_path("/dev/null")
     for path in cfgfile, logfile:
         path.touch()
     return str(cfgfile), str(logfile), str(schemafile)
