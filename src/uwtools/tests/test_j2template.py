faff0d42ae495abf3bdc2a5e145768dc44fcb6de
Paul Madden <paul.madden@noaa.gov>
2023-08-19 02:16:59 +0000

    100% coverage of uwtools.j2template

--- src/uwtools/tests/test_j2template.py
+++ src/uwtools/tests/test_j2template.py
@@ -31,8 +31,16 @@ def test_bad_args(testdata):
         J2Template(testdata.config)
 
 
+def test_dump_file(testdata, tmp_path):
+    path = str(tmp_path / "rendered.txt")
+    j2template = J2Template(testdata.config, template_str=testdata.template)
+    j2template.dump_file(output_path=path)
+    with open(path, "r", encoding="utf-8") as f:
+        assert f.read().strip() == "Hello to the world"
+
+
 def test_render_file(testdata, tmp_path):
-    path = tmp_path / "template.j2"
+    path = tmp_path / "template.jinja2"
     with path.open("w", encoding="utf-8") as f:
         print(testdata.template, file=f)
     validate(J2Template(testdata.config, template_path=str(path)))
