c6219276518ef370ee2e9263ea78313c84b2d9cc
Paul Madden <paul.madden@noaa.gov>
2023-08-20 21:16:37 +0000

    Typechecking fixes

--- src/uwtools/logging.py
+++ src/uwtools/logging.py
@@ -18,10 +18,9 @@ def setup_logging(quiet: bool = False, verbose: bool = False) -> None:
         print("Speficy at most one of 'quiet' and 'verbose'", file=sys.stderr)
         sys.exit(1)
     logging.basicConfig(
-        **{
-            "datefmt": "%Y-%m-%dT%H:%M:%S",
-            "format": "[%(asctime)s] %(levelname)8s %(message)s",
-            "level": logging.DEBUG if verbose else logging.INFO,
-            **({"filename": os.devnull} if quiet else {"stream": sys.stderr}),
-        }
+        datefmt="%Y-%m-%dT%H:%M:%S",
+        filename=os.devnull if quiet else None,
+        format="[%(asctime)s] %(levelname)8s %(message)s",
+        level=logging.DEBUG if verbose else logging.INFO,
+        stream=None if quiet else sys.stderr,
     )
