4c0435a61dbac0bfade797110e441e465e2449d5
Paul Madden <136389411+maddenp-noaa@users.noreply.github.com>
2023-08-21 09:14:44 -0600

    pre-uw-343-test-improvements (#282)

--- src/uwtools/tests/utils/test_atparse_to_jinja2.py
+++ src/uwtools/tests/utils/test_atparse_to_jinja2.py
@@ -1,21 +1,52 @@
-# pylint: disable=missing-function-docstring
+# pylint: disable=missing-function-docstring,redefined-outer-name
 """
 Tests for uwtools.utils.atparse_to_jinja2 module.
 """
+from pytest import fixture, raises
 
 from uwtools.utils import atparse_to_jinja2
 
+# Helper functions
 
-def test_convert():
-    # WRITE TEST(S) TO EXPLICITLY TEST THIS UNIT.
-    pass
+
+@fixture
+def atparsefile(tmp_path):
+    path = tmp_path / "atparse"
+    with open(path, "w", encoding="utf-8") as f:
+        for line in ["@[greeting] to the @[subject]", "@[flowers] are @[color]"]:
+            print(line, file=f)
+    return path
+
+
+@fixture
+def jinja2txt():
+    return "{{greeting}} to the {{subject}}\n{{flowers}} are {{color}}\n"
+
+
+# Test functions
+
+
+def test_convert_bad_args():
+    with raises(RuntimeError):
+        atparse_to_jinja2.convert(input_template="/dev/null")
+
+
+def test_convert_dry_run(atparsefile, capsys, jinja2txt):
+    atparse_to_jinja2.convert(input_template=atparsefile, dry_run=True)
+    assert capsys.readouterr().out == jinja2txt
+
+
+def test_convert_outfile(atparsefile, jinja2txt, tmp_path):
+    outfile = tmp_path / "outfile"
+    atparse_to_jinja2.convert(input_template=atparsefile, outfile=outfile)
+    with open(outfile, "r", encoding="utf-8") as f:
+        assert f.read() == jinja2txt
 
 
 def test__replace():
-    # WRITE TEST(S) TO EXPLICITLY TEST THIS UNIT.
-    pass
-
-
-def test_import():
-    # REMOVE THIS TEST WHEN ^^^ ARE IMPLEMENTED.
-    assert dir(atparse_to_jinja2)
+    # A line without atparse syntax should be returned unchanged:
+    line_without = "Hello to the world"
+    assert atparse_to_jinja2._replace(line_without) == line_without
+    # A line with atparse syntax should be returned updated to Jinja2 syntax:
+    line_with = "@[greeting] to the @[subject]"
+    assert atparse_to_jinja2._replace(line_with) == "{{greeting}} to the {{subject}}"
--- src/uwtools/tests/utils/test_cli_helpers.py
+++ src/uwtools/tests/utils/test_cli_helpers.py
@@ -3,29 +3,48 @@
 Tests for uwtools.utils.cli_helpers module.
 """
 
+from pytest import raises
+
 from uwtools.utils import cli_helpers
 
 
-def test_dict_from_config_args():
-    # WRITE TEST(S) TO EXPLICITLY TEST THIS UNIT.
-    pass
+def test_dict_from_key_eq_val_strings():
+    assert not cli_helpers.dict_from_key_eq_val_strings([])
+    assert cli_helpers.dict_from_key_eq_val_strings(["a=1", "b=2"]) == {"a": "1", "b": "2"}
 
 
 def test_get_file_type():
-    # WRITE TEST(S) TO EXPLICITLY TEST THIS UNIT.
-    pass
+    for ext, file_type in {
+        "yaml": "YAML",
+        "yml": "YAML",
+        "bash": "INI",
+        "sh": "INI",
+        "ini": "INI",
+        "cfg": "INI",
+        "nml": "F90",
+    }.items():
+        assert cli_helpers.get_file_type(f"a.{ext}") == file_type
 
 
-def test_path_if_file_exists():
-    # WRITE TEST(S) TO EXPLICITLY TEST THIS UNIT.
-    pass
+def test_path_if_it_exists(tmp_path):
+    # Test non-existent path:
+    path = tmp_path / "foo"
+    with raises(FileNotFoundError):
+        cli_helpers.path_if_it_exists(str(path))
+    # Test directory that exists:
+    assert cli_helpers.path_if_it_exists(str(tmp_path)) == str(tmp_path)
+    # Test file that exists:
+    path.touch()
+    assert cli_helpers.path_if_it_exists(str(path)) == str(path)
 
 
 def test_setup_logging():
-    # WRITE TEST(S) TO EXPLICITLY TEST THIS UNIT.
-    pass
-
-
-def test_import():
-    # REMOVE THIS TEST WHEN ^^^ ARE IMPLEMENTED.
-    assert dir(cli_helpers)
+    logger = cli_helpers.setup_logging(
+        log_file="/dev/null",
+        log_name="testing",
+        quiet=False,
+        verbose=False,
+        color=False,
+    )
+    assert logger.name == "testing"
+    assert logger.colored_log is False
