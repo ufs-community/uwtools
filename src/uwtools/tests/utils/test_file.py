078f3b8e9482aabe9a83e9e491930d5fee805dcf
Paul Madden <paul.madden@noaa.gov>
2023-08-24 17:32:29 +0000

    Explicit tests for uwtools.utils.file.readable()

--- src/uwtools/tests/utils/test_file.py
+++ src/uwtools/tests/utils/test_file.py
@@ -3,6 +3,7 @@
 Tests for uwtools.utils.file module.
 """
 
+import sys
 from datetime import datetime as dt
 from unittest.mock import patch
 
@@ -28,14 +29,14 @@ def test_handle_existing_delete_failure(exc, assets):
     _, _, rundir = assets
     with patch.object(file.shutil, "rmtree", side_effect=exc):
         with raises(RuntimeError) as e:
-            file.handle_existing(run_directory=rundir, exist_act="delete")
+            file.handle_existing(directory=rundir, action="delete")
         assert "Could not delete directory" in str(e.value)
     assert rundir.is_dir()
 
 
 def test_handle_existing_delete_success(assets):
     _, _, rundir = assets
-    file.handle_existing(run_directory=rundir, exist_act="delete")
+    file.handle_existing(directory=rundir, action="delete")
     assert not rundir.is_dir()
 
 
@@ -44,7 +45,7 @@ def test_handle_existing_rename_failure(exc, assets):
     _, renamed, rundir = assets
     with patch.object(file.shutil, "move", side_effect=exc):
         with raises(RuntimeError) as e:
-            file.handle_existing(run_directory=rundir, exist_act="rename")
+            file.handle_existing(directory=rundir, action="rename")
         assert "Could not rename directory" in str(e.value)
     assert not renamed.is_dir()
     assert rundir.is_dir()
@@ -54,6 +55,19 @@ def test_handle_existing_rename_success(assets):
     now, renamed, rundir = assets
     with patch.object(file, "dt") as dt:
         dt.now.return_value = now
-        file.handle_existing(run_directory=rundir, exist_act="rename")
+        file.handle_existing(directory=rundir, action="rename")
     assert renamed.is_dir()
     assert not rundir.is_dir()
+
+
+def test_readable_file(tmp_path):
+    apath = tmp_path / "afile"
+    with open(apath, "w", encoding="utf-8") as f:
+        f.write("hello")
+    with file.readable(filepath=apath) as f:
+        assert f.read() == "hello"
+
+
+def test_readable_nofile():
+    with file.readable() as f:
+        assert f is sys.stdin
--- src/uwtools/utils/file.py
+++ src/uwtools/utils/file.py
@@ -8,41 +8,46 @@ import shutil
 import sys
 from contextlib import contextmanager
 from datetime import datetime as dt
-from typing import Generator, Optional, TextIO
+from pathlib import Path
+from typing import Generator, Optional, TextIO, Union
 
 
-def handle_existing(run_directory: str, exist_act: str) -> None:
+def handle_existing(directory: str, action: str) -> None:
     """
     Given a run directory, and an action to do if directory exists, delete or rename directory.
+
+    :param directory: The directory to delete or rename.
+    :param action: The action to take on an existing directory ("delete" or "rename")
     """
 
     # Try to delete existing run directory if option is delete.
 
     try:
-        if exist_act == "delete" and os.path.isdir(run_directory):
-            shutil.rmtree(run_directory)
+        if action == "delete" and os.path.isdir(directory):
+            shutil.rmtree(directory)
     except (FileExistsError, RuntimeError) as e:
-        msg = f"Could not delete directory {run_directory}"
+        msg = f"Could not delete directory {directory}"
         logging.critical(msg)
         raise RuntimeError(msg) from e
 
     # Try to rename existing run directory if option is rename.
 
     try:
-        if exist_act == "rename" and os.path.isdir(run_directory):
+        if action == "rename" and os.path.isdir(directory):
             now = dt.now()
-            save_dir = "%s%s" % (run_directory, now.strftime("_%Y%m%d_%H%M%S"))
-            shutil.move(run_directory, save_dir)
+            save_dir = "%s%s" % (directory, now.strftime("_%Y%m%d_%H%M%S"))
+            shutil.move(directory, save_dir)
     except (FileExistsError, RuntimeError) as e:
-        msg = f"Could not rename directory {run_directory}"
+        msg = f"Could not rename directory {directory}"
         logging.critical(msg)
         raise RuntimeError(msg) from e
 
 
 @contextmanager
-def readable(filepath: Optional[str] = None) -> Generator[TextIO, None, None]:
+def readable(filepath: Optional[Union[str, Path]] = None) -> Generator[TextIO, None, None]:
     """
-    If a path to a file is specified, open it and return a readable handle; if not, return stdin.
+    If a path to a file is specified, open it and return a readable handle; if not, return readable
+    stdin.
 
     :param filepath: The path to a file to read.
     """
