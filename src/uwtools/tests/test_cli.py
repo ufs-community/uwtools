c09c9c20e9e9566351d34630b418d80e2ab45fe4
Paul Madden <paul.madden@noaa.gov>
2023-08-28 15:47:48 +0000

    Unit tests at 98%

--- src/uwtools/tests/test_cli.py
+++ src/uwtools/tests/test_cli.py
@@ -59,6 +59,72 @@ def test_add_subparser_template_render(subparsers):
     assert subparsers.choices["render"]
 
 
+@pytest.mark.parametrize(
+    "params",
+    [
+        ("compare", "dispatch_config_compare"),
+        ("realize", "dispatch_config_realize"),
+        ("translate", "dispatch_config_translate"),
+        ("validate", "dispatch_config_validate"),
+    ],
+)
+def test_dispatch_config(params):
+    submode, funcname = params
+    args = ns(submode=submode)
+    with patch.object(cli, funcname) as m:
+        cli.dispatch_config(args)  # type: ignore
+    assert m.called_once_with(args)
+
+
+def test_dispatch_config_compare():
+    args = ns(file_1_path=1, file_1_format=2, file_2_path=3, file_2_format=4)
+    with patch.object(cli.uwtools.config.core, "compare_configs") as m:
+        cli.dispatch_config_compare(args)  # type: ignore
+    assert m.called_once_with(args)
+
+
+def test_dispatch_config_realize():
+    args = ns(
+        input_file=1,
+        input_format=2,
+        output_file=3,
+        output_format=4,
+        values_file=5,
+        values_format=6,
+        values_needed=7,
+        dry_run=8,
+    )
+    with patch.object(cli.uwtools.config.core, "realize_config") as m:
+        cli.dispatch_config_realize(args)  # type: ignore
+    assert m.called_once_with(args)
+
+
+def test_dispatch_config_translate_arparse_to_jinja2():
+    args = ns(
+        input_file=1, input_format="atparse", output_file=3, output_format="jinja2", dry_run=5
+    )
+    with patch.object(cli.uwtools.config.atparse_to_jinja2, "convert") as m:
+        cli.dispatch_config_translate(args)  # type: ignore
+    assert m.called_once_with(args)
+
+
+def test_dispath_config_translate_unsupported():
+    args = ns(input_file=1, input_format="jpg", output_file=3, output_format="png", dry_run=5)
+    assert cli.dispatch_config_translate(args) is False  # type: ignore
+
+
+def test_dispatch_config_validate_yaml():
+    args = ns(input_file=1, input_format="yaml", schema_file=3)
+    with patch.object(cli.uwtools.config.validator, "validate_yaml") as m:
+        cli.dispatch_config_validate(args)  # type: ignore
+    assert m.called_once_with(args)
+
+
+def test_dispath_config_validate_unsupported():
+    args = ns(input_file=1, input_format="jpg", schema_file=3)
+    assert cli.dispatch_config_validate(args) is False  # type: ignore
+
+
 @pytest.mark.parametrize("params", [(False, 1, False, True), (True, 0, True, False)])
 def test_main_fail(params):
     fnretval, status, quiet, verbose = params
@@ -87,11 +153,13 @@ def test_dict_from_key_eq_val_strings():
 
 @fixture
 def subparsers():
+    # Create and return a subparsers test object.
     return Parser().add_subparsers()
 
 
-def submodes(subparser: Parser) -> List[str]:
-    if subparsers := subparser._subparsers:
+def submodes(parser: Parser) -> List[str]:
+    # Return submodes (named subparsers) belonging to the given parser.
+    if subparsers := parser._subparsers:
         if action := subparsers._actions[1]:
             if choices := action.choices:
                 submodes = choices.keys()  # type: ignore
