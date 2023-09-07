b51e8746ec87026a4120d11a3f989bc433ad591d
Paul Madden <paul.madden@noaa.gov>
2023-09-07 22:41:41 +0000

    Install run-time dependency packages from PyPI

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,19 @@
 """
 Basic setuptools configuration.
 
-See https://setuptools.pypa.io/en/latest/userguide/quickstart.html#basic-use for
-information on this standard setuptools file.
+See https://setuptools.pypa.io/en/latest/userguide/quickstart.html#basic-use for information on
+this standard setuptools file.
 
-In this codebase, it is used 1. By conda-build to install the uwtools files into
-the package tree, and 2. By condev-shell (called by "make devshell") to perform
-an "editable" install into the conda development environment, such that source
-files in the clone are live-linked into the conda environment so that they can
-be executed in a context where all build/run/test dependency packages are
-available.
+Here it is used 1. By conda-build to install the uwtools files into the package tree; 2. By "make
+devshell" to perform an "editable" install into the conda development environment, such that source
+files in the clone are live-linked into the conda environment so that they can be executed in a
+context where all build/run/test dependency packages are available; and 3. By pip users to install
+the local package along with its dependencies from PyPI.
 """
 
 import json
 import os
+import re
 
 from setuptools import find_packages, setup  # type: ignore
 
@@ -32,6 +32,10 @@ kwargs = {
 }
 
 if not os.environ.get("CONDA_PREFIX"):
-    kwargs["install_requires"] = [x.replace(" =", "==") for x in meta["packages"]["run"]]
+    kwargs["install_requires"] = [
+        pkg.replace(" =", "==")
+        for pkg in meta["packages"]["run"]
+        if not re.match(r"^python .*$", pkg)
+    ]
 
 setup(**kwargs)
