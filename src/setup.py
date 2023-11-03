af32a93aa1f2dd6b4d61a60ef77c6c4e68738fc1
Naureen Bharwani <naureen.bharwani@noaa.gov>
2023-11-03 12:26:30 -0500

    Attempt continue a workflow on tmate

--- src/setup.py
+++ src/setup.py
@@ -12,11 +12,11 @@ the local package along with its dependencies from PyPI.
 """
 
 
-
-from setuptools import find_packages, setup  # type: ignore
 import json
 import os
 import re
+
+from setuptools import find_packages, setup  # type: ignore
 
 recipe = os.environ.get("RECIPE_DIR", "../recipe")
 with open(os.path.join(recipe, "meta.json"), "r", encoding="utf-8") as f:
