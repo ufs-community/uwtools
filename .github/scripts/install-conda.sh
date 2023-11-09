7a9d920e4fbb41567a81c1df51837d8607697f33
Paul Madden <paul.madden@noaa.gov>
2023-11-09 17:32:37 +0000

    Add back missing url variable

--- .github/scripts/install-conda.sh
+++ .github/scripts/install-conda.sh
@@ -2,6 +2,7 @@
 
 set -ux
 source $(dirname ${BASH_SOURCE[0]})/common.sh
+url=https://github.com/conda-forge/miniforge/releases/download/23.1.0-4/Miniforge3-23.1.0-4-Linux-x86_64.sh
 installer=/tmp/$(basename $url)
 wget --no-verbose -O $installer $url
 bash $installer -bfp $CONDADIR
