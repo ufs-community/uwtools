453cf0c9b5c967319beae0a5acab2139ce6e8ee3
Paul Madden <paul.madden@noaa.gov>
2023-11-10 04:06:00 +0000

    Work on unit tests

--- src/uwtools/tests/config/formats/test_yaml.py
+++ src/uwtools/tests/config/formats/test_yaml.py
@@ -119,19 +119,7 @@ def test_constructor_error_unregistered_constructor(tmp_path):
     assert "Define the constructor before proceeding" in str(e.value)
 
 
-def test_YAMLConfig__load_unexpected_error(tmp_path):
-    cfgfile = tmp_path / "cfg.yaml"
-    with open(cfgfile, "w", encoding="utf-8") as f:
-        print("{n: 88}", file=f)
-    with patch.object(yaml, "load") as load:
-        msg = "Unexpected error"
-        load.side_effect = yaml.constructor.ConstructorError(note=msg)
-        with raises(UWConfigError) as e:
-            YAMLConfig(config_file=cfgfile)
-        assert msg in str(e.value)
-
-
-def test_YAMLConfig__load_paths_failure_stdin_plus_relpath(caplog):
+def test_stdin_plus_relpath_failure(caplog):
     # Instantiate a YAMLConfig with no input file, triggering a read from stdin. Patch stdin to
     # provide YAML with an include directive specifying a relative path. Since a relative path
     # is meaningless relative to stdin, assert that an appropriate error is logged and exception
@@ -145,3 +133,15 @@ def test_YAMLConfig__load_paths_failure_stdin_plus_relpath(caplog):
     msg = f"Reading from stdin, a relative path was encountered: {relpath}"
     assert msg in str(e.value)
     assert logged(caplog, msg)
+
+
+def test_unexpected_error(tmp_path):
+    cfgfile = tmp_path / "cfg.yaml"
+    with open(cfgfile, "w", encoding="utf-8") as f:
+        print("{n: 88}", file=f)
+    with patch.object(yaml, "load") as load:
+        msg = "Unexpected error"
+        load.side_effect = yaml.constructor.ConstructorError(note=msg)
+        with raises(UWConfigError) as e:
+            YAMLConfig(config_file=cfgfile)
+        assert msg in str(e.value)
