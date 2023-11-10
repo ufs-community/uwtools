c84ed68701584103b552c05176af13395b56c576
Paul Madden <paul.madden@noaa.gov>
2023-11-10 01:52:32 +0000

    Work on unit tests

--- src/uwtools/tests/config/test_support.py
+++ src/uwtools/tests/config/test_support.py
@@ -6,8 +6,26 @@ Tests for uwtools.config.jinja2 module.
 import pytest
 
 from uwtools.config import support
+from uwtools.config.formats.fieldtable import FieldTableConfig
+from uwtools.config.formats.ini import INIConfig
+from uwtools.config.formats.nml import NMLConfig
+from uwtools.config.formats.yaml import YAMLConfig
+from uwtools.utils.file import FORMAT
 
 
 @pytest.mark.parametrize("d,n", [({1: 88}, 1), ({1: {2: 88}}, 2), ({1: {2: {3: 88}}}, 3)])
 def test_depth(d, n):
     assert support.depth(d) == n
+
+
+@pytest.mark.parametrize(
+    "cfgtype,fmt",
+    [
+        (FieldTableConfig, FORMAT.fieldtable),
+        (INIConfig, FORMAT.ini),
+        (NMLConfig, FORMAT.nml),
+        (YAMLConfig, FORMAT.yaml),
+    ],
+)
+def test_format_to_config(cfgtype, fmt):
+    assert support.format_to_config(fmt) is cfgtype
