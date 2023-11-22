d7e05632a91c636dbcc5ba74bd3117208399a960
Paul Madden <paul.madden@noaa.gov>
2023-11-22 20:07:29 +0000

    Unit tests @ 100%

---
+++ src/uwtools/tests/api/test_forecast.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-function-docstring,protected-access
+
+import datetime as dt
+from unittest.mock import Mock, patch
+
+from uwtools.api import forecast
+
+
+def test_run():
+    SomeModel = Mock()
+    cycle = dt.datetime(2023, 11, 22, 12)
+    with patch.dict(forecast._CLASSES, {"foo": SomeModel}):
+        forecast.run(
+            model="foo",
+            cycle=cycle,
+            config_file="bar",
+            batch_script="baz",
+            dry_run=False,
+        )
+    SomeModel.assert_called_once_with(
+        batch_script="baz",
+        config_file="bar",
+        dry_run=False,
+    )
+    SomeModel().run.assert_called_once_with(cycle=cycle)
---
+++ src/uwtools/tests/api/test_rocoto.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-function-docstring
+
+from unittest.mock import patch
+
+from uwtools.api import rocoto
+
+
+def test_realize():
+    with patch.object(rocoto, "_realize") as _realize:
+        rocoto.realize(input_file="foo", output_file="bar")
+    _realize.assert_called_once_with(config_file="foo", output_file="bar")
+
+
+def test_validate():
+    with patch.object(rocoto, "_validate") as _validate:
+        rocoto.validate(input_file="foo")
+    _validate.assert_called_once_with(input_xml="foo")
