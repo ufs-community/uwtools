a30503539b2cc0c10905963529d8bc2da247df53
Paul Madden <paul.madden@noaa.gov>
2023-12-01 05:39:39 +0000

    WIP

--- src/uwtools/api/rocoto.py
+++ src/uwtools/api/rocoto.py
@@ -1,3 +1,5 @@
+import os
+
 from uwtools.rocoto import realize_rocoto_xml as _realize
 from uwtools.rocoto import validate_rocoto_xml_file as _validate
 from uwtools.types import OptionalPath
@@ -9,6 +11,13 @@ def realize(input_file: OptionalPath, output_file: OptionalPath) -> bool:
     """
     _realize(config=input_file, output_file=output_file)
     return True
+
+
+def realize_as_str(input_file: OptionalPath) -> str:
+    """
+    ???
+    """
+    return _realize(config=input_file, output_file=os.devnull)
 
 
 def validate(input_file: OptionalPath) -> bool:
