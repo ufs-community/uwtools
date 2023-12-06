19ab66b37d265950959647d4b24b49e5c7df3c49
Paul Madden <paul.madden@noaa.gov>
2023-12-06 18:37:09 +0000

    Add tests

---
+++ src/uwtools/tests/api/test_logging.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-function-docstring,protected-access
+
+import logging as py_logging
+from unittest.mock import patch
+
+from uwtools.api import logging as uw_logging
+
+
+def test_use_custom_logger():
+    kwargs = dict(logger=py_logging.getLogger())
+    with patch.object(uw_logging, "_use_logger") as _use_logger:
+        uw_logging.use_custom_logger(**kwargs)
+    _use_logger.assert_called_once_with(**kwargs)
+
+
+def test_use_uwtools_logger():
+    kwargs = dict(quiet=False, verbose=True)
+    with patch.object(uw_logging, "_setup_logging") as _setup_logging:
+        uw_logging.use_uwtools_logger(**kwargs)
+    _setup_logging.assert_called_once_with(**kwargs)
