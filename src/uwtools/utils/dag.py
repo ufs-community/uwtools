5139935c4c353cd642aa70d12b938ba6a2bec15c
Paul Madden <paul.madden@noaa.gov>
2024-01-31 05:20:28 +0000

    Add dag.py

---
+++ src/uwtools/utils/dag.py
@@ -0,0 +1,32 @@
+"""
+Helpers for iotaa workflows.
+"""
+
+from pathlib import Path
+from typing import Generator
+
+from iotaa import asset, external, task  # refs, tasks
+
+
+@task
+def directory(path: Path) -> Generator:
+    """
+    A filesystem directory, which will be created if not already present.
+
+    :param path: The path to the directory.
+    """
+    yield f"Directory: {path}"
+    yield asset(path, path.is_dir)
+    yield None
+    path.mkdir(parents=True)
+
+
+@external
+def file(path: Path) -> Generator:
+    """
+    A filesystem file, which must already be present.
+
+    :param path: The path to the file.
+    """
+    yield f"File: {path}"
+    yield asset(path, path.is_file)
