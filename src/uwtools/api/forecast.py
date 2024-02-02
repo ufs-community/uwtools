78baf4af9c8ba46deb8cb49e2fa72625e8daf0ab
Paul Madden <paul.madden@noaa.gov>
2024-02-02 20:44:25 +0000

    Add tasknames()

--- src/uwtools/api/forecast.py
+++ src/uwtools/api/forecast.py
@@ -1,4 +1,7 @@
 import datetime as dt
+from typing import List
+
+import iotaa
 
 from uwtools.drivers.forecast import CLASSES as _CLASSES
 from uwtools.types import DefinitePath, OptionalPath
@@ -11,14 +14,17 @@ def run(  # pylint: disable=missing-function-docstring
     batch_script: OptionalPath = None,
     dry_run: bool = False,
 ) -> bool:
-    forecast_class = _CLASSES[model]
-    forecast_class(
+    _CLASSES[model](
         batch_script=batch_script,
         config_file=config_file,
         cycle=cycle,
         dry_run=dry_run,
     ).run()
     return True
+
+
+def tasknames(model: str) -> List[str]:  # pylint: disable=missing-function-docstring
+    return iotaa.tasknames(_CLASSES[model])
 
 
 # The following statement dynamically interpolates values into run()'s docstring, which will not
@@ -38,6 +44,19 @@ immediately on the current system, without creation of a batch script.
 :param batch_script: Path to a batch script to write
 :param dry_run: Do not run forecast, just report what would have been done
 :return: Success status of requested operation (immediate run or batch-script creation)
+""".format(
+    models=", ".join(list(_CLASSES.keys()))
+).strip()
+
+
+# The following statement dynamically interpolates values into tasknames()'s docstring, which will
+# not work if the docstring is inlined in the function. It must remain a separate statement to avoid
+# hardcoding values into it.
+
+tasknames.__doc__ = """
+Returns the names of iotaa tasks in the given object.
+
+:param model: One of: {models}
 """.format(
     models=", ".join(list(_CLASSES.keys()))
 ).strip()
