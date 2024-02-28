45512651a83aeac1d6fa71c80975f5114fc25744
Paul Madden <136389411+maddenp-noaa@users.noreply.github.com>
2024-02-28 09:08:48 -0700

    Expose iotaa Graphviz output (#417)

--- src/uwtools/api/fv3.py
+++ src/uwtools/api/fv3.py
@@ -3,9 +3,9 @@ API access to the uwtools FV3 driver.
 """
 import datetime as dt
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional
 
-import iotaa
+import iotaa as _iotaa
 
 from uwtools.drivers.fv3 import FV3
 
@@ -16,6 +16,7 @@ def execute(
     cycle: dt.datetime,
     batch: bool = False,
     dry_run: bool = False,
+    graph_file: Optional[Path] = None,
 ) -> bool:
     """
     Execute an FV3 task.
@@ -28,11 +29,22 @@ def execute(
     :param cycle: The cycle to run
     :param batch: Submit run to the batch system
     :param dry_run: Do not run forecast, just report what would have been done
+    :param graph_file: Write Graphviz DOT output here
     :return: True if task completes without raising an exception
     """
     obj = FV3(config_file=config_file, cycle=cycle, batch=batch, dry_run=dry_run)
     getattr(obj, task)()
+    if graph_file:
+        with open(graph_file, "w", encoding="utf-8") as f:
+            print(graph(), file=f)
     return True
+
+
+def graph() -> str:
+    """
+    Returns Graphviz DOT code for the most recently executed task.
+    """
+    return _iotaa.graph()
 
 
 def tasks() -> Dict[str, str]:
@@ -40,5 +52,5 @@ def tasks() -> Dict[str, str]:
     Returns a mapping from task names to their one-line descriptions.
     """
     return {
-        task: getattr(FV3, task).__doc__.strip().split("\n")[0] for task in iotaa.tasknames(FV3)
+        task: getattr(FV3, task).__doc__.strip().split("\n")[0] for task in _iotaa.tasknames(FV3)
     }
