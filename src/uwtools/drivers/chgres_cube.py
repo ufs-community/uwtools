68354ee9903884cdb07d87cc1147a0a9cc6688dc
Christina Holt <christina.holt@noaa.gov>
2024-03-01 09:45:07 -0700

    Adding tasks from other drivers. Needs mods.

--- src/uwtools/drivers/chgres_cube.py
+++ src/uwtools/drivers/chgres_cube.py
@@ -43,6 +43,125 @@ class ChgresCube(Driver):
         self._cycle = cycle
         self._rundir = Path(self._driver_config["run_dir"])
 
+    # Workflow tasks
+
+    @tasks
+    def files_copied(self):
+        """
+        Files copied for run.
+        """
+        yield self._taskname("files copied")
+        yield [
+            filecopy(src=Path(src), dst=self._rundir / dst)
+            for dst, src in self._driver_config.get("files_to_copy", {}).items()
+        ]
+
+    @tasks
+    def files_linked(self):
+        """
+        Files linked for run.
+        """
+        yield self._taskname("files linked")
+        yield [
+            symlink(target=Path(target), linkname=self._rundir / linkname)
+            for linkname, target in self._driver_config.get("files_to_link", {}).items()
+        ]
+
+    @task
+    def namelist_file(self):
+        """
+        The namelist file.
+        """
+        fn = "fort.41"
+        yield self._taskname(f"namelist file {fn}")
+        path = self._rundir / fn
+        yield asset(path, path.is_file)
+        vals = self._driver_config["namelist"]["update_values"]["config"]
+        yield None
+        self._create_user_updated_config(
+            config_class=NMLConfig,
+            config_values=self._driver_config.get("namelist", {}),
+            path=path,
+        )
+
+    @tasks
+    def provisioned_run_directory(self):
+        """
+        Run directory provisioned with all required content.
+        """
+        yield self._taskname("provisioned run directory")
+        yield [
+            self.diag_table(),
+            self.field_table(),
+            self.files_copied(),
+            self.files_linked(),
+            self.namelist_file(),
+            self.runscript(),
+        ]
+
+    @tasks
+    def run(self):
+        """
+        A run.
+        """
+        yield self._taskname("run")
+        yield (self._run_via_batch_submission() if self._batch else self._run_via_local_execution())
+
+    @task
+    def runscript(self):
+        """
+        The runscript.
+        """
+        path = self._runscript_path
+        yield self._taskname(path.name)
+        yield asset(path, path.is_file)
+        yield None
+
+        envvars = {
+            "ESMF_RUNTIME_COMPLIANCECHECK": "OFF:depth=4",
+            "KMP_AFFINITY": "scatter",
+            "MPI_TYPE_DEPTH": 20,
+            "OMP_NUM_THREADS": self._driver_config.get("execution", {}).get("threads", 1),
+            "OMP_STACKSIZE": "512m",
+        }
+        envcmds = self._driver_config.get("execution", {}).get("envcmds", [])
+        execution = [self._runcmd, "test $? -eq 0 && touch %s/done" % self._rundir]
+        scheduler = self._scheduler if self._batch else None
+        path.parent.mkdir(parents=True, exist_ok=True)
+        rs = self._runscript(
+            envcmds=envcmds, envvars=envvars, execution=execution, scheduler=scheduler
+        )
+        with open(path, "w", encoding="utf-8") as f:
+            print(rs, file=f)
+        os.chmod(path, os.stat(path).st_mode | stat.S_IEXEC)
+
+    # Private workflow tasks
+
+    @task
+    def _run_via_batch_submission(self):
+        """
+        A run executed via the batch system.
+        """
+        yield self._taskname("run via batch submission")
+        path = Path("%s.submit" % self._runscript_path)
+        yield asset(path, path.is_file)
+        yield self.provisioned_run_directory()
+        self._scheduler.submit_job(runscript=self._runscript_path, submit_file=path)
+
+    @task
+    def _run_via_local_execution(self):
+        """
+        A run executed directly on the local system.
+        """
+        yield self._taskname("run via local execution")
+        path = self._rundir / "done"
+        yield asset(path, path.is_file)
+        yield self.provisioned_run_directory()
+        cmd = "{x} >{x}.out 2>&1".format(x=self._runscript_path)
+        execute(cmd=cmd, cwd=self._rundir, log_output=True)
+
+    # Private helper methods
+
     @property
     def _driver_config(self) -> Dict[str, Any]:
         """
@@ -63,15 +182,24 @@ class ChgresCube(Driver):
             **self._driver_config.get("execution", {}).get("batchargs", {}),
         }
 
+    @property
+    def _runscript_path(self) -> Path:
+        """
+        Returns the path to the runscript.
+        """
+        return self._rundir / "runscript"
+
+    def _taskname(self, suffix: str) -> str:
+        """
+        Returns a common tag for graph-task log messages.
+
+        :param suffix: Log-string suffix.
+        """
+        return "%s chgres_cube %s" % (self._cycle.strftime("%Y%m%d %HZ"), suffix)
+
     def _validate(self) -> None:
         """
         Perform all necessary schema validation.
         """
         for schema_name in ("chgres_cube", "platform"):
             self._validate_one(schema_name=schema_name)
-
-
-
-
-
-
