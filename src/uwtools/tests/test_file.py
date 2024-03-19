556fa143370488b535ded26f5d91dbf42734eaa1
Paul Madden <paul.madden@noaa.gov>
2024-03-19 00:28:48 +0000

    Add dict tests

--- src/uwtools/tests/test_file.py
+++ src/uwtools/tests/test_file.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-function-docstring,protected-access,redefined-outer-name
 
+import pytest
 import yaml
 from pytest import fixture, raises
 
@@ -16,43 +17,51 @@ def assets(tmp_path):
     fn2 = srcdir / "subdir" / "bar"
     fn2.parent.mkdir()
     fn2.touch()
-    config = {"a": {"b": {"foo": str(fn1), "subdir/bar": str(fn2)}}}
+    cfgdict = {"a": {"b": {"foo": str(fn1), "subdir/bar": str(fn2)}}}
     cfgfile = tmp_path / "config.yaml"
     with open(cfgfile, "w", encoding="utf-8") as f:
-        yaml.dump(config, f)
+        yaml.dump(cfgdict, f)
     dstdir = tmp_path / "dst"
-    return dstdir, cfgfile
+    return dstdir, cfgdict, cfgfile
 
 
-def test_FileStager(assets):
-    dstdir, cfgfile = assets
-    stager = file.FileStager(target_dir=dstdir, config=cfgfile, keys=["a", "b"])
+@pytest.mark.parametrize("source", ("dict", "file"))
+def test_FileStager(assets, source):
+    dstdir, cfgdict, cfgfile = assets
+    config = cfgdict if source == "dict" else cfgfile
+    stager = file.FileStager(target_dir=dstdir, config=config, keys=["a", "b"])
     assert set(stager._file_map.keys()) == {"foo", "subdir/bar"}
     assert stager._validate() is True
 
 
-def test_FileStager_bad_key(assets):
-    dstdir, cfgfile = assets
+@pytest.mark.parametrize("source", ("dict", "file"))
+def test_FileStager_bad_key(assets, source):
+    dstdir, cfgfile, cfgdict = assets
+    config = cfgdict if source == "dict" else cfgfile
     with raises(UWConfigError) as e:
-        file.FileStager(target_dir=dstdir, config=cfgfile, keys=["a", "x"])
+        file.FileStager(target_dir=dstdir, config=config, keys=["a", "x"])
     assert str(e.value) == "Config navigation a -> x failed"
 
 
-def test_FileCopier(assets):
-    dstdir, cfgfile = assets
+@pytest.mark.parametrize("source", ("dict", "file"))
+def test_FileCopier_config_file(assets, source):
+    dstdir, cfgdict, cfgfile = assets
+    config = cfgdict if source == "dict" else cfgfile
     assert not (dstdir / "foo").exists()
     assert not (dstdir / "subdir" / "bar").exists()
-    stager = file.FileCopier(target_dir=dstdir, config=cfgfile, keys=["a", "b"])
+    stager = file.FileCopier(target_dir=dstdir, config=config, keys=["a", "b"])
     stager.go()
     assert (dstdir / "foo").is_file()
     assert (dstdir / "subdir" / "bar").is_file()
 
 
-def test_FileLinker(assets):
-    dstdir, cfgfile = assets
+@pytest.mark.parametrize("source", ("dict", "file"))
+def test_FileLinker_config_file(assets, source):
+    dstdir, cfgdict, cfgfile = assets
+    config = cfgdict if source == "dict" else cfgfile
     assert not (dstdir / "foo").exists()
     assert not (dstdir / "subdir" / "bar").exists()
-    stager = file.FileLinker(target_dir=dstdir, config=cfgfile, keys=["a", "b"])
+    stager = file.FileLinker(target_dir=dstdir, config=config, keys=["a", "b"])
     stager.go()
     assert (dstdir / "foo").is_symlink()
     assert (dstdir / "subdir" / "bar").is_symlink()
