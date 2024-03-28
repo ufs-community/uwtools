88cb9c94e1e85fa799136e94119f0fb75c23a497
Christina Holt <christina.holt@noaa.gov>
2024-03-28 10:02:27 -0600

    WIP

--- src/uwtools/drivers/mpas.py
+++ src/uwtools/drivers/mpas.py
@@ -42,49 +42,24 @@ class MPAS(Driver):
     # Workflow tasks
 
     @task
-    def namelist_file(self):
+    def boundary_files(self):
         """
-        The namelist file.
+        Boundary condition files.
         """
-        path = self._rundir / "namelist.atmosphere"
-        yield self._taskname(str(path))
-        yield asset(path, path.is_file)
-        yield None
-        path.parent.mkdir(parents=True, exist_ok=True)
-        self._create_user_updated_config(
-            config_class=NMLConfig,
-            config_values=d,
-            path=path,
-        )
-
-    @tasks
-    def provisioned_run_directory(self):
-        """
-        Run directory provisioned with all required content.
-        """
-        yield self._taskname("provisioned run directory")
+        yield self._taskname("boundary files")
+        lbcs = self._driver_config["lateral_boundary_conditions"]
+        endhour = self._driver_config["length"]
+        interval = lbcs["interval_hours"]
+        symlinks = {}
+        for boundary_hour in range(0, endhour, interval):
+            file_date = self._cycle + timedelta(hours=boundary_hour)
+            fn = file_date.strftime(lbcs["filename"])
+            linkname = self._rundir / fn
+            symlinks[linkname] = Path(lbcs["path"]) / fn
         yield [
-            self.namelist_file(),
-            self.runscript(),
+            symlink(target=t, linkname=l)
+            for t, l in symlinks.items()
         ]
-
-    @task
-    def streams_file(self):
-        """
-        The streams file.
-        """
-        pass
-
-    @task
-    def runscript(self):
-        """
-        The runscript.
-        """
-        path = self._runscript_path
-        yield self._taskname(path.name)
-        yield asset(path, path.is_file)
-        yield None
-        self._write_runscript(path=path, envvars={})
 
     @tasks
     def files_copied(self):
@@ -99,15 +74,76 @@ class MPAS(Driver):
 
     @tasks
     def files_linked(self):
-        """     
+        """
         Files linked for run.
         """
-        yield self._taskname("files linked")
+        pass
+
+    @task
+    def namelist_file(self):
+        """
+        The namelist file.
+        """
+        path = self._rundir / "namelist.atmosphere"
+        yield self._taskname(str(path))
+        yield asset(path, path.is_file)
+        yield None
+        duration = timedelta(hours=self._driver_config["length"])
+        duration = str(duration).replace(" days, ", "")
+        d = {
+            "nhyd_model": {
+                "config_start_time": self._cycle.strftime("%Y-%m-%d_%H:00:00"),
+                "config_run_duration": duration,
+            }
+        }
+        path.parent.mkdir(parents=True, exist_ok=True)
+        self._create_user_updated_config(
+            config_class=NMLConfig,
+            config_values=d,
+            path=path,
+        )
+
+    @tasks
+    def provisioned_run_directory(self):
+        """
+        Run directory provisioned with all required content.
+        """
+        yield self._taskname("provisioned run directory")
         yield [
-            symlink(target=Path(target), linkname=self._rundir / linkname)
-            for linkname, target in self._driver_config.get("files_to_link", {}).items()
+            self.boundary_files(),
+            self.files_copied(),
+            self.files_linked(),
+            self.namelist(),
+            self.runscript(),
+            self.streams(),
         ]
 
+    @task
+    def streams(self):
+        """
+        The streams file.
+        """
+        fn = "streams.atmosphere"
+        yield self._taskname(fn)
+        path = self._rundir / fn
+        yield asset(path, path.is_file)
+        yield self._driver_config["streams"]["path"]
+        render(
+            input_file=self._driver_config["streams"]["path"],
+            output_file=path,
+            values_src=self._driver_config["streams"]["values"],
+        )
+
+    @task
+    def runscript(self):
+        """
+        The runscript.
+        """
+        path = self._runscript_path
+        yield self._taskname(path.name)
+        yield asset(path, path.is_file)
+        yield None
+        self._write_runscript(path=path, envvars={})
 
     # Private helper methods
 
