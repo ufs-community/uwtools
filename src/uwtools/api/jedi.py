c58e4cebbfef0bb18df6a5f55d28934b48836c13
Naureen Bharwani <naureen.bharwani@noaa.gov>
2024-04-01 05:37:37 -0500

    Update jedi api to include cycle

--- src/uwtools/api/jedi.py
+++ src/uwtools/api/jedi.py
@@ -7,28 +7,27 @@ from typing import Dict, Optional
 import uwtools.drivers.support as _support
 from uwtools.drivers.jedi import Jedi as _Jedi
 
-
 def execute(
     task: str,
     config_file: Path,
+    cycle: dt.datetime,
     batch: bool = False,
     dry_run: bool = False,
     graph_file: Optional[Path] = None,
 ) -> bool:
     """
-    Execute an ``jedi`` task.
-
+    Execute a jedi task.
     If ``batch`` is specified, a runscript will be written and submitted to the batch system.
-    Otherwise, the forecast will be run directly on the current system.
-
+    Otherwise, the executable will be run directly on the current system.
     :param task: The task to execute
     :param config_file: Path to YAML config file
+    :param cycle: The cycle to run
     :param batch: Submit run to the batch system
-    :param dry_run: Do not run forecast, just report what would have been done
+    :param dry_run: Do not run the executable, just report what would have been done
     :param graph_file: Write Graphviz DOT output here
-    :return: ``True`` if task completes without raising an exception
+    :return: True if task completes without raising an exception
     """
-    obj = _Jedi(config_file=config_file, batch=batch, dry_run=dry_run)
+    obj = _Jedi(config_file=config_file, cycle=cycle, batch=batch, dry_run=dry_run)
     getattr(obj, task)()
     if graph_file:
         with open(graph_file, "w", encoding="utf-8") as f:
