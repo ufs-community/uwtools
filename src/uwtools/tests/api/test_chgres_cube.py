382b1c650637f2ce6678c78d2932f9c75e14f67f
Paul Madden <paul.madden@noaa.gov>
2024-04-24 17:25:54 +0000

    WIP

--- src/uwtools/tests/api/test_chgres_cube.py
+++ src/uwtools/tests/api/test_chgres_cube.py
@@ -1,23 +1,23 @@
 # pylint: disable=missing-function-docstring,protected-access,redefined-outer-name
 
-import datetime as dt
-from pathlib import Path
-from unittest.mock import patch
+# import datetime as dt
+# from pathlib import Path
+# from unittest.mock import patch
 
-from uwtools.api import chgres_cube
+# from uwtools.api import chgres_cube
 
 
-def test_execute(tmp_path):
-    kwargs = {
-        "task": "foo",
-        "cycle": dt.datetime.now(),
-        "config": "config.yaml",
-        "batch": False,
-        "dry_run": True,
-        "graph_file": tmp_path / "g.dot",
-    }
-    with patch.object(chgres_cube, "_execute", return_value=True) as _execute:
-        assert chgres_cube.execute(**kwargs, stdin_ok=True) is True
-        _execute.assert_called_once_with(
-            **{**kwargs, "config": Path(kwargs["config"]), "driver_class": chgres_cube._Driver}
-        )
+# def test_execute(tmp_path):
+#     kwargs = {
+#         "task": "foo",
+#         "cycle": dt.datetime.now(),
+#         "config": "config.yaml",
+#         "batch": False,
+#         "dry_run": True,
+#         "graph_file": tmp_path / "g.dot",
+#     }
+#     with patch.object(chgres_cube, "execute", return_value=True) as execute:
+#         assert chgres_cube.execute(**kwargs, stdin_ok=True) is True
+#         _execute.assert_called_once_with(
+#             **{**kwargs, "config": Path(kwargs["config"]), "driver_class": chgres_cube._Driver}
+#         )
--- src/uwtools/tests/api/test_sfc_climo_gen.py
+++ src/uwtools/tests/api/test_sfc_climo_gen.py
@@ -1,21 +1,21 @@
 # pylint: disable=missing-function-docstring,protected-access,redefined-outer-name
 
-from pathlib import Path
-from unittest.mock import patch
+# from pathlib import Path
+# from unittest.mock import patch
 
-from uwtools.api import sfc_climo_gen
+# from uwtools.api import sfc_climo_gen
 
 
-def test_execute(tmp_path):
-    kwargs = {
-        "task": "foo",
-        "config": "config.yaml",
-        "batch": False,
-        "dry_run": True,
-        "graph_file": tmp_path / "g.dot",
-    }
-    with patch.object(sfc_climo_gen, "_execute", return_value=True) as _execute:
-        assert sfc_climo_gen.execute(**kwargs, stdin_ok=True) is True
-        _execute.assert_called_once_with(
-            **{**kwargs, "config": Path(kwargs["config"]), "driver_class": sfc_climo_gen._Driver}
-        )
+# def test_execute(tmp_path):
+#     kwargs = {
+#         "task": "foo",
+#         "config": "config.yaml",
+#         "batch": False,
+#         "dry_run": True,
+#         "graph_file": tmp_path / "g.dot",
+#     }
+#     with patch.object(sfc_climo_gen, "_execute", return_value=True) as _execute:
+#         assert sfc_climo_gen.execute(**kwargs, stdin_ok=True) is True
+#         _execute.assert_called_once_with(
+#             **{**kwargs, "config": Path(kwargs["config"]), "driver_class": sfc_climo_gen._Driver}
+#         )
