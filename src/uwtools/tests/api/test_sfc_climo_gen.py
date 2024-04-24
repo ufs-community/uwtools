0fc1437d6329ff8f3ba9870261e6d7a62b79d436
Paul Madden <paul.madden@noaa.gov>
2024-04-24 17:18:38 +0000

    WIP

--- src/uwtools/tests/api/test_sfc_climo_gen.py
+++ src/uwtools/tests/api/test_sfc_climo_gen.py
@@ -3,36 +3,19 @@
 from pathlib import Path
 from unittest.mock import patch
 
-from pytest import fixture
-
 from uwtools.api import sfc_climo_gen
 
 
-@fixture
-def kwargs():
-    return {
-        "batch": False,
+def test_execute(tmp_path):
+    kwargs = {
+        "task": "foo",
         "config": "config.yaml",
+        "batch": False,
         "dry_run": True,
+        "graph_file": tmp_path / "g.dot",
     }
-
-
-def test_execute(kwargs, tmp_path):
-    with patch.object(sfc_climo_gen, "_SfcClimoGen") as SfcClimoGen:
-        assert (
-            sfc_climo_gen.execute(**kwargs, task="foo", graph_file=tmp_path / "graph.dot") is True
+    with patch.object(sfc_climo_gen, "_execute", return_value=True) as _execute:
+        assert sfc_climo_gen.execute(**kwargs, stdin_ok=True) is True
+        _execute.assert_called_once_with(
+            **{**kwargs, "config": Path(kwargs["config"]), "driver_class": sfc_climo_gen._Driver}
         )
-    SfcClimoGen.assert_called_once_with(**{**kwargs, "config": Path(kwargs["config"])})
-    SfcClimoGen().foo.assert_called_once_with()
-
-
-def test_graph():
-    with patch.object(sfc_climo_gen._support, "graph") as graph:
-        sfc_climo_gen.graph()
-    graph.assert_called_once_with()
-
-
-def test_tasks():
-    with patch.object(sfc_climo_gen._support, "tasks") as _tasks:
-        sfc_climo_gen.tasks()
-    _tasks.assert_called_once_with(sfc_climo_gen._SfcClimoGen)
