df2a801a1cecba6e15fe7ec46c51f5df2c0dfff0
Paul Madden <paul.madden@noaa.gov>
2024-04-26 05:38:05 +0000

    Undo some test function names

--- src/uwtools/tests/utils/test_api.py
+++ src/uwtools/tests/utils/test_api.py
@@ -26,28 +26,28 @@ def execute_kwargs():
 
 
 @pytest.mark.parametrize("val", [Path("/some/path"), {"foo": 88}])
-def test_api_ensure_data_source_passthrough(val):
+def test_ensure_data_source_passthrough(val):
     assert api.ensure_data_source(data_source=val, stdin_ok=False) == val
 
 
-def test_api_ensure_data_source_stdin_not_ok():
+def test_ensure_data_source_stdin_not_ok():
     with raises(UWError) as e:
         api.ensure_data_source(data_source=None, stdin_ok=False)
     assert str(e.value) == "Set stdin_ok=True to permit read from stdin"
 
 
-def test_api_ensure_data_source_stdin_ok():
+def test_ensure_data_source_stdin_ok():
     assert api.ensure_data_source(data_source=None, stdin_ok=True) is None
 
 
-def test_api_ensure_data_source_str_to_path():
+def test_ensure_data_source_str_to_path():
     val = "/some/path"
     result = api.ensure_data_source(data_source=val, stdin_ok=False)
     assert isinstance(result, Path)
     assert result == Path(val)
 
 
-def test_api_make_execute(execute_kwargs):
+def test_make_execute(execute_kwargs):
     func = api.make_execute(driver_class=ConcreteDriver, with_cycle=False)
     assert func.__name__ == "execute"
     assert func.__doc__ is not None
@@ -59,7 +59,7 @@ def test_api_make_execute(execute_kwargs):
         _execute.assert_called_once_with(driver_class=ConcreteDriver, cycle=None, **execute_kwargs)
 
 
-def test_api_make_execute_cycle(execute_kwargs):
+def test_make_execute_cycle(execute_kwargs):
     execute_kwargs["cycle"] = dt.now()
     func = api.make_execute(driver_class=ConcreteDriver, with_cycle=True)
     assert func.__name__ == "execute"
@@ -72,7 +72,7 @@ def test_api_make_execute_cycle(execute_kwargs):
         _execute.assert_called_once_with(driver_class=ConcreteDriver, **execute_kwargs)
 
 
-def test_api_make_tasks():
+def test_make_tasks():
     func = api.make_tasks(driver_class=ConcreteDriver)
     assert func.__name__ == "tasks"
     taskmap = func()
@@ -80,18 +80,18 @@ def test_api_make_tasks():
 
 
 @pytest.mark.parametrize("val", [Path("/some/path"), {"foo": 88}])
-def test_api_str2path_passthrough(val):
+def test_str2path_passthrough(val):
     assert api.str2path(val) == val
 
 
-def test_api_str2path_convert():
+def test_str2path_convert():
     val = "/some/path"
     result = api.str2path(val)
     assert isinstance(result, Path)
     assert result == Path(val)
 
 
-def test_api__execute(execute_kwargs, tmp_path):
+def test__execute(execute_kwargs, tmp_path):
     config = tmp_path / "config.yaml"
     with open(config, "w", encoding="utf-8") as f:
         yaml.dump({"some": "config"}, f)
