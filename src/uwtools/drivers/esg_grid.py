813a29506efd5ac8f047d3921e69b9848c6fb0a6
Paul Madden <paul.madden@noaa.gov>
2024-04-26 23:35:03 +0000

    Remove unneeded ESGGrid._runcmd()

--- src/uwtools/drivers/esg_grid.py
+++ src/uwtools/drivers/esg_grid.py
@@ -83,14 +83,6 @@ class ESGGrid(Driver):
         """
         return STR.esggrid
 
-    @property
-    def _runcmd(self):
-        """
-        Returns the full command-line component invocation.
-        """
-        executable = self._driver_config["execution"]["executable"]
-        return "%s" % (executable)
-
     def _taskname(self, suffix: str) -> str:
         """
         Returns a common tag for graph-task log messages.
