581308dc93b032046043014fad4593fd7a53f57c
WeirAE <weirbs@gmail.com>
2024-04-29 09:31:21 -0500

    revert _runcmd to inherit from Driver

--- src/uwtools/drivers/make_hgrid.py
+++ src/uwtools/drivers/make_hgrid.py
@@ -63,14 +63,6 @@ class MakeHgrid(Driver):
         """
         return STR.makehgrid
 
-    @property
-    def _runcmd(self):
-        """
-        Returns the full command-line component invocation.
-        """
-        executable = self._driver_config["execution"]["executable"]
-        return "%s" % (executable)
-
     def _taskname(self, suffix: str) -> str:
         """
         Returns a common tag for graph-task log messages.
