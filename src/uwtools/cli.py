10c7390a78311c72941f8927d7b402f9b71083f0
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2024-05-02 14:24:41 -0400

    debug

--- src/uwtools/cli.py
+++ src/uwtools/cli.py
@@ -925,6 +925,7 @@ def _dispatch_to_driver(name: str, args: Args) -> bool:
     :param args: Parsed command-line args.
     """
     execute: Callable[..., bool] = import_module("uwtools.api.%s" % name).execute
+    breakpoint()
     return execute(
         task=args[STR.action],
         config=args[STR.cfgfile],
