0461b93ace564c64b4b1444be09952abc5d740c0
Paul Madden <paul.madden@noaa.gov>
2024-05-02 19:18:34 +0000

    Handle leadtime in API

--- src/uwtools/api/upp.py
+++ src/uwtools/api/upp.py
@@ -7,6 +7,6 @@ from uwtools.drivers.upp import UPP as _Driver
 from uwtools.utils.api import make_execute as _make_execute
 from uwtools.utils.api import make_tasks as _make_tasks
 
-execute = _make_execute(_Driver, with_cycle=True)
+execute = _make_execute(_Driver, with_cycle=True, with_leadtime=True)
 tasks = _make_tasks(_Driver)
 __all__ = ["execute", "graph", "tasks"]
