fc0ca6fb1a0c05d1ef454bc79caf415462868dcc
Paul Madden <136389411+maddenp-noaa@users.noreply.github.com>
2024-05-15 08:38:35 -0600

    Create parent directories in file copy mode (#485)

--- src/uwtools/utils/tasks.py
+++ src/uwtools/utils/tasks.py
@@ -54,7 +54,7 @@ def filecopy(src: Path, dst: Path):
     yield "Copy %s -> %s" % (src, dst)
     yield asset(dst, dst.is_file)
     yield file(src)
-    dst.parent.mkdir(exist_ok=True)
+    dst.parent.mkdir(parents=True, exist_ok=True)
     copy(src, dst)
 
 
