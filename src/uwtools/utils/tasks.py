bbaf2f4403f278f184da06473846f508c215f0ef
Paul Madden <paul.madden@noaa.gov>
2024-05-21 01:30:40 +0000

    WIP

--- src/uwtools/utils/tasks.py
+++ src/uwtools/utils/tasks.py
@@ -70,4 +70,7 @@ def symlink(target: Path, linkname: Path):
     yield asset(linkname, linkname.exists)
     yield existing(target)
     linkname.parent.mkdir(parents=True, exist_ok=True)
-    os.symlink(src=target, dst=linkname)
+    os.symlink(
+        src=target if target.is_absolute() else target.relative_to(linkname.parent, walk_up=True),
+        dst=linkname,
+    )
