3ab74e80d48186de686032d6c9b30e321ae024fa
Paul Madden <paul.madden@noaa.gov>
2024-05-29 16:49:52 +0000

    Work on docs

--- docs/sections/user_guide/cli/tools/file/copy-exec-timedep.cmd
+++ docs/sections/user_guide/cli/tools/file/copy-exec-timedep.cmd
@@ -1,4 +1,4 @@
-rm -rf dst-copy-timedep
-uw file copy --target-dir dst-copy-timedep --config-file copy-config-timedep.yaml --cycle 2024-05-29T12 --leadtime 6 config files
+rm -rf copy-dst-timedep
+uw file copy --target-dir copy-dst-timedep --config-file copy-config-timedep.yaml --cycle 2024-05-29T12 --leadtime 6 config files
 echo
-tree dst-copy-timedep
+tree copy-dst-timedep
--- docs/sections/user_guide/cli/tools/file/link-exec-timedep.cmd
+++ docs/sections/user_guide/cli/tools/file/link-exec-timedep.cmd
@@ -1,4 +1,4 @@
-rm -rf dst-link-timedep
-uw file link --target-dir dst-link-timedep --config-file link-config-timedep.yaml --cycle 2024-05-29T12 --leadtime 6 config files
+rm -rf link-dst-timedep
+uw file link --target-dir link-dst-timedep --config-file link-config-timedep.yaml --cycle 2024-05-29T12 --leadtime 6 config files
 echo
-tree dst-link-timedep
+tree link-dst-timedep
