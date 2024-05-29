5be978fb65ac11185bc503c2dea02572f01dd8ad
Paul Madden <paul.madden@noaa.gov>
2024-05-29 15:38:37 +0000

    WIP

--- src/uwtools/api/file.py
+++ src/uwtools/api/file.py
@@ -2,6 +2,7 @@
 API access to ``uwtools`` file-management tools.
 """
 
+import datetime as dt
 from pathlib import Path
 from typing import List, Optional, Union
 
@@ -13,6 +14,8 @@ from uwtools.utils.api import ensure_data_source as _ensure_data_source
 def copy(
     target_dir: Union[Path, str],
     config: Optional[Union[dict, Path, str]] = None,
+    cycle: Optional[dt.datetime] = None,
+    leadtime: Optional[dt.timedelta] = None,
     keys: Optional[List[str]] = None,
     dry_run: bool = False,
     stdin_ok: bool = False,
@@ -22,6 +25,8 @@ def copy(
 
     :param target_dir: Path to target directory
     :param config: YAML-file path, or ``dict`` (read ``stdin`` if missing or ``None``).
+    :param cycle: A datetime object to make available for use in the config.
+    :param leadtime: A timedelta object to make available for use in the config.
     :param keys: YAML keys leading to file dst/src block
     :param dry_run: Do not copy files
     :param stdin_ok: OK to read from ``stdin``?
@@ -30,6 +35,8 @@ def copy(
     _FileCopier(
         target_dir=Path(target_dir),
         config=_ensure_data_source(config, stdin_ok),
+        cycle=cycle,
+        leadtime=leadtime,
         keys=keys,
         dry_run=dry_run,
     ).go()
@@ -39,6 +46,8 @@ def copy(
 def link(
     target_dir: Union[Path, str],
     config: Optional[Union[dict, Path, str]] = None,
+    cycle: Optional[dt.datetime] = None,
+    leadtime: Optional[dt.timedelta] = None,
     keys: Optional[List[str]] = None,
     dry_run: bool = False,
     stdin_ok: bool = False,
@@ -48,6 +57,8 @@ def link(
 
     :param target_dir: Path to target directory
     :param config: YAML-file path, or ``dict`` (read ``stdin`` if missing or ``None``).
+    :param cycle: A datetime object to make available for use in the config.
+    :param leadtime: A timedelta object to make available for use in the config.
     :param keys: YAML keys leading to file dst/src block
     :param dry_run: Do not link files
     :param stdin_ok: OK to read from ``stdin``?
@@ -56,6 +67,8 @@ def link(
     _FileLinker(
         target_dir=Path(target_dir),
         config=_ensure_data_source(config, stdin_ok),
+        cycle=cycle,
+        leadtime=leadtime,
         keys=keys,
         dry_run=dry_run,
     ).go()
