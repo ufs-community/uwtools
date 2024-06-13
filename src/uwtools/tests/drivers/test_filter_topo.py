dc45f67d576aeb26faac0f22e5b324bfa75ec9a5
Paul Madden <paul.madden@noaa.gov>
2024-06-13 19:24:33 +0000

    WIP on tests

---
+++ src/uwtools/tests/drivers/test_filter_topo.py
@@ -0,0 +1,103 @@
+# pylint: disable=missing-function-docstring,protected-access,redefined-outer-name
+"""
+filter_topo driver tests.
+"""
+# from pathlib import Path
+# from unittest.mock import DEFAULT as D
+# from unittest.mock import patch
+
+from pytest import fixture
+
+from uwtools.drivers import filter_topo
+
+# from uwtools.scheduler import Slurm
+
+# Fixtures
+
+
+@fixture
+def config():
+    # afile = tmp_path / "afile"
+    # afile.touch()
+    return {
+        "filter_topo": {
+            "execution": {
+                "executable": "/path/to/orog_gsl",
+            },
+            "namelist": {
+                "update_values": {
+                    "filter_topo_nml": {
+                        "grid_file": "/path/to/grid/file",
+                        "grid_type": 0,
+                        "mask_field": "land_frac",
+                        "regional": True,
+                        "res": 403,
+                        "stretch_fac": 0.999,
+                        "topo_field": "orog_filt",
+                        "topo_file": "/path/to/topo/file",
+                        "zero_ocean": True,
+                    }
+                }
+            },
+            "run_dir": "/path/to/run/dir",
+        }
+    }
+
+
+@fixture
+def driverobj(config):
+    return filter_topo.FilterTopo(config=config, batch=True)
+
+
+# Tests
+
+
+def test_FilterTopo(driverobj):
+    assert isinstance(driverobj, filter_topo.FilterTopo)
+
+
+# def test_FilterTopo_provisioned_run_directory(driverobj):
+#     with patch.multiple(
+#         driverobj, input_grid_file=D, runscript=D, topo_data_2p5m=D, topo_data_30s=D
+#     ) as mocks:
+#         driverobj.provisioned_run_directory()
+#     for m in mocks:
+#         mocks[m].assert_called_once_with()
+
+
+# def test_FilterTopo_run_batch(driverobj):
+#     with patch.object(driverobj, "_run_via_batch_submission") as func:
+#         driverobj.run()
+#         func.assert_called_once_with()
+
+
+# def test_FilterTopo_run_local(driverobj):
+#     driverobj._batch = False
+#     with patch.object(driverobj, "_run_via_local_execution") as func:
+#         driverobj.run()
+#         func.assert_called_once_with()
+
+
+# def test_FilterTopo_runscript(driverobj):
+#     with patch.object(driverobj, "_runscript") as runscript:
+#         driverobj.runscript()
+#         runscript.assert_called_once()
+#         args = ("envcmds", "envvars", "execution", "scheduler")
+#         types = [list, dict, list, Slurm]
+#         assert [type(runscript.call_args.kwargs[x]) for x in args] == types
+
+
+# def test_FilterTopo__driver_config(driverobj):
+#     assert driverobj._driver_config == driverobj._config["filter_topo"]
+
+
+# def test_FilterTopo__runscript_path(driverobj):
+#     assert driverobj._runscript_path == driverobj._rundir / "runscript.filter_topo"
+
+
+# def test_FilterTopo__taskname(driverobj):
+#     assert driverobj._taskname("foo") == "filter_topo foo"
+
+
+# def test_FilterTopo__validate(driverobj):
+#     driverobj._validate()
