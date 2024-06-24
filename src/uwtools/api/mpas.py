366a9751d451069ddc3a792fc89868008b430f9e
Paul Madden <paul.madden@noaa.gov>
2024-06-24 20:31:19 +0000

    Expose driver classes via API

--- src/uwtools/api/mpas.py
+++ src/uwtools/api/mpas.py
@@ -2,11 +2,12 @@
 API access to the ``uwtools`` ``mpas`` driver.
 """
 
-from uwtools.drivers.mpas import MPAS as _Driver
+from uwtools.drivers.mpas import MPAS
 from uwtools.drivers.support import graph
 from uwtools.utils.api import make_execute as _make_execute
 from uwtools.utils.api import make_tasks as _make_tasks
 
-execute = _make_execute(_Driver, with_cycle=True)
-tasks = _make_tasks(_Driver)
-__all__ = ["execute", "graph", "tasks"]
+_driver = MPAS
+execute = _make_execute(_driver, with_cycle=True)
+tasks = _make_tasks(_driver)
+__all__ = [_driver.__name__, "execute", "graph", "tasks"]
--- src/uwtools/api/sfc_climo_gen.py
+++ src/uwtools/api/sfc_climo_gen.py
@@ -2,11 +2,12 @@
 API access to the ``uwtools`` ``sfc_climo_gen`` driver.
 """
 
-from uwtools.drivers.sfc_climo_gen import SfcClimoGen as _Driver
+from uwtools.drivers.sfc_climo_gen import SfcClimoGen
 from uwtools.drivers.support import graph
 from uwtools.utils.api import make_execute as _make_execute
 from uwtools.utils.api import make_tasks as _make_tasks
 
-execute = _make_execute(_Driver)
-tasks = _make_tasks(_Driver)
-__all__ = ["execute", "graph", "tasks"]
+_driver = SfcClimoGen
+execute = _make_execute(_driver)
+tasks = _make_tasks(_driver)
+__all__ = [_driver.__name__, "execute", "graph", "tasks"]
