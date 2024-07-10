c095378b74101ba90d17a98022dcf03f19c716aa
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-09 20:07:16 -0600

    Added cell output for easier review and updated path of rendered config file

--- notebooks/tests/test_example.py
+++ notebooks/tests/test_example.py
@@ -3,8 +3,10 @@ from testbook import testbook
 import os
 import yaml
 
+# Run the YAML config section of the notebook
 @testbook('./example.ipynb', execute=range(0,4))
 def test_get_yaml_config(tb):
+
    # check output text
    assert tb.cell_output_text(3) == 'values:\n  date: 20240105\n  greeting: Good Night\n  recipient: Moon\n  repeat: 2'
    
@@ -14,24 +16,26 @@ def test_get_yaml_config(tb):
    
 def test_template_render():
    # remove the rendered file if it exists
-   if os.path.exists("./tmp/user_config.yaml"):
-      os.remove("./tmp/user_config.yaml")
+   rendered_path = "./fixtures/rendered_config.yaml"
+   if os.path.exists(rendered_path):
+      os.remove(rendered_path)
 
    # run the template rendering section of the notebook
    with testbook('./example.ipynb', execute=range(4,14)) as tb:
+
       # check output text of cells with %%bash cell magics
       assert tb.cell_output_text(7) == 'user:\n  name: {{ first }} {{ last }}\n  favorite_food: {{ food }}'
       assert tb.cell_output_text(9) == 'first: John\nlast: Doe\nfood: burritos'
       assert tb.cell_output_text(13) == 'user:\n  name: John Doe\n  favorite_food: burritos'
       
       # check that the rendered file was created in the correct directory
-      assert os.path.exists("./tmp/user_config.yaml")
+      assert os.path.exists(rendered_path)
       
       # check the contents of the rendered file
-      with open('./tmp/user_config.yaml', 'r') as f:
+      with open(rendered_path, 'r') as f:
          user_config = yaml.safe_load(f)
       assert user_config['user'] == {'name': 'John Doe', 'favorite_food': 'burritos'}
 
    # clean up the temporary file after the test is run
-   os.remove("./tmp/user_config.yaml")
+   os.remove(rendered_path)
    
