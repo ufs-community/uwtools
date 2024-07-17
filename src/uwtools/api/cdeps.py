5d46c7bf0eb28285c932c2e7da7158690a224c91
Paul Madden <paul.madden@noaa.gov>
2024-07-17 01:09:37 +0000

    Add API test

--- src/uwtools/api/cdeps.py
+++ src/uwtools/api/cdeps.py
@@ -2,11 +2,12 @@
 API access to the ``uwtools`` ``cdeps`` driver.
 """
 
-from uwtools.drivers.cdeps import CDEPS as _Driver
+from uwtools.drivers.cdeps import CDEPS
 from uwtools.drivers.support import graph
 from uwtools.utils.api import make_execute as _make_execute
 from uwtools.utils.api import make_tasks as _make_tasks
 
-execute = _make_execute(_Driver, with_cycle=True)
-tasks = _make_tasks(_Driver)
-__all__ = ["execute", "graph", "tasks"]
+_driver = CDEPS
+execute = _make_execute(_driver, with_cycle=True)
+tasks = _make_tasks(_driver)
+__all__ = [_driver.__name__, "execute", "graph", "tasks"]
