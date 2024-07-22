fa78f408a24b15f6f00c0ab318b117156f4d09aa
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-22 13:45:29 -0600

    Remove version number and update dependency

--- .github/scripts/test-nb.sh
+++ .github/scripts/test-nb.sh
@@ -1,10 +1,7 @@
 set -ae
-
-LATEST_UWTOOLS_VERSION= conda search -c ufs-community --override-channels uwtools | tail -n 1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]'
-
 source $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
-conda install -c ufs-community -c conda-forge --override-channels --repodata-fn repodata.json uwtools=$LATEST_UWTOOLS_VERSION
+conda install -c ufs-community -c conda-forge --override-channels --repodata-fn repodata.json uwtools
 cd notebooks
 source install-deps
 make test-nb
