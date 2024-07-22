b52ce7d36c5b2389b48e391fcfe6e7d58d62cf46
Paul Madden <paul.madden@noaa.gov>
2024-07-22 14:20:53 +0000

    Use partial() on tasks() in CDEPS API module

--- src/uwtools/api/cdeps.py
+++ src/uwtools/api/cdeps.py
@@ -2,12 +2,13 @@
 API access to the ``uwtools`` ``cdeps`` driver.
 """
 
+from functools import partial
+
 from uwtools.drivers.cdeps import CDEPS
-from uwtools.drivers.support import graph
+from uwtools.drivers.support import graph, tasks
 from uwtools.utils.api import make_execute as _make_execute
-from uwtools.utils.api import make_tasks as _make_tasks
 
 _driver = CDEPS
 execute = _make_execute(_driver, with_cycle=True)
-tasks = _make_tasks(_driver)
+tasks = partial(tasks, _driver)
 __all__ = [_driver.__name__, "execute", "graph", "tasks"]
