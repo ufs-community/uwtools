a503fb525fbc4870cdb7a991b0d4780a163d0b65
Paul Madden <paul.madden@noaa.gov>
2024-07-30 15:38:48 +0000

    Use newer conda in CI

--- .github/scripts/install-conda.sh
+++ .github/scripts/install-conda.sh
@@ -1,6 +1,6 @@
 set -eux
 source $(dirname ${BASH_SOURCE[0]})/common.sh
-url=https://github.com/conda-forge/miniforge/releases/download/23.1.0-4/Miniforge3-23.1.0-4-Linux-x86_64.sh
+url=https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-Linux-x86_64.sh
 installer=/tmp/$(basename $url)
 wget --no-verbose -O $installer $url
 bash $installer -bfp $CI_CONDA_DIR
