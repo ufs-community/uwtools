219960f94df497abebbf1ad79246f264d0ab34e3
Paul Madden <paul.madden@noaa.gov>
2024-08-12 04:37:06 +0000

    WIP

---
+++ docs/sections/user_guide/cli/tools/file/mkdir-exec-no-target-dir-err.cmd
@@ -0,0 +1,1 @@
+uw file mkdir --config-file mkdir-config.yaml config
--- docs/sections/user_guide/cli/tools/file/mkdir-exec-timedep.cmd
+++ docs/sections/user_guide/cli/tools/file/mkdir-exec-timedep.cmd
@@ -1,4 +1,4 @@
 rm -rf mkdir-parent-timedep
-uw file link --target-dir mkdir-parent-timedep --config-file mkdir-config-timedep.yaml --cycle 2024-05-29T12 --leadtime 6 config
+uw file mkdir --target-dir mkdir-parent-timedep --config-file mkdir-config-timedep.yaml --cycle 2024-05-29T12 --leadtime 6 config
 echo
 tree -F mkdir-parent-timedep
