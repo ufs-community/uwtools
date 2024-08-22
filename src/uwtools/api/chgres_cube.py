0308c31a11559e9d596a557a5c08b489b8601f4b
Paul Madden <paul.madden@noaa.gov>
2024-08-22 00:12:27 +0000

    WIP

--- src/uwtools/api/chgres_cube.py
+++ src/uwtools/api/chgres_cube.py
@@ -2,13 +2,27 @@
 API access to the ``uwtools`` ``chgres_cube`` driver.
 """
 
-from functools import partial
-
 from uwtools.drivers.chgres_cube import ChgresCube
-from uwtools.drivers.support import graph, tasks
+from uwtools.drivers.support import graph
+from uwtools.drivers.support import tasks as _tasks
 from uwtools.utils.api import make_execute as _make_execute
 
 _driver = ChgresCube
 execute = _make_execute(_driver, with_cycle=True)
-tasks = partial(tasks, _driver)
-__all__ = ["ChgresCube", "execute", "graph", "tasks"]
+
+
+def schema() -> dict:
+    """
+    Return the driver's schema.
+    """
+    return _driver.schema()
+
+
+def tasks() -> dict[str, str]:
+    """
+    Return a mapping from task names to their one-line descriptions.
+    """
+    return _tasks(_driver)
+
+
+__all__ = ["ChgresCube", "execute", "graph", "schema", "tasks"]
