01c721a9bf5f1efff69ffb954c5545e95f029235
Travis Byrne <travis.p.byrne@gmail.com>
2024-09-23 13:43:22 -0600

    change error demo style and update section links

--- notebooks/tests/test_config.py
+++ notebooks/tests/test_config.py
@@ -1,42 +1,24 @@
 import yaml
-from pytest import raises
 from testbook import testbook
-from testbook.exceptions import TestbookRuntimeError
-from uwtools.exceptions import UWConfigError, UWConfigRealizeError
-
-all_cells = [*range(0, 57)]
-err_cells = [13, 17, 45]
-non_err_cells = list(filter(lambda n: n not in err_cells, all_cells))
 
 
 def test_get_config():
     with open("fixtures/config/get-config.yaml", "r", encoding="utf-8") as f:
         config1_str = f.read().rstrip()
         config1_dict = yaml.safe_load(config1_str)
-    with testbook("config.ipynb", execute=non_err_cells) as tb:
+    with testbook("config.ipynb", execute=True) as tb:
         assert tb.ref("config1") == config1_dict
         assert tb.cell_output_text(5) == config1_str
-        assert tb.cell_output_text(7) == config1_str
+        assert config1_str in tb.cell_output_text(7)
         assert tb.cell_output_text(9) == "message:\n  greeting: Hi\n  recipient: Earth"
 
 
 def test_depth():
-    with testbook("config.ipynb", execute=non_err_cells) as tb:
+    with testbook("config.ipynb", execute=True) as tb:
         assert tb.cell_output_text(11) == "greeting=Salutations\nrecipient=Mars"
+        assert tb.cell_output_text(13) == "Cannot instantiate depth-1 SHConfig with depth-2 config"
         assert tb.cell_output_text(15) == "[message]\ngreeting = Salutations\nrecipient = Mars"
-
-
-def test_depth_errs():
-    with raises(TestbookRuntimeError) as e:
-        with testbook("config.ipynb", execute=[1, 13]):
-            pass
-    assert e.value.eclass == UWConfigError
-    assert e.value.evalue == "Cannot instantiate depth-1 SHConfig with depth-2 config"
-    with raises(TestbookRuntimeError) as e:
-        with testbook("config.ipynb", execute=[1, 17]):
-            pass
-    assert e.value.eclass == UWConfigError
-    assert e.value.evalue == "Cannot instantiate depth-2 INIConfig with depth-1 config"
+        assert tb.cell_output_text(17) == "Cannot instantiate depth-2 INIConfig with depth-1 config"
 
 
 def test_realize():
@@ -48,7 +30,7 @@ def test_realize():
         update_config_str = f.read().rstrip()
     with open("fixtures/config/keys-config.yaml", "r", encoding="utf-8") as f:
         keys_config_str = f.read().rstrip()
-    with testbook("config.ipynb", execute=non_err_cells) as tb:
+    with testbook("config.ipynb", execute=True) as tb:
         # Ensure that cell output text matches expectations.
         assert tb.cell_output_text(21) == str(config_dict)
         assert tb.cell_output_text(23) == config_str
@@ -73,6 +55,7 @@ def test_realize():
             41
         ) and "memo.message: {{ greeting }}, {{ recipient }}!" in tb.cell_output_text(41)
         assert tb.cell_output_text(43) == update_config_str
+        assert tb.cell_output_text(45) == "Config could not be totally realized"
         assert (
             "'sender_id': '321'" in tb.cell_output_text(47)
             and "'message': 'Salutations, Mars!'" in tb.cell_output_text(47)
@@ -85,18 +68,10 @@ def test_realize():
         )
 
 
-def test_realize_errs():
-    with raises(TestbookRuntimeError) as e:
-        with testbook("config.ipynb", execute=[1, 45]):
-            pass
-    assert e.value.eclass == UWConfigRealizeError
-    assert e.value.evalue == "Config could not be totally realized"
-
-
 def test_realize_to_dict():
     with open("fixtures/config/get-config.yaml", "r", encoding="utf-8") as f:
         config_str = f.read().rstrip()
-    with testbook("config.ipynb", execute=non_err_cells) as tb:
+    with testbook("config.ipynb", execute=True) as tb:
         assert tb.cell_output_text(53) == config_str
         assert (
             "'id': '456'" in tb.cell_output_text(55)
