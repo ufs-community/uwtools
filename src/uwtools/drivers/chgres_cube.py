bd2e7d1066d8a2b7ee6176e210e6c77a54dd4e91
WeirAE <weirbs@gmail.com>
2024-10-09 17:14:14 -0500

    schema and key logic complete

--- src/uwtools/drivers/chgres_cube.py
+++ src/uwtools/drivers/chgres_cube.py
@@ -36,28 +36,38 @@ class ChgresCube(DriverCycleBased):
         if update_values := namelist.get(STR.updatevalues):
             config_files = update_values["config"]
             file_keys = [
+                "atm_files_input_grid",
+                "grib2_file_input_grid",
                 "mosaic_file_target_grid",
+                "nst_files_input_grid",
+                "sfc_files_input_grid",
                 "orog_files_input_grid",
                 "orog_files_target_grid",
             ]
-            dir_keys = ["fix_dir_target_grid", "orog_dir_input_grid", "orog_dir_target_grid"]
-            for file_key, dir_key in zip(file_keys, dir_keys):
-                full_path = Path(config_files[dir_key]) / config_files[file_key]
-                input_files.append(full_path)
-            for k in [
-                "atm_files_input_grid",
-                "grib2_file_input_grid",
-                "nst_files_input_grid",
-                "sfc_files_input_grid",
-            ]:
-                full_path = Path(config_files[k]) / config_files["data_dir_input_grid"]
-                input_files.append(full_path)
-            for k in [
+            path_keys = [
                 "atm_core_files_input_grid",
                 "atm_tracer_files_input_grid",
                 "varmap_file",
                 "vcoord_file_target_grid",
-            ]:
+            ]
+            dir_keys = [
+                "data_dir_input_grid",
+                "data_dir_input_grid",
+                "fix_dir_target_grid",
+                "data_dir_input_grid",
+                "data_dir_input_grid",
+                "orog_dir_input_grid",
+                "orog_dir_target_grid",
+            ]
+            for file_key, dir_key in zip(file_keys, dir_keys):
+                if file_key and dir_key in config_files:
+                    v = config_files[file_key]
+                    if isinstance(v, str):
+                        full_path = Path(config_files[dir_key]) / config_files[file_key]
+                        input_files.append(full_path)
+                    else:
+                        input_files += v
+            for k in path_keys:
                 if k in config_files:
                     v = config_files[k]
                     if isinstance(v, str):
