2cb5a1918f9631015febface24d80d2474fb2877
WeirAE <weirbs@gmail.com>
2024-10-10 13:41:19 -0500

    apply changes from comments

--- src/uwtools/drivers/chgres_cube.py
+++ src/uwtools/drivers/chgres_cube.py
@@ -35,45 +35,33 @@ class ChgresCube(DriverCycleBased):
             input_files.append(base_file)
         if update_values := namelist.get(STR.updatevalues):
             config_files = update_values["config"]
-            file_keys = [
-                "atm_files_input_grid",
-                "grib2_file_input_grid",
-                "mosaic_file_target_grid",
-                "nst_files_input_grid",
-                "sfc_files_input_grid",
-                "orog_files_input_grid",
-                "orog_files_target_grid",
-            ]
-            path_keys = [
+            for k in ["mosaic_file_target_grid", "varmap_file", "vcoord_file_target_grid"]:
+                input_files.append(config_files[k])
+            for k in [
                 "atm_core_files_input_grid",
                 "atm_tracer_files_input_grid",
-                "varmap_file",
-                "vcoord_file_target_grid",
-            ]
-            dir_keys = [
-                "data_dir_input_grid",
-                "data_dir_input_grid",
-                "fix_dir_target_grid",
-                "data_dir_input_grid",
-                "data_dir_input_grid",
-                "orog_dir_input_grid",
-                "orog_dir_target_grid",
-            ]
-            for file_key, dir_key in zip(file_keys, dir_keys):
-                if file_key and dir_key in config_files:
-                    v = config_files[file_key]
-                    if isinstance(v, str):
-                        full_path = Path(config_files[dir_key]) / config_files[file_key]
-                        input_files.append(full_path)
-                    else:
-                        input_files += v
-            for k in path_keys:
+                "grib2_file_input_grid",
+                "nst_files_input_grid",
+                "sfc_files_input_grid",
+            ]:
                 if k in config_files:
+                    grid_path = Path(config_files["data_dir_input_grid"])
                     v = config_files[k]
                     if isinstance(v, str):
-                        input_files.append(v)
+                        input_files.append(grid_path / v)
                     else:
-                        input_files += v
+                        input_files.extend([grid_path / f for f in v])
+            for k in [
+                "orog_files_input_grid",
+                "orog_files_target_grid",
+            ]:
+                if k in config_files:
+                    grid_path = Path(config_files[k.replace("files", "dir")])
+                    v = config_files[k]
+                    if isinstance(v, str):
+                        input_files.append(grid_path / v)
+                    else:
+                        input_files.extend([grid_path / f for f in v])
         yield [file(Path(input_file)) for input_file in input_files]
         self._create_user_updated_config(
             config_class=NMLConfig,
