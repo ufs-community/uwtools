68d452af946551e360115cb993b8034b5659e251
Travis Byrne <travis.p.byrne@gmail.com>
2024-10-16 08:10:47 -0600

    improvements from code review

--- notebooks/tests/test_config.py
+++ notebooks/tests/test_config.py
@@ -96,18 +96,21 @@ def test_compare():
 
 
 def test_validate():
+    with open("fixtures/config/get-config.yaml", "r", encoding="utf-8") as f:
+        cfg = f.read().rstrip()
     with open("fixtures/config/validate.jsonschema", "r", encoding="utf-8") as f:
         schema = f.read().rstrip()
     with testbook("config.ipynb", execute=True) as tb:
-        assert tb.cell_output_text(67) == schema
+        assert tb.cell_output_text(67) == cfg
+        assert tb.cell_output_text(69) == schema
         valid_out = ("INFO 0 UW schema-validation errors found", "True")
-        assert all(x in tb.cell_output_text(69) for x in valid_out)
+        assert all(x in tb.cell_output_text(71) for x in valid_out)
         invalid_out = (
             "ERROR 1 UW schema-validation error found",
             "ERROR   47 is not of type 'string'",
             "False",
         )
-        assert all(x in tb.cell_output_text(71) for x in invalid_out)
+        assert all(x in tb.cell_output_text(73) for x in invalid_out)
 
 
 def test_cfg_classes():
@@ -116,17 +119,17 @@ def test_cfg_classes():
     with testbook("config.ipynb", execute=True) as tb:
         with open("tmp/fruits.ini", "r", encoding="utf-8") as f:
             dump = f.read().rstrip()
-        assert tb.cell_output_text(75) == cfg
-        assert tb.cell_output_text(77) == "True"
+        assert tb.cell_output_text(77) == cfg
+        assert tb.cell_output_text(79) == "True"
         diff_cmp = (
             "INFO fruit count:          grapes:  - {{ grape_count }} + 8",
             "INFO fruit count:           kiwis:  - 2 + 1",
             "False",
         )
-        assert all(x in tb.cell_output_text(79) for x in diff_cmp)
-        assert "grapes = 15" in tb.cell_output_text(81)
-        assert tb.cell_output_text(85) == dump
+        assert all(x in tb.cell_output_text(81) for x in diff_cmp)
+        assert "grapes = 15" in tb.cell_output_text(83)
+        assert tb.cell_output_text(87) == dump
         dump_dict = ("[fruit count]", "oranges = 4", "blueberries = 9")
-        assert all(x in tb.cell_output_text(87) for x in dump_dict)
+        assert all(x in tb.cell_output_text(89) for x in dump_dict)
         updated_vals = ("kiwis = 4", "raspberries = 12")
-        assert all(x in tb.cell_output_text(89) for x in updated_vals)
+        assert all(x in tb.cell_output_text(91) for x in updated_vals)
