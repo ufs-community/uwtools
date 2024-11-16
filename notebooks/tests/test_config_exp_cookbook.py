b8c2d46fc9eff9971f629010fe6b5321db61386d
Travis Byrne <travis.p.byrne@gmail.com>
2024-11-15 18:15:10 -0700

    Add validation example using chgres_cube driver

--- notebooks/tests/test_config_exp_cookbook.py
+++ notebooks/tests/test_config_exp_cookbook.py
@@ -11,11 +11,12 @@ def test_config_exp():
     with open("fixtures/config-exp/user.yaml", "r", encoding="utf-8") as f:
         user_cfg = f.read().rstrip()
     with testbook("config-exp-cookbook.ipynb", execute=True) as tb:
-        assert tb.cell_output_text(1) == base_cfg
-        assert tb.cell_output_text(3) == hrrr_ics
-        assert tb.cell_output_text(5) == fv3_rap_phys
-        assert tb.cell_output_text(7) == user_cfg
-        assert tb.cell_output_text(9) == ""
+        assert tb.cell_output_text(1) == ""
+        assert tb.cell_output_text(2) == base_cfg
+        assert tb.cell_output_text(4) == hrrr_ics
+        assert tb.cell_output_text(6) == fv3_rap_phys
+        assert tb.cell_output_text(8) == user_cfg
+        assert tb.cell_output_text(10) == ""
         updated_cfg = (
             "ACCOUNT: zrtrr",
             "convert_nst: false",
@@ -23,4 +24,11 @@ def test_config_exp():
             "varmap_file: /path/to/ufs-srweather-app/parm/",
             "rundir: /path/to/my/output/make_ics",
         )
-        assert all(x in tb.cell_output_text(11) for x in updated_cfg)
+        assert all(x in tb.cell_output_text(12) for x in updated_cfg)
+        validate_out = (
+            "INFO Validating config against internal schema: chgres-cube",
+            "INFO 0 UW schema-validation errors found",
+            "INFO Validating config against internal schema: platform",
+            "chgres_cube valid schema: State: Ready",
+        )
+        assert all(x in tb.cell_output_text(14) for x in validate_out)
