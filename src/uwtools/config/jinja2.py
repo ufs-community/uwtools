15a5905330b2359498b4e9da827b00eea961c429
Paul Madden <paul.madden@noaa.gov>
2024-11-27 05:46:21 +0000

    Simplify

--- src/uwtools/config/jinja2.py
+++ src/uwtools/config/jinja2.py
@@ -132,15 +132,7 @@ def dereference(
             if isinstance(v, UWYAMLRemove):
                 _deref_debug("Removing value at", ".".join([*keys, k]))
             else:
-                kd, vd = [
-                    dereference(
-                        val=x,
-                        context=context,
-                        local={**val, **rendered},
-                        keys=[*keys, k],
-                    )
-                    for x in (k, v)
-                ]
+                kd, vd = [dereference(x, context, val, [*keys, k]) for x in (k, v)]
                 rendered[kd] = vd
     elif isinstance(val, list):
         rendered = [dereference(v, context) for v in val]
