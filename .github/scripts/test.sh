093c6c28bb33ac35d4fc9f37a302acb03dd4670e
Paul Madden <paul.madden@noaa.gov>
2025-02-19 20:23:58 +0000

    Try installing Python version later

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -6,11 +6,11 @@ run_tests() {
   echo TESTING PYTHON $PYTHON_VERSION
   env=python-$PYTHON_VERSION
   devpkgs=$(jq .packages.dev[] recipe/meta.json | tr -d ' "')
-  conda create --yes --name $env --quiet $devpkgs python=$PYTHON_VERSION
+  conda create --yes --name $env --quiet $devpkgs
   conda activate $env
+  conda install --yes python=$PYTHON_VERSION
   . notebooks/install-deps
   set -x
-  python --version
   git clean -dfx
   pip install --editable src # sets new Python version in entry-point scripts
   make test && make test-nb
