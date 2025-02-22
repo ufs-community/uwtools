9b5ced32cbc061bb969ab745bb47bf76e1cf2e63
Paul Madden <paul.madden@noaa.gov>
2025-02-22 16:53:19 +0000

    WIP

--- notebooks/tests/test_fs.py
+++ notebooks/tests/test_fs.py
@@ -1,10 +1,19 @@
+# pylint: disable=redefined-outer-name
+
 import os
 
 import yaml
+from pytest import fixture
 from testbook import testbook
 
 
-def test_copy():
+@fixture(scope="module")
+def tb():
+    with testbook("fs.ipynb", execute=True) as tb:
+        yield tb
+
+
+def test_fs_copy(tb):
     # Get the config files as text and dictionaries.
     with open("fixtures/fs/config/copy.yaml", "r", encoding="utf-8") as f:
         config_str = f.read().rstrip()
@@ -12,46 +21,45 @@ def test_copy():
     with open("fixtures/fs/config/copy-keys.yaml", "r", encoding="utf-8") as f:
         config_keys_str = f.read().rstrip()
         config_keys_dict = yaml.safe_load(config_keys_str)
-    with testbook("fs.ipynb", execute=True) as tb:
-        # Each key in each config should have created a copy of the file given by each value.
-        for item in config_dict.items():
-            with open("tmp/copy-target/" + item[0], "r", encoding="utf-8") as f:
-                copy_dst_txt = f.read().rstrip()
-            with open("tmp/copier-target/" + item[0], "r", encoding="utf-8") as f:
-                copier_dst_txt = f.read().rstrip()
-            with open(item[1], "r", encoding="utf-8") as f:
-                src_txt = f.read().rstrip()
-            assert copy_dst_txt == src_txt
-            assert copier_dst_txt == src_txt
-        for item in config_keys_dict["files"]["to"]["copy"].items():
-            with open("tmp/copy-keys-target/" + item[0], "r", encoding="utf-8") as f:
-                copy_keys_dst_txt = f.read().rstrip()
-            with open(item[1], "r", encoding="utf-8") as f:
-                src_txt = f.read().rstrip()
-            assert copy_keys_dst_txt == src_txt
-        # Ensure that cell output text matches expectations.
-        assert tb.cell_output_text(5) == config_str
-        assert (
-            "{'ready': ['tmp/copy-target/file1-copy.nml',"
-            "\n  'tmp/copy-target/data/file2-copy.txt',"
-            "\n  'tmp/copy-target/data/file3-copy.csv'],"
-            "\n 'not-ready': []}" in tb.cell_output_text(7)
-        )
-        assert (
-            "{'ready': [], 'not-ready': ['tmp/copy-target/missing-copy.nml']}"
-            in tb.cell_output_text(11)
-        )
-        assert tb.cell_output_text(13) == tb.cell_output_text(9)
-        assert tb.cell_output_text(15) == config_keys_str
-        assert (
-            "{'ready': ['tmp/copy-keys-target/file1-copy.nml',"
-            "\n  'tmp/copy-keys-target/data/file2-copy.txt',"
-            "\n  'tmp/copy-keys-target/data/file3-copy.csv'],"
-            "\n 'not-ready': []}" in tb.cell_output_text(17)
-        )
+    # Each key in each config should have created a copy of the file given by each value.
+    for item in config_dict.items():
+        with open("tmp/copy-target/" + item[0], "r", encoding="utf-8") as f:
+            copy_dst_txt = f.read().rstrip()
+        with open("tmp/copier-target/" + item[0], "r", encoding="utf-8") as f:
+            copier_dst_txt = f.read().rstrip()
+        with open(item[1], "r", encoding="utf-8") as f:
+            src_txt = f.read().rstrip()
+        assert copy_dst_txt == src_txt
+        assert copier_dst_txt == src_txt
+    for item in config_keys_dict["files"]["to"]["copy"].items():
+        with open("tmp/copy-keys-target/" + item[0], "r", encoding="utf-8") as f:
+            copy_keys_dst_txt = f.read().rstrip()
+        with open(item[1], "r", encoding="utf-8") as f:
+            src_txt = f.read().rstrip()
+        assert copy_keys_dst_txt == src_txt
+    # Ensure that cell output text matches expectations.
+    assert tb.cell_output_text(5) == config_str
+    assert (
+        "{'ready': ['tmp/copy-target/file1-copy.nml',"
+        "\n  'tmp/copy-target/data/file2-copy.txt',"
+        "\n  'tmp/copy-target/data/file3-copy.csv'],"
+        "\n 'not-ready': []}" in tb.cell_output_text(7)
+    )
+    assert (
+        "{'ready': [], 'not-ready': ['tmp/copy-target/missing-copy.nml']}"
+        in tb.cell_output_text(11)
+    )
+    assert tb.cell_output_text(13) == tb.cell_output_text(9)
+    assert tb.cell_output_text(15) == config_keys_str
+    assert (
+        "{'ready': ['tmp/copy-keys-target/file1-copy.nml',"
+        "\n  'tmp/copy-keys-target/data/file2-copy.txt',"
+        "\n  'tmp/copy-keys-target/data/file3-copy.csv'],"
+        "\n 'not-ready': []}" in tb.cell_output_text(17)
+    )
 
 
-def test_link():
+def test_fs_link(tb):
     # Get the config files as text and dictionaries.
     with open("fixtures/fs/config/link.yaml", "r", encoding="utf-8") as f:
         config_str = f.read().rstrip()
@@ -59,52 +67,51 @@ def test_link():
     with open("fixtures/fs/config/link-keys.yaml", "r", encoding="utf-8") as f:
         config_keys_str = f.read().rstrip()
         config_keys_dict = yaml.safe_load(config_keys_str)
-    with testbook("fs.ipynb", execute=True) as tb:
-        # Each key in each config should have created a symlink of the file given by each value.
-        for item in config_dict.items():
-            link_path = "tmp/link-target/" + item[0]
-            linker_path = "tmp/linker-target/" + item[0]
-            with open(link_path, "r", encoding="utf-8") as f:
-                link_dst_txt = f.read().rstrip()
-            with open(linker_path, "r", encoding="utf-8") as f:
-                linker_dst_txt = f.read().rstrip()
-            with open(item[1], "r", encoding="utf-8") as f:
-                src_txt = f.read().rstrip()
-            assert os.path.islink(link_path)
-            assert link_dst_txt == src_txt
-            assert os.path.islink(linker_path)
-            assert linker_dst_txt == src_txt
-        for item in config_keys_dict["files"]["to"]["link"].items():
-            link_keys_path = "tmp/link-keys-target/" + item[0]
-            with open(link_keys_path, "r", encoding="utf-8") as f:
-                link_keys_dst_txt = f.read().rstrip()
-            with open(item[1], "r", encoding="utf-8") as f:
-                src_txt = f.read().rstrip()
-            assert os.path.islink(link_keys_path)
-            assert link_keys_dst_txt == src_txt
-        # Ensure that cell output text matches expectations.
-        assert tb.cell_output_text(29) == config_str
-        assert (
-            "{'ready': ['tmp/link-target/file1-link.nml',"
-            "\n  'tmp/link-target/file2-link.txt',"
-            "\n  'tmp/link-target/data/file3-link.csv'],"
-            "\n 'not-ready': []}"
-        ) in tb.cell_output_text(31)
-        assert (
-            "{'ready': [], 'not-ready': ['tmp/link-target/missing-link.nml']}"
-            in tb.cell_output_text(35)
-        )
-        assert tb.cell_output_text(37) == tb.cell_output_text(33)
-        assert tb.cell_output_text(39) == config_keys_str
-        assert (
-            "{'ready': ['tmp/link-keys-target/file1-link.nml',"
-            "\n  'tmp/link-keys-target/file2-link.txt',"
-            "\n  'tmp/link-keys-target/data/file3-link.csv'],"
-            "\n 'not-ready': []}"
-        ) in tb.cell_output_text(41)
+    # Each key in each config should have created a symlink of the file given by each value.
+    for item in config_dict.items():
+        link_path = "tmp/link-target/" + item[0]
+        linker_path = "tmp/linker-target/" + item[0]
+        with open(link_path, "r", encoding="utf-8") as f:
+            link_dst_txt = f.read().rstrip()
+        with open(linker_path, "r", encoding="utf-8") as f:
+            linker_dst_txt = f.read().rstrip()
+        with open(item[1], "r", encoding="utf-8") as f:
+            src_txt = f.read().rstrip()
+        assert os.path.islink(link_path)
+        assert link_dst_txt == src_txt
+        assert os.path.islink(linker_path)
+        assert linker_dst_txt == src_txt
+    for item in config_keys_dict["files"]["to"]["link"].items():
+        link_keys_path = "tmp/link-keys-target/" + item[0]
+        with open(link_keys_path, "r", encoding="utf-8") as f:
+            link_keys_dst_txt = f.read().rstrip()
+        with open(item[1], "r", encoding="utf-8") as f:
+            src_txt = f.read().rstrip()
+        assert os.path.islink(link_keys_path)
+        assert link_keys_dst_txt == src_txt
+    # Ensure that cell output text matches expectations.
+    assert tb.cell_output_text(29) == config_str
+    assert (
+        "{'ready': ['tmp/link-target/file1-link.nml',"
+        "\n  'tmp/link-target/file2-link.txt',"
+        "\n  'tmp/link-target/data/file3-link.csv'],"
+        "\n 'not-ready': []}"
+    ) in tb.cell_output_text(31)
+    assert (
+        "{'ready': [], 'not-ready': ['tmp/link-target/missing-link.nml']}"
+        in tb.cell_output_text(35)
+    )
+    assert tb.cell_output_text(37) == tb.cell_output_text(33)
+    assert tb.cell_output_text(39) == config_keys_str
+    assert (
+        "{'ready': ['tmp/link-keys-target/file1-link.nml',"
+        "\n  'tmp/link-keys-target/file2-link.txt',"
+        "\n  'tmp/link-keys-target/data/file3-link.csv'],"
+        "\n 'not-ready': []}"
+    ) in tb.cell_output_text(41)
 
 
-def test_makedirs():
+def test_fs_makedirs(tb):
     # Get the config files as text and dictionaries.
     with open("fixtures/fs/config/dir.yaml", "r", encoding="utf-8") as f:
         config_str = f.read().rstrip()
@@ -112,21 +119,20 @@ def test_makedirs():
     with open("fixtures/fs/config/dir-keys.yaml", "r", encoding="utf-8") as f:
         config_keys_str = f.read().rstrip()
         config_keys_dict = yaml.safe_load(config_keys_str)
-    with testbook("fs.ipynb", execute=True) as tb:
-        # Each value in each config should have been created as one or more subdirectories.
-        for subdir in config_dict["makedirs"]:
-            assert os.path.exists("tmp/dir-target/" + subdir)
-            assert os.path.exists("tmp/makedirs-target/" + subdir)
-        for subdir in config_keys_dict["path"]["to"]["dirs"]["makedirs"]:
-            assert os.path.exists("tmp/dir-keys-target/" + subdir)
-        # Ensure that cell output text matches expectations.
-        assert tb.cell_output_text(53) == config_str
-        assert (
-            "{'ready': ['tmp/dir-target/foo', 'tmp/dir-target/bar/baz'], 'not-ready': []}"
-            in tb.cell_output_text(55)
-        )
-        assert tb.cell_output_text(59) == config_keys_str
-        assert (
-            "{'ready': ['tmp/dir-keys-target/foo/bar', 'tmp/dir-keys-target/baz'],"
-            "\n 'not-ready': []}"
-        ) in tb.cell_output_text(61)
+    # Each value in each config should have been created as one or more subdirectories.
+    for subdir in config_dict["makedirs"]:
+        assert os.path.exists("tmp/dir-target/" + subdir)
+        assert os.path.exists("tmp/makedirs-target/" + subdir)
+    for subdir in config_keys_dict["path"]["to"]["dirs"]["makedirs"]:
+        assert os.path.exists("tmp/dir-keys-target/" + subdir)
+    # Ensure that cell output text matches expectations.
+    assert tb.cell_output_text(53) == config_str
+    assert (
+        "{'ready': ['tmp/dir-target/foo', 'tmp/dir-target/bar/baz'], 'not-ready': []}"
+        in tb.cell_output_text(55)
+    )
+    assert tb.cell_output_text(59) == config_keys_str
+    assert (
+        "{'ready': ['tmp/dir-keys-target/foo/bar', 'tmp/dir-keys-target/baz'],"
+        "\n 'not-ready': []}"
+    ) in tb.cell_output_text(61)
--- notebooks/tests/test_template.py
+++ notebooks/tests/test_template.py
@@ -1,75 +1,63 @@
-import os
+# pylint: disable=redefined-outer-name
+
+from glob import glob
+from pathlib import Path
 
 import yaml
+from pytest import fixture
 from testbook import testbook
 
 
-def test_render():
+@fixture(scope="module")
+def tb():
+    for p in glob("fixtures/template/complete-*"):
+        Path(p).unlink()
+    with testbook("template.ipynb", execute=True) as tb:
+        yield tb
 
+
+def test_template_render(tb):
     template = "fixtures/template/render-template.yaml"
     values = "fixtures/template/render-values.yaml"
-    rendered_template1 = "fixtures/template/render-complete-1.yaml"
-    rendered_template2 = "fixtures/template/render-complete-2.yaml"
-
-    for file in rendered_template1, rendered_template2:
-        if os.path.exists(file):
-            os.remove(file)
-
+    rendered_template1 = "fixtures/template/complete-render-1.yaml"
+    rendered_template2 = "fixtures/template/complete-render-2.yaml"
     with open(template, "r", encoding="utf-8") as f:
         template_str = f.read().rstrip()
     with open(values, "r", encoding="utf-8") as f:
         values_str = f.read().rstrip()
-
-    with testbook("template.ipynb", execute=True) as tb:
-
-        with open(rendered_template1, "r", encoding="utf-8") as f:
-            rend_temp_str1 = f.read().rstrip()
-            temp_yaml1 = yaml.safe_load(rend_temp_str1)
-        assert temp_yaml1["user"] == {"name": "John Doe", "favorite_food": "burritos"}
-
-        with open(rendered_template2, "r", encoding="utf-8") as f:
-            rend_temp_str2 = f.read().rstrip()
-            temp_yaml2 = yaml.safe_load(rend_temp_str2)
-        assert temp_yaml2["user"] == {"name": "Jane Doe", "favorite_food": "tamales"}
-
-        assert tb.cell_output_text(5) == template_str
-        assert (
-            "INFO   first" in tb.cell_output_text(7)
-            and "INFO   food" in tb.cell_output_text(7)
-            and "INFO   last" in tb.cell_output_text(7)
-        )
-        assert tb.cell_output_text(9) == values_str
-        assert tb.cell_output_text(11) == rend_temp_str1
-        assert tb.cell_output_text(13) == rend_temp_str2
-        assert rend_temp_str1 in tb.cell_output_text(15) and rend_temp_str2 in tb.cell_output_text(
-            15
-        )
+    with open(rendered_template1, "r", encoding="utf-8") as f:
+        rend_temp_str1 = f.read().rstrip()
+        temp_yaml1 = yaml.safe_load(rend_temp_str1)
+    assert temp_yaml1["user"] == {"name": "John Doe", "favorite_food": "burritos"}
+    with open(rendered_template2, "r", encoding="utf-8") as f:
+        rend_temp_str2 = f.read().rstrip()
+        temp_yaml2 = yaml.safe_load(rend_temp_str2)
+    assert temp_yaml2["user"] == {"name": "Jane Doe", "favorite_food": "tamales"}
+    assert tb.cell_output_text(5) == template_str
+    assert (
+        "INFO   first" in tb.cell_output_text(7)
+        and "INFO   food" in tb.cell_output_text(7)
+        and "INFO   last" in tb.cell_output_text(7)
+    )
+    assert tb.cell_output_text(9) == values_str
+    assert tb.cell_output_text(11) == rend_temp_str1
+    assert tb.cell_output_text(13) == rend_temp_str2
+    assert rend_temp_str1 in tb.cell_output_text(15) and rend_temp_str2 in tb.cell_output_text(15)
 
 
-@testbook("template.ipynb", execute=True)
-def test_render_to_str(tb):
-
+def test_template_render_to_str(tb):
     rend_temp_str = "user:\n  name: John Doe\n  favorite_food: burritos"
     assert tb.ref("result") == rend_temp_str
     assert tb.cell_output_text(19) == rend_temp_str
 
 
-def test_translate():
-
+def test_template_translate(tb):
     atparse_template = "fixtures/template/translate-template.yaml"
-    translated_template = "fixtures/template/translate-complete.yaml"
-
-    if os.path.exists(translated_template):
-        os.remove(translated_template)
-
+    translated_template = "fixtures/template/complete-translate.yaml"
     with open(atparse_template, "r", encoding="utf-8") as f:
         atparse_str = f.read().rstrip()
-
-    with testbook("template.ipynb", execute=True) as tb:
-
-        with open(translated_template, "r", encoding="utf-8") as f:
-            translated_str = f.read().rstrip()
-
-        assert tb.cell_output_text(23) == atparse_str
-        assert tb.cell_output_text(25) == "True"
-        assert tb.cell_output_text(27) == translated_str
+    with open(translated_template, "r", encoding="utf-8") as f:
+        translated_str = f.read().rstrip()
+    assert tb.cell_output_text(23) == atparse_str
+    assert tb.cell_output_text(25) == "True"
+    assert tb.cell_output_text(27) == translated_str
