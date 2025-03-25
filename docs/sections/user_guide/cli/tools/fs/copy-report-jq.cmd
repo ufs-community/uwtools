7c3fed303d64772ad79dfdaeb120457bbbcda33e
Paul Madden <paul.madden@noaa.gov>
2025-03-25 16:49:58 +0000

    Fix copy-report-jq.cmd

--- docs/sections/user_guide/cli/tools/fs/copy-report-jq.cmd
+++ docs/sections/user_guide/cli/tools/fs/copy-report-jq.cmd
@@ -1,2 +1,2 @@
-rm -rf dst/copy
-uw fs copy --report --target-dir dst/copy --config-file copy-report.yaml 2>/dev/null | jq -r .ready[]
+rm -rf dst/copy-report-jq
+uw fs copy --report --target-dir dst/copy-report-jq --config-file copy-report.yaml 2>/dev/null | jq -r .ready[]
