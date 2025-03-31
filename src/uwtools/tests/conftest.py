ca37b7e3733571a85fe142e46a3d936e3d2cbe49
Paul Madden <paul.madden@noaa.gov>
2025-03-31 20:55:35 +0000

    Add logged fixture [skip ci]

--- src/uwtools/tests/conftest.py
+++ src/uwtools/tests/conftest.py
@@ -1,7 +1,19 @@
 # pylint: disable=missing-function-docstring
 
+import re
+
 from iotaa import asset, external
 from pytest import fixture
+
+
+@fixture
+def logged(caplog):
+    def logged_(s: str):
+        found = any(re.match(rf"^.*{s}.*$", message) for message in caplog.messages)
+        caplog.clear()
+        return found
+
+    return logged_
 
 
 @fixture
