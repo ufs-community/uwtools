a0406e8671fb010ffa9227536bc86f4c60390d71
Paul Madden <136389411+maddenp-cu@users.noreply.github.com>
2025-06-12 20:51:49 -0600

    No user-agent string overrides (#752)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,6 @@ numfig = True
 numfig_format = {"figure": "Figure %s"}
 project = "Unified Workflow Tools"
 release = _metadata["version"]
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 version = _metadata["version"]
 
 extlinks = {
