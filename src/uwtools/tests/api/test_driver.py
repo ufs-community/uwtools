a61a3858cce7205a4c08ed7707c44ad216771288
Paul Madden <paul.madden@noaa.gov>
2025-06-13 23:44:13 +0000

    WIP

--- src/uwtools/tests/api/test_driver.py
+++ src/uwtools/tests/api/test_driver.py
@@ -17,5 +17,13 @@ from uwtools.drivers import driver as driver_lib
         "DriverTimeInvariant",
     ],
 )
-def test_driver(classname):
+def test_api_driver(classname):
     assert getattr(driver_api, classname) is getattr(driver_lib, classname)
+
+
+def test_api_driver_yaml_keys_to_classes():
+    mapping = driver_api.yaml_keys_to_classes()
+    assert mapping
+    for k, v in mapping.items():
+        assert isinstance(k, str)
+        assert issubclass(v, driver_api.Assets)
