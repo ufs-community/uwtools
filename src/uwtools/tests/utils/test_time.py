d14dd9496d944fbdf76070cf05e7a4e6656f373e
Paul Madden <paul.madden@noaa.gov>
2025-07-31 22:57:40 +0000

    Add to_timedelta [skip ci]

--- src/uwtools/tests/utils/test_time.py
+++ src/uwtools/tests/utils/test_time.py
@@ -2,19 +2,20 @@
 Tests for uwtools.utils.time module.
 """
 
-from datetime import datetime, timezone
-
-from pytest import mark
+from datetime import timedelta
 
 from uwtools.utils import time
 
 
-@mark.parametrize(
-    "value",
-    [
-        datetime(2025, 7, 31, 12, tzinfo=timezone.utc).replace(tzinfo=None),
-        "2025-07-31T12:00:00",
-    ],
-)
-def test_utils_time_to_datetime(utc, value):
-    assert time.to_datetime(value=value) == utc(2025, 7, 31, 12)
+def test_utils_time_to_datetime(utc):
+    expected = utc(2025, 7, 31, 12)
+    for value in [utc(2025, 7, 31, 12), "2025-07-31T12:00:00"]:
+        assert time.to_datetime(value=value) == expected
+
+
+def test_utils_time_to_timedelta():
+    for value, expected in [
+        ("01:02:03", timedelta(hours=1, minutes=2, seconds=3)),
+        ("168:00:00", timedelta(days=7)),
+    ]:
+        assert time.to_timedelta(value=value) == expected
--- src/uwtools/utils/time.py
+++ src/uwtools/utils/time.py
@@ -4,10 +4,18 @@ Helpers for working with time values.
 
 from __future__ import annotations
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def to_datetime(value: str | datetime) -> datetime:
     if isinstance(value, datetime):
         return value
     return datetime.fromisoformat(value)
+
+
+def to_timedelta(value: str | int) -> timedelta:
+    if isinstance(value, int):
+        return timedelta(hours=value)
+    keys = ["hours", "minutes", "seconds"]
+    args = dict(zip(keys, map(int, value.split(":"))))
+    return timedelta(**args)
