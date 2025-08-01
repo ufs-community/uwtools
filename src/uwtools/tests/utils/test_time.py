4c46e14cc005fe9776f96203d6a9f62eefede4b5
Paul Madden <paul.madden@noaa.gov>
2025-08-01 05:30:53 +0000

    Add to_iso8601() tests

--- src/uwtools/tests/utils/test_time.py
+++ src/uwtools/tests/utils/test_time.py
@@ -4,6 +4,8 @@ Tests for uwtools.utils.time module.
 
 from datetime import timedelta
 
+from pytest import mark
+
 from uwtools.utils import time
 
 
@@ -11,6 +13,17 @@ def test_utils_time_to_datetime(utc):
     expected = utc(2025, 7, 31, 12)
     for value in [utc(2025, 7, 31, 12), "2025-07-31T12:00:00"]:
         assert time.to_datetime(value=value) == expected
+
+
+@mark.parametrize("sep", ["T", " "])
+@mark.parametrize("val", ["2025-07-31%s12", "2025-07-31%s12:00", "2025-07-31%s12:00:00"])
+def test_utils_time_to_iso8601(sep, val):
+    assert time.to_iso8601(val % sep) == "2025-07-31T12:00:00"
+
+
+def test_utils_time_to_iso8601_dtobj(utc):
+    dtobj = utc(2025, 7, 31, 12)
+    assert time.to_iso8601(dtobj) == "2025-07-31T12:00:00"
 
 
 def test_utils_time_to_timedelta():
