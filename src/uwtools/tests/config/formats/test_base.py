f15dc2cf3a77e1bfb73dee3b8817e5842a648750
Paul Madden <paul.madden@noaa.gov>
2025-09-15 21:50:47 +0000

    Add unit test

--- src/uwtools/tests/config/formats/test_base.py
+++ src/uwtools/tests/config/formats/test_base.py
@@ -306,6 +306,20 @@ def test_config_base__obj_dereference__context_override(tmp_path, utc):
     assert config["file"] == "gfs.t06z.atmanl.nc"
 
 
+@mark.parametrize("self_as_context", [False, True])
+def test_config_base__obj_dereference__self_context(self_as_context, tmp_path):
+    yaml = """
+    a: !int '{{ 1 + 1 }}'
+    b: !int '{{ a + 1 }}'
+    """
+    path = tmp_path / "config.yaml"
+    path.write_text(yaml)
+    config = YAMLConfig(path)
+    context = YAMLConfig(path) if self_as_context else None
+    assert config.dereference(context=context).data == {"a": 2, "b": 3}
+    
+
+
 @mark.parametrize("fmt2", [FORMAT.ini, FORMAT.sh])
 def test_config_base__obj_invalid_config(fmt2, tmp_path):
     """
