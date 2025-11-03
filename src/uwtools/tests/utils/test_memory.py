6c5d21f78966e42d27c34cab51044efa6113ae79
Paul Madden <paul.madden@noaa.gov>
2025-11-03 20:18:51 +0000

    Fixes for memory.py

--- src/uwtools/tests/utils/test_memory.py
+++ src/uwtools/tests/utils/test_memory.py
@@ -2,13 +2,23 @@
 Tests for uwtools.utils.file_helpers module.
 """
 
+from pytest import mark
+
 from uwtools.utils.memory import Memory
 
 
-def test_memory_conversions():
-    assert str(Memory("100MB").convert("KB")) == "100000KB"
-    assert str(Memory("100KB").convert("MB")) == "0.1MB"
-    assert str(Memory("100GB").convert("KB")) == "100000000KB"
-    assert str(Memory("100MB").convert("GB")) == "0.1GB"
-    assert str(Memory("100GB").convert("MB")) == "100000MB"
-    assert str(Memory("100KB").convert("GB")) == "9.999999999999999e-05GB"
+@mark.parametrize(
+    ("value", "measurement", "expected"),
+    [
+        ("100MB", "KB", "100000KB"),
+        ("100KB", "MB", "0.1MB"),
+        ("100GB", "KB", "100000000KB"),
+        ("100MB", "GB", "0.1GB"),
+        ("100GB", "MB", "100000MB"),
+        ("100KB", "GB", "9.999999999999999e-05GB"),
+    ],
+)
+def test_memory_conversions(value, measurement, expected):
+    m = Memory(value)
+    assert str(m.convert(measurement)) == expected
+    assert m.measurement == value[-2:]
