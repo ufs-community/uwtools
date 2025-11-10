8173852b1c47cbea852a5f468adf0f0c68b5fd14
Paul Madden <paul.madden@noaa.gov>
2025-11-10 20:46:27 +0000

    Update CI workflow/scripts

---
+++ .github/scripts/validate-yaml-configs.sh
@@ -0,0 +1,11 @@
+set -ae
+. $(dirname ${BASH_SOURCE[0]})/common.sh
+ci_conda_activate
+for config in docs/shared/drivers/*.yaml; do
+  driver=$(basename ${config%.yaml})
+  basecmd="uw $driver validate"
+  args=( --config $config )
+  $basecmd --help | grep -q -- "--cycle" && args+=( --cycle 1970-01-01T00:00:00 )
+  $basecmd --help | grep -q -- "--leadtime" && args+=( --leadtime 6 )
+  ( set -x && $basecmd ${args[*]} )
+done
