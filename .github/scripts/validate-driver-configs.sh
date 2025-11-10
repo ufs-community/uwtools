85ec3ac40a799d264c2222023e02a29c53cc7c56
Paul Madden <paul.madden@noaa.gov>
2025-11-10 21:31:34 +0000

    WIP

--- .github/scripts/validate-driver-configs.sh
+++ .github/scripts/validate-driver-configs.sh
@@ -1,11 +1,16 @@
 set -ae
 . $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
-for config in docs/shared/drivers/*.yaml; do
-  driver=$(basename ${config%.yaml})
-  basecmd="uw $driver validate"
-  args=( --config $config )
-  $basecmd --help | grep -q -- "--cycle" && args+=( --cycle 1970-01-01T00:00:00 )
-  $basecmd --help | grep -q -- "--leadtime" && args+=( --leadtime 6 )
-  ( set -x && $basecmd ${args[*]} )
-done
+
+validate_driver_configs() {
+  for config in docs/shared/drivers/*.yaml; do
+    driver=$(basename ${config%.yaml})
+    basecmd="uw $driver validate"
+    args=( --config $config )
+    $basecmd --help | grep -q -- "--cycle" && args+=( --cycle 1970-01-01T00:00:00 )
+    $basecmd --help | grep -q -- "--leadtime" && args+=( --leadtime 6 )
+    ( set -x && $basecmd ${args[*]} )
+  done
+}
+
+CONDEV_SHELL_CMD=validate_driver_configs condev-shell
