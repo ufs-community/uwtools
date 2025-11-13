e6d384b981027b7026756b417b526b07836d4439
Paul Madden <paul.madden@noaa.gov>
2025-11-13 01:07:27 +0000

    Clean up jinja2 tests

--- src/uwtools/tests/config/test_jinja2.py
+++ src/uwtools/tests/config/test_jinja2.py
@@ -74,13 +74,13 @@ def validate(template):
 # Tests
 
 
-def test_dereference_key_expression():
+def test_config_jinja2_dereference__key_expression():
     assert jinja2.dereference(val={"{{ fruit }}": "red"}, context={"fruit": "apple"}) == {
         "apple": "red"
     }
 
 
-def test_dereference_local_values():
+def test_config_jinja2_dereference__local_values():
     # Rendering can use values from the local contents of the enclosing dict, but are shadowed by
     # values from the top-level context object.
     val = {
@@ -102,7 +102,7 @@ def test_dereference_local_values():
 
 
 @mark.parametrize("val", [True, 3.14, 42, None])
-def test_dereference_no_op(val):
+def test_config_jinja2_dereference__no_op(val):
     # These types of values pass through dereferencing unmodified:
     assert jinja2.dereference(val=val, context={}) == val
 
@@ -116,7 +116,7 @@ def test_dereference_no_op(val):
         ("division by zero", "{{ 1 / 0 }}"),
     ],
 )
-def test_dereference_no_op_due_to_error(logged, logmsg, val):
+def test_config_jinja2_dereference__no_op_due_to_error(logged, logmsg, val):
     # Erroneous inputs cause:
     #   - A type error due to + operating on a str and an int.
     #   - An undefined error due to reference to a non-existent value.
@@ -127,61 +127,42 @@ def test_dereference_no_op_due_to_error(logged, logmsg, val):
     assert logged(logmsg)
 
 
-def test_dereference_remove(logged):
+def test_config_jinja2_dereference__remove(logged):
     remove = UWYAMLRemove(yaml.SafeLoader(""), yaml.ScalarNode(tag="!remove", value=""))
     val = {"a": {"b": {"c": "cherry", "d": remove}}}
     assert jinja2.dereference(val=val, context={}) == {"a": {"b": {"c": "cherry"}}}
     assert logged("Removing value at: a.b.d")
 
 
-def test_dereference_str_expression_rendered():
+def test_config_jinja2_dereference__str_expression_rendered():
     # Context permitting, Jinja2 variables/expressions are rendered:
     val = "{% for a in as %}{{ a }}{% endfor %}"
     assert jinja2.dereference(val=val, context={"as": ["a", "b", "c"]}) == "abc"
 
 
-def test_dereference_str_filter_rendered():
+def test_config_jinja2_dereference__str_filter_rendered():
     val = "{{ ['hello', recipient] | join(', ') }}"
     assert jinja2.dereference(val=val, context={"recipient": "world"}) == "hello, world"
 
 
-def test_dereference_str_variable_rendered_mixed():
+def test_config_jinja2_dereference__str_variable_rendered_mixed():
     # A mixed result remains a str.
     val = "{{ n }} is an {{ t }}"
     assert jinja2.dereference(val=val, context={"n": 42, "t": "int"}) == "42 is an int"
 
 
-def test_dereference_str_variable_rendered_str():
+def test_config_jinja2_dereference__str_variable_rendered_str():
     # A pure str result remains a str.
     val = "{{ greeting }}"
     assert jinja2.dereference(val=val, context={"greeting": "hello"}) == "hello"
 
 
-def test_deref_debug(logged):
+def test_config_jinja2_deref_debug(logged):
     jinja2.deref_debug(action="Frobnicated", val="foo")
     assert logged("[dereference] Frobnicated: foo")
 
 
-def test_register_filters_env():
-    s = "hello {{ 'RECIPIENT' | env }}"
-    template = jinja2._register_filters(Environment(undefined=DebugUndefined)).from_string(s)
-    with patch.dict(os.environ, {"RECIPIENT": "world"}, clear=True):
-        assert template.render() == "hello world"
-
-
-@mark.parametrize("key", ["foo", "bar"])
-def test_register_filters_path_join(key):
-    s = "{{ ['dir', %s] | path_join }}" % key
-    template = jinja2._register_filters(Environment(undefined=DebugUndefined)).from_string(s)
-    context = {"foo": "subdir"}
-    if key in context:
-        template.render(**context)  # path_join filter succeeds
-    else:
-        with raises(UndefinedError):
-            template.render(**context)  # path_join filter fails
-
-
-def test_render(values_file, template_file, tmp_path):
+def test_config_jinja2_render(values_file, template_file, tmp_path):
     outfile = tmp_path / "out.txt"
     expected = "roses are red, violets are blue"
     result = render_helper(input_file=template_file, values_file=values_file, output_file=outfile)
@@ -189,7 +170,7 @@ def test_render(values_file, template_file, tmp_path):
     assert outfile.read_text().strip() == expected
 
 
-def test_render_calls__dry_run(template_file, tmp_path, values_file):
+def test_config_jinja2_render__calls__dry_run(template_file, tmp_path, values_file):
     outfile = str(tmp_path / "out.txt")
     with patch.object(jinja2, "_dry_run_template") as dr:
         render_helper(
@@ -198,7 +179,7 @@ def test_render_calls__dry_run(template_file, tmp_path, values_file):
         dr.assert_called_once_with("roses are red, violets are blue")
 
 
-def test_render_calls__log_missing(template_file, tmp_path, values_file):
+def test_config_jinja2_render__calls__log_missing(template_file, tmp_path, values_file):
     outfile = str(tmp_path / "out.txt")
     cfgobj = yaml.safe_load(values_file.read_text())
     del cfgobj["roses_color"]
@@ -208,7 +189,7 @@ def test_render_calls__log_missing(template_file, tmp_path, values_file):
         lmv.assert_called_once_with(["roses_color"])
 
 
-def test_render_calls__values_needed(template_file, tmp_path, values_file):
+def test_config_jinja2_render__calls__values_needed(template_file, tmp_path, values_file):
     outfile = str(tmp_path / "out.txt")
     with patch.object(jinja2, "_values_needed") as vn:
         render_helper(
@@ -220,21 +201,21 @@ def test_render_calls__values_needed(template_file, tmp_path, values_file):
         vn.assert_called_once_with({"roses_color", "violets_color"})
 
 
-def test_render_calls__write(template_file, tmp_path, values_file):
+def test_config_jinja2_render__calls__write_template(template_file, tmp_path, values_file):
     outfile = str(tmp_path / "out.txt")
     with patch.object(jinja2, "_write_template") as write:
         render_helper(input_file=template_file, values_file=values_file, output_file=outfile)
         write.assert_called_once_with(outfile, "roses are red, violets are blue")
 
 
-def test_render_dry_run(logged, template_file, values_file):
+def test_config_jinja2_render__dry_run(logged, template_file, values_file):
     expected = "roses are red, violets are blue"
     result = render_helper(input_file=template_file, values_file=values_file, dry_run=True)
     assert result == expected
     assert logged(expected)
 
 
-def test_render_fails(logged, tmp_path):
+def test_config_jinja2_render__fail(logged, tmp_path):
     input_file = tmp_path / "template.yaml"
     input_file.write_text("{{ constants.pi }} {{ constants.e }}")
     values_file = tmp_path / "values.yaml"
@@ -243,7 +224,7 @@ def test_render_fails(logged, tmp_path):
     assert logged("Template render failed with error: 'dict object' has no attribute 'e'")
 
 
-def test_render_values_missing(logged, template_file, values_file):
+def test_config_jinja2_render__values_missing(logged, template_file, values_file):
     # Read in the config, remove the "roses" key, then re-write it.
     cfgobj = yaml.safe_load(values_file.read_text())
     del cfgobj["roses_color"]
@@ -253,33 +234,15 @@ def test_render_values_missing(logged, template_file, values_file):
     assert logged("  roses_color")
 
 
-def test_render_values_needed(logged, template_file, values_file):
+def test_config_jinja2_render__values_needed(logged, template_file, values_file):
     render_helper(input_file=template_file, values_file=values_file, values_needed=True)
     for var in ("roses_color", "violets_color"):
         assert logged(f"  {var}")
 
 
 @mark.parametrize(("s", "status"), [("foo: bar", False), ("foo: '{{ bar }} {{ baz }}'", True)])
-def test_unrendered(s, status):
+def test_config_jinja2_unrendered(s, status):
     assert jinja2.unrendered(s) is status
-
-
-@mark.parametrize(
-    ("tag", "value"),
-    [
-        ("!datetime", "foo"),
-        ("!dict", "foo"),
-        ("!float", "foo"),
-        ("!int", "foo"),
-        ("!list", "null"),
-    ],
-)
-def test__deref_convert_no(logged, tag, value):
-    loader = yaml.SafeLoader(os.devnull)
-    val = UWYAMLConvert(loader, yaml.ScalarNode(tag=tag, value=value))
-    assert jinja2._deref_convert(val=val) == val
-    assert not logged("Converted")
-    assert logged("Conversion failed")
 
 
 @mark.parametrize(
@@ -301,7 +264,7 @@ def test__deref_convert_no(logged, tag, value):
         ([0, 1, 2], "!list", "{0: a, 1: b, 2: c}"),
     ],
 )
-def test__deref_convert_ok(converted, logged, tag, value):
+def test_config_jinja2__deref_convert(converted, logged, tag, value):
     loader = yaml.SafeLoader(os.devnull)
     val = UWYAMLConvert(loader, yaml.ScalarNode(tag=tag, value=value))
     assert jinja2._deref_convert(val=val) == converted
@@ -309,41 +272,59 @@ def test__deref_convert_ok(converted, logged, tag, value):
     assert not logged("Conversion failed")
 
 
-def test__deref_render_held(logged):
+@mark.parametrize(
+    ("tag", "value"),
+    [
+        ("!datetime", "foo"),
+        ("!dict", "foo"),
+        ("!float", "foo"),
+        ("!int", "foo"),
+        ("!list", "null"),
+    ],
+)
+def test_config_jinja2__deref_convert__fail(logged, tag, value):
+    loader = yaml.SafeLoader(os.devnull)
+    val = UWYAMLConvert(loader, yaml.ScalarNode(tag=tag, value=value))
+    assert jinja2._deref_convert(val=val) == val
+    assert not logged("Converted")
+    assert logged("Conversion failed")
+
+
+def test_config_jinja2__deref_render__held(logged):
     val, context = "!int '{{ a }}'", yaml.load("a: !int '42'", Loader=uw_yaml_loader())
     assert jinja2._deref_render(val=val, context=context) == val
     assert logged("Rendered")
     assert logged("Held")
 
 
-def test__deref_render_no(deref_render_assets, logged):
-    val, context, _ = deref_render_assets
-    assert jinja2._deref_render(val=val, context=context) == val
-    assert not logged("Rendered")
-    assert logged("Rendering failed")
-
-
-def test__deref_render_ok(deref_render_assets, logged):
+def test_config_jinja2__deref_render(deref_render_assets, logged):
     val, context, local = deref_render_assets
     assert jinja2._deref_render(val=val, context=context, local=local) == "hello world"
     assert logged("Rendered")
     assert not logged("Rendering failed")
 
 
-def test__deref_render_unloadable_val(logged):
+def test_config_jinja2__deref_render__fail(deref_render_assets, logged):
+    val, context, _ = deref_render_assets
+    assert jinja2._deref_render(val=val, context=context) == val
+    assert not logged("Rendered")
+    assert logged("Rendering failed")
+
+
+def test_config_jinja2__deref_render__unloadable_val(logged):
     val = "&XMLENTITY;"
     assert jinja2._deref_render(val='{{ "%s" if True }}' % val, context={}) == val
     assert logged("Rendered")
     assert not logged("Rendering failed")
 
 
-def test__dry_run_template(logged):
+def test_config_jinja2__dry_run_template(logged):
     jinja2._dry_run_template("roses are red\nviolets are blue")
     assert logged("roses are red")
     assert logged("violets are blue")
 
 
-def test__log_missing_values(logged):
+def test_config_jinja2__log_missing_values(logged):
     missing = ["roses_color", "violets_color"]
     jinja2._log_missing_values(missing)
     assert logged("Value(s) required to render template not provided:")
@@ -351,7 +332,26 @@ def test__log_missing_values(logged):
     assert logged("  violets_color")
 
 
-def test__report(logged):
+def test_config_jinja2__register_filters__env():
+    s = "hello {{ 'RECIPIENT' | env }}"
+    template = jinja2._register_filters(Environment(undefined=DebugUndefined)).from_string(s)
+    with patch.dict(os.environ, {"RECIPIENT": "world"}, clear=True):
+        assert template.render() == "hello world"
+
+
+@mark.parametrize("key", ["foo", "bar"])
+def test_config_jinja2__register_filters__path_join(key):
+    s = "{{ ['dir', %s] | path_join }}" % key
+    template = jinja2._register_filters(Environment(undefined=DebugUndefined)).from_string(s)
+    context = {"foo": "subdir"}
+    if key in context:
+        template.render(**context)  # path_join filter succeeds
+    else:
+        with raises(UndefinedError):
+            template.render(**context)  # path_join filter fails
+
+
+def test_config_jinja2__report(logged):
     expected = """
 Internal arguments when rendering template:
 ---------------------------------------------------------------------
@@ -363,22 +363,22 @@ longish_variable: 42
     assert logged(expected, full=True)
 
 
-def test__supplement_values():
+def test_config_jinja2__supplement_values():
     assert jinja2._supplement_values() == {}
 
 
-def test__supplement_values_dict(supplemental_values):
+def test_config_jinja2__supplement_values__dict(supplemental_values):
     sv = supplemental_values
     assert jinja2._supplement_values(values_src=sv.d) == sv.d
 
 
-def test__supplement_values_dict_plus_env(supplemental_values):
+def test_config_jinja2__supplement_values__dict_plus_env(supplemental_values):
     sv = supplemental_values
     with patch.dict(os.environ, sv.e, clear=True):
         assert jinja2._supplement_values(values_src=sv.d, env=True) == {**sv.d, **sv.e}
 
 
-def test__supplement_values_dict_plus_env_plus_overrides(supplemental_values):
+def test_config_jinja2__supplement_values__dict_plus_env_plus_overrides(supplemental_values):
     sv = supplemental_values
     with patch.dict(os.environ, sv.e, clear=True):
         assert jinja2._supplement_values(values_src=sv.d, env=True, overrides=sv.o) == {
@@ -388,40 +388,40 @@ def test__supplement_values_dict_plus_env_plus_overrides(supplemental_values):
         }
 
 
-def test__supplement_values_dict_plus_overrides(supplemental_values):
+def test_config_jinja2__supplement_values__dict_plus_overrides(supplemental_values):
     sv = supplemental_values
     assert jinja2._supplement_values(values_src=sv.d, overrides=sv.o) == {**sv.d, **sv.o}
 
 
-def test__supplement_values_env(supplemental_values):
+def test_config_jinja2__supplement_values__env(supplemental_values):
     sv = supplemental_values
     with patch.dict(os.environ, sv.e, clear=True):
         assert jinja2._supplement_values(env=True) == sv.e
 
 
-def test__supplement_values_env_plus_overrides(supplemental_values):
+def test_config_jinja2__supplement_values__env_plus_overrides(supplemental_values):
     sv = supplemental_values
     with patch.dict(os.environ, sv.e, clear=True):
         assert jinja2._supplement_values(env=True, overrides=sv.o) == {**sv.o, **sv.e}
 
 
-def test__supplement_values_overrides(supplemental_values):
+def test_config_jinja2__supplement_values__overrides(supplemental_values):
     sv = supplemental_values
     assert jinja2._supplement_values(overrides=sv.o) == sv.o
 
 
-def test__supplement_values_file(supplemental_values):
+def test_config_jinja2__supplement_values__file(supplemental_values):
     sv = supplemental_values
     assert jinja2._supplement_values(values_src=sv.f) == sv.d
 
 
-def test__supplement_values_file_plus_env(supplemental_values):
+def test_config_jinja2__supplement_values__file_plus_env(supplemental_values):
     sv = supplemental_values
     with patch.dict(os.environ, sv.e, clear=True):
         assert jinja2._supplement_values(values_src=sv.f, env=True) == {**sv.d, **sv.e}
 
 
-def test__supplement_values_file_plus_env_plus_overrides(supplemental_values):
+def test_config_jinja2__supplement_values__file_plus_env_plus_overrides(supplemental_values):
     sv = supplemental_values
     with patch.dict(os.environ, sv.e, clear=True):
         assert jinja2._supplement_values(values_src=sv.f, env=True, overrides=sv.o) == {
@@ -431,12 +431,12 @@ def test__supplement_values_file_plus_env_plus_overrides(supplemental_values):
         }
 
 
-def test__supplement_values_file_plus_overrides(supplemental_values):
+def test_config_jinja2__supplement_values__file_plus_overrides(supplemental_values):
     sv = supplemental_values
     assert jinja2._supplement_values(values_src=sv.d, overrides=sv.o) == {**sv.d, **sv.o}
 
 
-def test__supplement_values_priority(supplemental_values):
+def test_config_jinja2__supplement_values__priority(supplemental_values):
     # Test environment variable > CLI key=value overrides > file value priority.
     sv = supplemental_values
     assert jinja2._supplement_values(values_src=sv.f)["foo"] == "bar"
@@ -447,7 +447,7 @@ def test__supplement_values_priority(supplemental_values):
         assert jinja2._supplement_values(values_src=sv.f, env=True, overrides=o)["foo"] == e["foo"]
 
 
-def test__values_needed(logged):
+def test_config_jinja2__values_needed(logged):
     undeclared_variables = {"roses_color", "lavender_smell"}
     jinja2._values_needed(undeclared_variables)
     assert logged("Value(s) needed to render this template are:")
@@ -455,13 +455,13 @@ def test__values_needed(logged):
     assert logged("  lavender_smell")
 
 
-def test__write_template_to_file(tmp_path):
+def test_config_jinja2__write_template__to_file(tmp_path):
     outfile = tmp_path / "out.txt"
     jinja2._write_template(outfile, "roses are red, violets are blue")
     assert outfile.read_text().strip() == "roses are red, violets are blue"
 
 
-def test__write_template_stdout(capsys):
+def test_config_jinja2__write_template__to_stdout(capsys):
     jinja2._write_template(None, "roses are red, violets are blue")
     actual = capsys.readouterr().out
     expected = "roses are red, violets are blue"
