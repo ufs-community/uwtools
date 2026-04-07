59ad46713d7bfe1f8c9e7a1c3f13e6ca5456edc8
Anna Smoot <anna.smoot@noaa.gov>
2026-04-07 15:52:20 +0000

    Add basic pytest for ecflow module

---
+++ src/uwtools/tests/test_ecflow.py
@@ -0,0 +1,24 @@
+"""
+Tests for uwtools.ecflow module.
+"""
+
+from pytest import mark
+
+from uwtools.ecflow import _ECFlowDef
+
+
+class Test_ECFlowDef:
+
+    @mark.parametrize(
+        ("key", "expected"),
+        [
+            ("task_foo", ("task", "foo")),
+            ("family_my_family", ("family", "my_family")),
+            ("suite_prod", ("suite", "prod")),
+            ("vars", ("vars", "")),
+        ],
+    )
+    def test__tag_name(self, key, expected):
+        # _tag_name is called on self, so we need a minimal instance
+        ecf = _ECFlowDef(config={"ecflow": {}})
+        assert ecf._tag_name(key) == expected
