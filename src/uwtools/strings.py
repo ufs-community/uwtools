13c3063332a1c73551643be0f0a5e974541d1439
Paul Madden <paul.madden@noaa.gov>
2026-04-18 16:27:07 +0000

    Fix docstring

--- src/uwtools/strings.py
+++ src/uwtools/strings.py
@@ -26,7 +26,7 @@ class _ValsMatchKeys:
 @dataclass(frozen=True)
 class _EC(_ValsMatchKeys):
     """
-    EcFlow-specific string constants.
+    ecFlow-specific strings.
     """
 
     account: str = _
