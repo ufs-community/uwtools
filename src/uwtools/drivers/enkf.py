6e7d68e4b8c227c53e378a0a049a54384b854be0
Christina Holt <christina.holt@noaa.gov>
2026-06-02 08:10:04 -0600

    Human-readable names for tasks instead of config keys.

--- src/uwtools/drivers/enkf.py
+++ src/uwtools/drivers/enkf.py
@@ -47,7 +47,7 @@ class EnKF(DriverCycleBased, FileStager):
             Linker(
                 config=files.as_dict(),
                 target_dir=self.rundir,
-            ).go("background_files")
+            ).go("background files")
             for files in file_sets
         ]
 
