9cd2b6a04d8e93a191984c7e345441ef27cc12aa
Paul Madden <paul.madden@noaa.gov>
2026-07-07 21:19:50 +0000

    Add WIP notebook files

---
+++ wip/common.h
@@ -0,0 +1,20 @@
+abort() {
+  trap EXIT # unset previous trap
+  ecflow_client --abort=$ECF_RID
+  exit 1
+}
+
+trap abort ERR SIGTERM
+
+complete() {
+  ecflow_client --ssl --complete
+}
+
+trap complete EXIT
+
+init() {
+  export ECF_RID=$1
+  ecflow_client --ssl --init=$ECF_RID
+}
+
+set -euxo pipefail
